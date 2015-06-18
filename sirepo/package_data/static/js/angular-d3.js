526c9bf7635a644a00a6ccf7d9e45e3e99605b7c
Paul Moeller <moeller@bivio.biz>
2015-06-19 04:00:43 +0900

    initial commit

---
+++ sirepo/package_data/static/js/angular-d3.js
@@ -0,0 +1,28 @@
+
+// code from http://www.ng-newsletter.com/posts/d3-on-angular.html
+angular.module('d3', [])
+    .factory('d3Service', function($document, $q, $rootScope) {
+        var d = $q.defer();
+        function onScriptLoad() {
+            // Load client in the browser
+            $rootScope.$apply(function() { d.resolve(window.d3); });
+        }
+        // Create a script tag with d3 as the source
+        // and call our onScriptLoad callback when it
+        // has been loaded
+        var scriptTag = $document[0].createElement('script');
+        scriptTag.type = 'text/javascript';
+        scriptTag.async = true;
+        scriptTag.src = '/static/js/d3-3.5.5.js';
+        scriptTag.onreadystatechange = function () {
+            if (this.readyState == 'complete') onScriptLoad();
+        }
+        scriptTag.onload = onScriptLoad;
+
+        var s = $document[0].getElementsByTagName('body')[0];
+        s.appendChild(scriptTag);
+
+        return {
+            d3: function() { return d.promise; }
+        };
+    });
