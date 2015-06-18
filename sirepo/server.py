21cab393e6585e3294d58e73d5e2fd1463e881e2
Paul Moeller <moeller@bivio.biz>
2015-06-19 03:59:40 +0900

    added srw_run()

--- sirepo/server.py
+++ sirepo/server.py
@@ -5,5 +5,11 @@ app = flask.Flask(__name__, static_folder='package_data/static')
 def srw_root():
     return app.send_static_file('html/srw.html')
 
+@app.route('/srw/run', methods=('GET', 'POST'))
+def srw_run():
+    print("in srw_run()")
+    print(flask.request.json)
+    return 'OK'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=1)
