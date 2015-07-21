6b6fdc88a9a1029f7bf3bfe4c85872d1ff377e5a
Paul Moeller <moeller@bivio.biz>
2015-07-21 22:48:42 +0000

    add tests

---
+++ tests/app/stringToNumber_test.js
@@ -0,0 +1,46 @@
+'use strict';
+beforeEach(module('SRWApp'));
+
+describe('Directive: stringToNumber', function() {
+    var $scope, $compile, $sniffer;
+
+    beforeEach(inject(function(_$rootScope_, _$compile_, _$sniffer_) {
+        $scope = _$rootScope_.$new();
+        $compile = _$compile_;
+        $sniffer = _$sniffer_;
+    }));
+
+    function changeInputValue(element, value) {
+        element.val(value);
+        browserTrigger(element, $sniffer.hasEvent('input') ? 'input' : 'change');
+    }
+
+    function createDirective(template) {
+        return $compile(template || '<form name="f1"><input string-to-number ng-model="data"></form>')($scope);
+    }
+
+    it('should throw error if nmModel not defined', function() {
+        expect(function() { createDirective('<input string-to-number>'); }).toThrow();
+    });
+    it('should accept numeric input', function() {
+        var element = createDirective().find('input');
+        expect($scope.data).toEqual(undefined);
+        changeInputValue(element, '11');
+        expect($scope.data).not.toEqual('11');
+        expect($scope.data).toEqual(11);
+        expect($scope.f1.$valid).toBe(true);
+    });
+    it('should accept exponential notation', function() {
+        var element = createDirective().find('input');
+        changeInputValue(element, '5e2');
+        expect($scope.data).toEqual(500);
+        expect($scope.f1.$valid).toBe(true);
+    });
+    it('should mark form as invalid on non-numeric input', function() {
+        var element = createDirective().find('input');
+        changeInputValue(element, 'x');
+        expect(element.val()).toEqual('x');
+        expect($scope.data).toEqual(undefined);
+        expect($scope.f1.$valid).toBe(false);
+    });
+});
