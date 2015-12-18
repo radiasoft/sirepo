1fe0d92603486de6057a22180c9ac798dd4cc665
Paul Moeller <moeller@bivio.biz>
2015-12-18 18:13:22 +0000

    resolve d3 in $timeout(), avoids problems with $apply() during active $digest()

--- sirepo/package_data/static/js/angular-d3.js
+++ sirepo/package_data/static/js/angular-d3.js
@@ -1,11 +1,11 @@
 
 // code from http://www.ng-newsletter.com/posts/d3-on-angular.html
 angular.module('d3', [])
-    .factory('d3Service', function($document, $q, $rootScope) {
+    .factory('d3Service', function($document, $q, $timeout) {
         var d = $q.defer();
         function onScriptLoad() {
             // Load client in the browser
-            $rootScope.$apply(function() { d.resolve(window.d3); });
+            $timeout(function() { d.resolve(window.d3); });
         }
         // Create a script tag with d3 as the source
         // and call our onScriptLoad callback when it
