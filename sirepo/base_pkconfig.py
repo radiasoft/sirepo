1dffd77938a2ea9c8b6980918d9a44a397ce844c
git-user <git-email>
2016-01-29 18:23:23 +0000

    need base_pkconfig.py

---
+++ sirepo/base_pkconfig.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+u"""Default config
+
+:copyright: Copyright (c) 2016 RadiaSoft LLC.  All Rights Reserved.
+:license: http://www.apache.org/licenses/LICENSE-2.0.html
+"""
+from __future__ import absolute_import, division, print_function
+
+def alpha():
+    return {}
+
+
+def beta():
+    return {}
+
+
+def dev():
+    return {}
+
+def prod():
+    return {}
