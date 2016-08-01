b4e391992e448d499b1c3eb1cafd1e14e9f973f1
Maksim Rakitin <maxim.rakitin@gmail.com>
2016-08-01 19:14:45 +0000

    SRW importer - test updated to use pkresource and py.path.local.

--- tests/importer_test.py
+++ tests/importer_test.py
@@ -12,13 +12,9 @@ from pykern.pkdebug import pkdc, pkdp
 import glob
 import py
 import pytest
-import shutil
-import uuid
 
 pytest.importorskip('srwl_bl')
 
-_ROOT_DIR = py.path.local()
-_DAT_DIR = _ROOT_DIR.join('sirepo/package_data/static/dat')
 _TESTS = {  # Values are optional arguments:
     'amx': ('amx', None),
     'amx_bl2': ('amx', '--op_BL=2'),
@@ -42,38 +38,24 @@ _TESTS = {  # Values are optional arguments:
 
 def test_importer():
     from sirepo.importer import import_python
+    dat_dir = py.path.local(pkresource.filename('static/dat/', import_python))
     with pkunit.save_chdir_work():
+        work_dir = py.path.local('.')
+        for f in glob.glob(str(dat_dir.join('mirror_*d.dat'))):
+            py.path.local(f).copy(work_dir)
         for b in sorted(_TESTS.keys()):
             base_py = '{}.py'.format(_TESTS[b][0])
             code = pkio.read_text(pkunit.data_dir().join(base_py))
-            tmp_dir = _create_tmp_dir()
             error, actual = import_python(
                 code,
-                tmp_dir='.',
-                lib_dir=str(tmp_dir),
+                tmp_dir=str(work_dir),
+                lib_dir=str(work_dir),
                 user_filename=r'c:\anything\{}.anysuffix'.format(_TESTS[b][0]),
                 arguments=_TESTS[b][1],
             )
-            _remove_dir(tmp_dir)
             assert not error, \
                 '{}: should import with an error: {}'.format(base_py, error)
             actual['version'] = 'IGNORE-VALUE'
             assert not error, \
                 '{}: should be valid input'.format(base_py)
             pkunit.assert_object_with_json(b, actual)
-
-
-def _create_tmp_dir():
-    tmp_dir = py.path.local('/tmp').join(str(uuid.uuid4()))
-    _remove_dir(tmp_dir)
-    tmp_dir.mkdir()
-    for f in glob.glob(str(_DAT_DIR.join('mirror_*d.dat'))):
-        shutil.copy2(f, str(tmp_dir))
-    return tmp_dir
-
-
-def _remove_dir(tmp_dir):
-    try:
-        tmp_dir.remove(ignore_errors=True)
-    except:
-        pass
