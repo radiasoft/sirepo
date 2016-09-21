e1aedd28744b51171aac6736b50d73c57dd81988
robnagler <github@q33.us>
2016-09-21 21:35:09 +0000

    cleaned up file handling and fpc: remove exit

--- test.sh
+++ test.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
+#
+# Check for debug statements and run all tests
+#
 set -e
+
 assert_no_prints() {
     local pat=$1
-    local files=$2
-    #POSIT: No spaces in file names
-    if egrep "$pat" $files; then
+    shift
+    local files=( $@ )
+    if egrep "$pat" ${files[@]}; then
         echo "$pat: remove all debugging calls" 1>&2
         exit 1
     fi
 }
-assert_no_prints '\s(pkdp|print)\(' "$(find sirepo -name \*.py)"
-assert_no_prints '\s(srdbg|console.log)\(' "$(echo sirepo/package_data/static/js/*.js)"
-exit
-set -x
-./node_modules/jshint/bin/jshint --config=etc/jshint.conf sirepo/package_data/static/js/*.js
+
+pyfiles=( $(find sirepo -name \*.py | sort) )
+assert_no_prints '\s(pkdp|print)\(' "${pyfiles[@]}"
+jsfiles=( sirepo/package_data/static/js/*.js )
+assert_no_prints '\s(srdbg|console.log)\(' "${jsfiles[@]}"
+./node_modules/jshint/bin/jshint --config=etc/jshint.conf "${jsfiles[@]}"
 if [[ $PKSETUP_PYPI_PASSWORD ]]; then
     python setup.py pkdeploy
 else
