e9fbb8f155d10d0988e8b27ba1a5869340bd7c2c
Paul Moeller <moeller@bivio.biz>
2016-09-27 22:14:59 +0000

    fixed typo in error message

--- sirepo/server.py
+++ sirepo/server.py
@@ -622,9 +622,9 @@ def _cfg_job_queue(value):
         err = None
         try:
             if not celery_tasks.celery.control.ping():
-                err = 'You need to start Celery:\nsirepo server celery'
+                err = 'You need to start Celery:\nsirepo service celery'
         except Exception:
-            err = 'You need to start Rabbit:\nsirepo server rabbitmq'
+            err = 'You need to start Rabbit:\nsirepo service rabbitmq'
         if err:
             #TODO(robnagler) really should be pkconfig.Error() or something else
             # but this prints a nice message. Don't call sys.exit, not nice
