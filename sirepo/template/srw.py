1f738760e8111fc1967cca41410b93e38ed26c2a
Maksim Rakitin <maxim.rakitin@gmail.com>
2016-12-02 23:41:29 +0000

    #564 - Fixed error with copying a wrongly formatted simulation.

--- sirepo/template/srw.py
+++ sirepo/template/srw.py
@@ -916,7 +916,7 @@ def _copy_lib_files(data, source_lib, target):
             field_type = _SCHEMA['model'][model['type']][f][1]
             if model[f] and (field_type in ['MirrorFile', 'ImageFile']):
                 if field_type == 'ImageFile':
-                    if re.search('watchpointReport', data['report']) or data['report'] == 'multiElectronAnimation':
+                    if 'report' in data and (re.search('watchpointReport', data['report']) or data['report'] == 'multiElectronAnimation'):
                         filename = os.path.splitext(os.path.basename(str(model[f])))[0]
                         # Save the processed file:
                         srwl_samples.SRWLSamples(file_path=str(source_lib.join(model[f])), is_save_images=True, prefix=filename)
