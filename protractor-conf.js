fca5a2c6d88c7c80ee3b708e4045af76d29454b1
robnagler <github@q33.us>
2016-12-30 23:04:45 +0000

    disable animations in protractor

--- protractor-conf.js
+++ protractor-conf.js
@@ -97,10 +97,34 @@ exports.config = {
     },
 
     onPrepare: function () {
-
         // For Jasmine V2 a reporter needs to be added to be able to access the suite/spec names
         var $protractorSnapshot = require('protractor-snapshot');
         $protractorSnapshot.addReporter();
-
+        // Disable animations for better reliability: https://gist.github.com/ariel-symphony/4acfc04813c89d60e7a4
+        var disableNgAnimate = function() {
+            angular
+                .module('disableNgAnimate', [])
+                .run(['$animate', function($animate) {
+                    $animate.enabled(false);
+                }]);
+        };
+        var disableCssAnimate = function() {
+            angular
+                .module('disableCssAnimate', [])
+                .run(function() {
+                    var style = document.createElement('style');
+                    style.type = 'text/css';
+                    style.innerHTML = '* {' +
+                        '-webkit-transition: none !important;' +
+                        '-moz-transition: none !important' +
+                        '-o-transition: none !important' +
+                        '-ms-transition: none !important' +
+                        'transition: none !important' +
+                        '}';
+                    document.getElementsByTagName('head')[0].appendChild(style);
+                });
+        };
+        browser.addMockModule('disableNgAnimate', disableNgAnimate);
+        browser.addMockModule('disableCssAnimate', disableCssAnimate);
     }
 }
