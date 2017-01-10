217ce5b427498fcf4f48883a67afc6485fe3ba3b
robnagler <github@q33.us>
2017-01-10 22:44:01 +0000

    need to skip https://travis-ci.org/radiasoft/sirepo/builds/190757567

--- tests/exporter_test.py
+++ tests/exporter_test.py
@@ -6,33 +6,40 @@ u"""?
 """
 from __future__ import absolute_import, division, print_function
 import pytest
-from pykern import pkunit
 
+pytest.importorskip('srwl_bl')
 
 def test_create_zip():
     from pykern.pkdebug import pkdp, pkdpretty
     from pykern.pkunit import pkfail, pkok
+    from pykern import pkio
     from sirepo import sr_unit
     import copy
     import zipfile
 
     fc = sr_unit.flask_client()
-    sim_type = 'srw'
-    sim_id = fc.sr_sim_data(sim_type, 'Tabulated Undulator Example')['models']['simulation']['simulationId']
-    res = fc.sr_get_raw(
-        'exportSimulation',
-        {
-            'simulation_type': sim_type,
-            'simulation_id': sim_id,
-            'filename': 'anything.zip',
-        },
-    )
-    fn = str(pkunit.work_dir().join('foo.zip'))
-    with open(fn, 'wb') as f:
-        f.write(res)
-    z = zipfile.ZipFile(fn)
-    nl = sorted(z.namelist())
-    pkok(
-        nl == ['magnetic_measurements.zip', 'sirepo-data.json'],
-        '{}: zip namelist unexpected',
-    )
+    for sim_type, sim_name, expect in (
+        ('srw', 'Tabulated Undulator Example', ['magnetic_measurements.zip', 'sirepo-data.json']),
+        ('elegant', 'bunchComp - fourDipoleCSR', ['magnetic_measurements.zip', 'sirepo-data.json']),
+    ):
+        sim_id = fc.sr_sim_data(sim_type, sim_name)['models']['simulation']['simulationId']
+        res = fc.sr_get_raw(
+            'exportSimulation',
+            {
+                'simulation_type': sim_type,
+                'simulation_id': sim_id,
+                'filename': 'anything.zip',
+            },
+        )
+        with pkio.save_chdir(pkunit.work_dir()):
+            fn = 'foo.zip'
+            with open(fn, 'wb') as f:
+                f.write(res)
+            z = zipfile.ZipFile(fn)
+            nl = sorted(z.namelist())
+            pkok(
+                nl == expect,
+                '{}: zip namelist incorrect, expect={}',
+                nl,
+                expect,
+            )
