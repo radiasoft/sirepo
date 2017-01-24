2ce5250b3b6377eb97b5e1aa9219cc0d0fdec979
robnagler <github@q33.us>
2017-01-24 22:25:54 +0000

    Simple sharing link in title (#374)
    Need to make easier to copy (popup)

--- sirepo/package_data/static/js/sirepo-components.js
+++ sirepo/package_data/static/js/sirepo-components.js
@@ -984,7 +984,7 @@ SIREPO.app.directive('reportPanel', function(appState) {
     };
 });
 
-SIREPO.app.directive('appHeaderLeft', function(panelState) {
+SIREPO.app.directive('appHeaderLeft', function(panelState, appState, requestSender) {
     return {
         restrict: 'A',
         scope: {
@@ -994,7 +994,7 @@ SIREPO.app.directive('appHeaderLeft', function(panelState) {
             '<ul class="nav navbar-nav" data-ng-if="showMenu()">',
               '<li data-ng-class="{active: nav.isActive(\'simulations\')}"><a href data-ng-click="nav.openSection(\'simulations\')"><span class="glyphicon glyphicon-th-list"></span> Simulations</a></li>',
             '</ul>',
-            '<div data-ng-if="showTitle()" class="navbar-text"><a href data-ng-click="showSimulationModal()"><span data-ng-if="nav.sectionTitle()" class="glyphicon glyphicon-pencil"></span> <strong data-ng-bind="nav.sectionTitle()"></strong></a></div>',
+            '<div data-ng-if="showTitle()" class="navbar-text"><a href data-ng-click="showSimulationModal()"><span data-ng-if="nav.sectionTitle()" class="glyphicon glyphicon-pencil"></span> <strong data-ng-bind="nav.sectionTitle()"></strong></a> <a href="{{ nav.sectionURL() }}" class="glyphicon glyphicon-link"></a></div>',
         ].join(''),
         controller: function($scope) {
             $scope.showMenu = function() {
