5f1c2cb73b5a2cd334dbb1a968ad250edf427b61
Paul Moeller <moeller@bivio.biz>
2017-03-23 23:12:07 +0000

    added beamHistogramReport for 2d plots

--- sirepo/pkcli/hellweg2d.py
+++ sirepo/pkcli/hellweg2d.py
@@ -11,9 +11,10 @@ from rslinac.solver import BeamSolver
 from sirepo import simulation_db
 from sirepo.template import template_common, hellweg2d_dump_reader
 import numpy
+import sirepo.template.hellweg2d as template
 
 _HELLWEG2D_DUMP_FILE = 'all-data.bin'
-
+_SCHEMA = simulation_db.get_schema(template.SIM_TYPE)
 
 def run(cfg_dir):
     """Run Hellweg2D in ``cfg_dir``
@@ -24,24 +25,49 @@ def run(cfg_dir):
     with pkio.save_chdir(cfg_dir):
         _run_hellweg2d()
         beam_info = hellweg2d_dump_reader.beam_info(_HELLWEG2D_DUMP_FILE, 0)
-        data_list = [
-            hellweg2d_dump_reader.get_points(beam_info, 'x'),
-            hellweg2d_dump_reader.get_points(beam_info, 'y'),
-        ]
-        hist, edges = numpy.histogramdd(data_list, template_common.histogram_bins('100'))
-        res = {
-            'x_range': [float(edges[0][0]), float(edges[0][-1]), len(hist)],
-            'y_range': [float(edges[1][0]), float(edges[1][-1]), len(hist[0])],
-            'x_label': 'X [m]',
-            'y_label': 'Y [m]',
-            'title': 'X-Y Plot',
-            'z_matrix': hist.T.tolist(),
-        }
+        data = simulation_db.read_json(template_common.INPUT_BASE_NAME)
+        report = data['models'][data['report']]
+        res = None
+        if data['report'] == 'beamReport':
+            x, y = report.reportType.split('-')
+            data_list = [
+                hellweg2d_dump_reader.get_points(beam_info, x),
+                hellweg2d_dump_reader.get_points(beam_info, y),
+            ]
+            hist, edges = numpy.histogramdd(data_list, template_common.histogram_bins(report.histogramBins))
+            res = {
+                'x_range': [float(edges[0][0]), float(edges[0][-1]), len(hist)],
+                'y_range': [float(edges[1][0]), float(edges[1][-1]), len(hist[0])],
+                'x_label': hellweg2d_dump_reader.get_label(x),
+                'y_label': hellweg2d_dump_reader.get_label(y),
+                'title': _report_title(report.reportType, _SCHEMA['enum']['BeamReportType']),
+                'z_matrix': hist.T.tolist(),
+            }
+        elif data['report'] == 'beamHistogramReport':
+            points = hellweg2d_dump_reader.get_points(beam_info, report.reportType)
+            hist, edges = numpy.histogram(points, template_common.histogram_bins(report.histogramBins))
+            res = {
+                'title': _report_title(report.reportType, _SCHEMA['enum']['BeamHistogramReportType']),
+                'x_range': [edges[0], edges[-1]],
+                'y_label': 'Number of Particles',
+                'x_label': hellweg2d_dump_reader.get_label(report.reportType),
+                'points': hist.T.tolist(),
+                'frameCount': 1,
+            }
+        else:
+            raise RuntimeError('unknown report: {}'.format(data['report']))
         simulation_db.write_result(res)
 
 
 def run_background(cfg_dir):
     pass
+
+
+def _report_title(report_type, enum_values):
+    for e in enum_values:
+        if e[0] == report_type:
+            return e[1]
+    raise RuntimeError('unknown report type: {}'.format(report_type))
 
 
 def _run_hellweg2d(bunch_report=False, with_mpi=False):
--- sirepo/template/hellweg2d_dump_reader.py
+++ sirepo/template/hellweg2d_dump_reader.py
@@ -32,7 +32,29 @@ _PARTICLE_ACCESS = {
     'zrel': lambda p, lmb: lmb * p.phi / (2 * math.pi),
     'z0': lambda p, lmb: p.z,
     'beta': lambda p, lmb: p.beta0,
-    'w': lambda p, lmb: _We0 * (1 / math.sqrt(1 - p.beta0 ** 2) - 1) * 1e-6,
+    'w': lambda p, lmb: _velocity_to_mev(p.beta0),
+}
+
+_PARTICLE_LABEL = {
+    'r': 'r [m]',
+    'th': 'theta [deg]',
+    'x': 'x [m]',
+    'y': 'y [m]',
+    # 'br': '',
+    # 'bth': '',
+    # 'bx': '',
+    # 'by': '',
+    # 'bz': '',
+    'ar': 'r\' [rad]',
+    'ath': 'theta\' [rad]',
+    'ax': 'x\' [rad]',
+    'ay': 'y\' [rad]',
+    # 'az': '',
+    'phi': 'phi [deg]',
+    # 'zrel': '',
+    # 'z0': '',
+    # 'beta': '',
+    'w': 'W [eV]',
 }
 
 class THeader(ctypes.Structure):
@@ -103,6 +125,11 @@ def beam_info(filename, idx):
     assert 'Structure' in info
     return info
 
+
+def get_label(field):
+    return _PARTICLE_LABEL[field]
+
+
 def get_points(info, field):
     res = []
     fn = _PARTICLE_ACCESS[field]
@@ -113,3 +140,15 @@ def get_points(info, field):
             continue
         res.append(fn(p, lmb))
     return res
+
+
+def _gamma_to_mev(g):
+    return _We0 * (g - 1) * 1e-6
+
+
+def _velocity_to_energy(b):
+    return 1 / math.sqrt(1 - b ** 2)
+
+
+def _velocity_to_mev(b):
+    return _gamma_to_mev(_velocity_to_energy(b))
