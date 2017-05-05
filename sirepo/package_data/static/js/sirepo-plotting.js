e75f4d7072e00ec752f0c7e011429cdb1d6accbc
Paul Moeller <moeller@bivio.biz>
2017-05-05 21:46:42 +0000

    added jet colormap, moved particle and parameterPlot plots from hellweg to sirepo-plotting

--- sirepo/package_data/static/js/sirepo-plotting.js
+++ sirepo/package_data/static/js/sirepo-plotting.js
@@ -9,7 +9,8 @@ SIREPO.app.factory('plotting', function(appState, d3Service, frameCache, panelSt
     var MAX_PLOTS = 11;
     var COLOR_MAP = {
         grayscale: ['#333', '#fff'],
-        afmhot: colorsFromString('0000000200000400000600000800000a00000c00000e00001000001200001400001600001800001a00001c00001e00002000002200002400002600002800002a00002c00002e00003000003200003400003600003800003a00003c00003e00004000004200004400004600004800004a00004c00004e00005000005200005400005600005800005a00005c00005e00006000006200006400006600006800006a00006c00006e00007000007200007400007600007800007a00007c00007e0000800000810200830400850600870800890a008b0c008d0e008f1000911200931400951600971800991a009b1c009d1e009f2000a12200a32400a52600a72800a92a00ab2c00ad2e00af3000b13200b33400b53600b73800b93a00bb3c00bd3e00bf4000c14200c34400c54600c74800c94a00cb4c00cd4e00cf5000d15200d35400d55600d75800d95a00db5c00dd5e00df6000e16200e36400e56600e76800e96a00eb6c00ed6e00ef7000f17200f37400f57600f77800f97a00fb7c00fd7e00ff8000ff8102ff8304ff8506ff8708ff890aff8b0cff8d0eff8f10ff9112ff9314ff9516ff9718ff991aff9b1cff9d1eff9f20ffa122ffa324ffa526ffa728ffa92affab2cffad2effaf30ffb132ffb334ffb536ffb738ffb93affbb3cffbd3effbf40ffc142ffc344ffc546ffc748ffc94affcb4cffcd4effcf50ffd152ffd354ffd556ffd758ffd95affdb5cffdd5effdf60ffe162ffe364ffe566ffe768ffe96affeb6cffed6effef70fff172fff374fff576fff778fff97afffb7cfffd7effff80ffff81ffff83ffff85ffff87ffff89ffff8bffff8dffff8fffff91ffff93ffff95ffff97ffff99ffff9bffff9dffff9fffffa1ffffa3ffffa5ffffa7ffffa9ffffabffffadffffafffffb1ffffb3ffffb5ffffb7ffffb9ffffbbffffbdffffbfffffc1ffffc3ffffc5ffffc7ffffc9ffffcbffffcdffffcfffffd1ffffd3ffffd5ffffd7ffffd9ffffdbffffddffffdfffffe1ffffe3ffffe5ffffe7ffffe9ffffebffffedffffeffffff1fffff3fffff5fffff7fffff9fffffbfffffd'),
+        afmhot: colorsFromString('0000000200000400000600000800000a00000c00000e00001000001200001400001600001800001a00001c00001e00002000002200002400002600002800002a00002c00002e00003000003200003400003600003800003a00003c00003e00004000004200004400004600004800004a00004c00004e00005000005200005400005600005800005a00005c00005e00006000006200006400006600006800006a00006c00006e00007000007200007400007600007800007a00007c00007e00008000008202008404008607008808008a0a008c0d008e0f009010009212009414009617009818009a1a009c1d009e1f00a02000a22200a42400a62700a82800aa2a00ac2d00ae2f00b03000b23200b43400b63700b83800ba3a00bc3d00be3f00c04000c24200c44400c64600c84800ca4a00cc4d00ce4e00d05000d25200d45400d65600d85800da5a00dc5d00de5e00e06000e26200e46400e66600e86800ea6a00ec6d00ee6e00f07000f27200f47400f67600f87800fa7a00fc7d00fe7e00ff8001ff8203ff8405ff8607ff8809ff8b0bff8c0dff8e0fff9011ff9213ff9415ff9617ff9919ff9b1bff9c1dff9e1fffa021ffa223ffa425ffa627ffa829ffab2bffac2dffae2fffb031ffb233ffb435ffb637ffb939ffbb3bffbc3dffbe3fffc041ffc243ffc445ffc647ffc849ffcb4bffcc4dffce4fffd051ffd253ffd455ffd657ffd959ffdb5bffdc5dffde5fffe061ffe263ffe465ffe667ffe869ffeb6bffec6dffee6ffff071fff273fff475fff677fff979fffb7bfffc7dfffe7fffff81ffff83ffff85ffff87ffff89ffff8bffff8dffff8fffff91ffff93ffff95ffff97ffff99ffff9bffff9dffff9fffffa1ffffa3ffffa5ffffa7ffffa9ffffabffffadffffafffffb1ffffb3ffffb5ffffb7ffffb9ffffbbffffbdffffbfffffc1ffffc3ffffc5ffffc7ffffc9ffffcbffffcdffffcfffffd1ffffd3ffffd5ffffd7ffffd9ffffdbffffddffffdfffffe1ffffe3ffffe5ffffe7ffffe9ffffebffffedffffeffffff1fffff3fffff5fffff7fffff9fffffbfffffdffffff'),
+        jet: colorsFromString('00008000008400008900008d00009200009600009b00009f0000a40000a80000ad0000b20000b60000bb0000bf0000c40000c80000cd0000d10000d60000da0000df0000e30000e80000ed0000f10000f60000fa0000ff0000ff0000ff0000ff0000ff0004ff0008ff000cff0010ff0014ff0018ff001cff0020ff0024ff0028ff002cff0030ff0034ff0038ff003cff0040ff0044ff0048ff004cff0050ff0054ff0058ff005cff0060ff0064ff0068ff006cff0070ff0074ff0078ff007cff0080ff0084ff0088ff008cff0090ff0094ff0098ff009cff00a0ff00a4ff00a8ff00acff00b0ff00b4ff00b8ff00bcff00c0ff00c4ff00c8ff00ccff00d0ff00d4ff00d8ff00dcfe00e0fb00e4f802e8f406ecf109f0ee0cf4eb0ff8e713fce416ffe119ffde1cffdb1fffd723ffd426ffd129ffce2cffca30ffc733ffc436ffc139ffbe3cffba40ffb743ffb446ffb149ffad4dffaa50ffa753ffa456ffa05aff9d5dff9a60ff9763ff9466ff906aff8d6dff8a70ff8773ff8377ff807aff7d7dff7a80ff7783ff7387ff708aff6d8dff6a90ff6694ff6397ff609aff5d9dff5aa0ff56a4ff53a7ff50aaff4dadff49b1ff46b4ff43b7ff40baff3cbeff39c1ff36c4ff33c7ff30caff2cceff29d1ff26d4ff23d7ff1fdbff1cdeff19e1ff16e4ff13e7ff0febff0ceeff09f1fc06f4f802f8f500fbf100feed00ffea00ffe600ffe200ffde00ffdb00ffd700ffd300ffd000ffcc00ffc800ffc400ffc100ffbd00ffb900ffb600ffb200ffae00ffab00ffa700ffa300ff9f00ff9c00ff9800ff9400ff9100ff8d00ff8900ff8600ff8200ff7e00ff7a00ff7700ff7300ff6f00ff6c00ff6800ff6400ff6000ff5d00ff5900ff5500ff5200ff4e00ff4a00ff4700ff4300ff3f00ff3b00ff3800ff3400ff3000ff2d00ff2900ff2500ff2200ff1e00ff1a00ff1600ff1300fa0f00f60b00f10800ed0400e80000e40000df0000da0000d60000d10000cd0000c80000c40000bf0000bb0000b60000b20000ad0000a80000a400009f00009b00009600009200008d0000890000840000800000'),
         viridis: colorsFromString('44015444025645045745055946075a46085c460a5d460b5e470d60470e6147106347116447136548146748166848176948186a481a6c481b6d481c6e481d6f481f70482071482173482374482475482576482677482878482979472a7a472c7a472d7b472e7c472f7d46307e46327e46337f463480453581453781453882443983443a83443b84433d84433e85423f854240864241864142874144874045884046883f47883f48893e49893e4a893e4c8a3d4d8a3d4e8a3c4f8a3c508b3b518b3b528b3a538b3a548c39558c39568c38588c38598c375a8c375b8d365c8d365d8d355e8d355f8d34608d34618d33628d33638d32648e32658e31668e31678e31688e30698e306a8e2f6b8e2f6c8e2e6d8e2e6e8e2e6f8e2d708e2d718e2c718e2c728e2c738e2b748e2b758e2a768e2a778e2a788e29798e297a8e297b8e287c8e287d8e277e8e277f8e27808e26818e26828e26828e25838e25848e25858e24868e24878e23888e23898e238a8d228b8d228c8d228d8d218e8d218f8d21908d21918c20928c20928c20938c1f948c1f958b1f968b1f978b1f988b1f998a1f9a8a1e9b8a1e9c891e9d891f9e891f9f881fa0881fa1881fa1871fa28720a38620a48621a58521a68522a78522a88423a98324aa8325ab8225ac8226ad8127ad8128ae8029af7f2ab07f2cb17e2db27d2eb37c2fb47c31b57b32b67a34b67935b77937b87838b9773aba763bbb753dbc743fbc7340bd7242be7144bf7046c06f48c16e4ac16d4cc26c4ec36b50c46a52c56954c56856c66758c7655ac8645cc8635ec96260ca6063cb5f65cb5e67cc5c69cd5b6ccd5a6ece5870cf5773d05675d05477d1537ad1517cd2507fd34e81d34d84d44b86d54989d5488bd6468ed64590d74393d74195d84098d83e9bd93c9dd93ba0da39a2da37a5db36a8db34aadc32addc30b0dd2fb2dd2db5de2bb8de29bade28bddf26c0df25c2df23c5e021c8e020cae11fcde11dd0e11cd2e21bd5e21ad8e219dae319dde318dfe318e2e418e5e419e7e419eae51aece51befe51cf1e51df4e61ef6e620f8e621fbe723fde725'),
     };
 
@@ -1506,6 +1507,293 @@ SIREPO.app.directive('heatmap', function(plotting) {
                 if (imageObj) {
                     imageObj.onload = null;
                 }
+            };
+        },
+        link: function link(scope, element) {
+            plotting.linkPlot(scope, element);
+        },
+    };
+});
+
+//TODO(pjm): consolidate plot code with plotting service
+SIREPO.app.directive('parameterPlot', function(plotting) {
+    return {
+        restrict: 'A',
+        scope: {
+            modelName: '@',
+        },
+        templateUrl: '/static/html/plot2d.html' + SIREPO.SOURCE_CACHE_KEY,
+        controller: function($scope) {
+            var ASPECT_RATIO = 4.0 / 7;
+            $scope.margin = {top: 50, right: 20, bottom: 50, left: 70};
+            $scope.width = $scope.height = 0;
+            $scope.dataCleared = true;
+            var graphLine, xAxis, xAxisGrid, xAxisScale, xDomain, yAxis, yAxisGrid, yAxisScale, yDomain, y1Label, y2Label, zoom, xPoints;
+
+            function refresh() {
+                if (! xDomain) {
+                    return;
+                }
+                var xdom = xAxisScale.domain();
+                var zoomWidth = xdom[1] - xdom[0];
+
+                if (zoomWidth >= (xDomain[1] - xDomain[0])) {
+                    select('.overlay').attr('class', 'overlay mouse-zoom');
+                    xAxisScale.domain(xDomain);
+                    yAxisScale.domain(yDomain).nice();
+                }
+                else {
+                    select('.overlay').attr('class', 'overlay mouse-move-ew');
+                    if (xdom[0] < xDomain[0]) {
+                        xAxisScale.domain([xDomain[0], zoomWidth + xDomain[0]]);
+                    }
+                    if (xdom[1] > xDomain[1]) {
+                        xAxisScale.domain([xDomain[1] - zoomWidth, xDomain[1]]);
+                    }
+                }
+                resetZoom();
+                select('.overlay').call(zoom);
+                select('.x.axis').call(xAxis);
+                select('.x.axis.grid').call(xAxisGrid); // tickLine == gridline
+                select('.y.axis').call(yAxis);
+                select('.y.axis.grid').call(yAxisGrid);
+                select('.plot-viewport').selectAll('.line').attr('d', graphLine);
+            }
+
+            function resetZoom() {
+                zoom = d3.behavior.zoom()
+                    .x(xAxisScale)
+                    .on('zoom', refresh);
+            }
+
+            function select(selector) {
+                var e = d3.select($scope.element);
+                return selector ? e.select(selector) : e;
+            }
+
+            $scope.clearData = function() {
+                $scope.dataCleared = true;
+                xDomain = null;
+            };
+
+            $scope.destroy = function() {
+                zoom.on('zoom', null);
+                $('.overlay').off();
+            };
+
+            $scope.init = function() {
+                select('svg').attr('height', plotting.initialHeight($scope));
+                xAxisScale = d3.scale.linear();
+                yAxisScale = d3.scale.linear();
+                xAxis = plotting.createAxis(xAxisScale, 'bottom');
+                xAxis.tickFormat(plotting.fixFormat($scope, 'x'));
+                xAxisGrid = plotting.createAxis(xAxisScale, 'bottom');
+                yAxis = plotting.createAxis(yAxisScale, 'left');
+                yAxis.tickFormat(plotting.fixFormat($scope, 'y'));
+                yAxisGrid = plotting.createAxis(yAxisScale, 'left');
+                graphLine = d3.svg.line()
+                    .x(function(d, i) {
+                        return xAxisScale(xPoints[i]);
+                    })
+                    .y(function(d) {
+                        return yAxisScale(d);
+                    });
+                resetZoom();
+                // y1/y2 legend
+                select('svg')
+                    .append('circle').attr('class', 'line-y1').attr('r', 5).attr('cx', 8).attr('cy', 10);
+                y1Label = select('svg')
+                    .append('text').attr('class', 'focus-text').attr('x', 16).attr('y', 16);
+                select('svg')
+                    .append('circle').attr('class', 'line-y2').attr('r', 5).attr('cx', 8).attr('cy', 30);
+                y2Label = select('svg')
+                    .append('text').attr('class', 'focus-text').attr('x', 16).attr('y', 36);
+            };
+
+            $scope.load = function(json) {
+                $scope.dataCleared = false;
+                xPoints = json.x_points
+                    ? json.x_points
+                    : plotting.linspace(json.x_range[0], json.x_range[1], json.points.length);
+                $scope.xRange = json.x_range;
+                var xdom = [json.x_range[0], json.x_range[1]];
+                xDomain = xdom;
+                xAxisScale.domain(xdom);
+                yDomain = [json.y_range[0], json.y_range[1]];
+                yAxisScale.domain(yDomain).nice();
+                var viewport = select('.plot-viewport');
+                viewport.selectAll('.line').remove();
+                viewport.append('path').attr('class', 'line line-y1').datum(json.points[0]);
+                viewport.append('path').attr('class', 'line line-y2').datum(json.points[1]);
+                select('.y-axis-label').text(plotting.extractUnits($scope, 'y', json.y_label));
+                select('.x-axis-label').text(plotting.extractUnits($scope, 'x', json.x_label));
+                select('.main-title').text(json.title);
+                $scope.resize();
+                y1Label.text(json.y1_title);
+                y2Label.text(json.y2_title);
+            };
+
+            $scope.resize = function() {
+                var width = parseInt(select().style('width')) - $scope.margin.left - $scope.margin.right;
+                if (! xPoints || isNaN(width)) {
+                    return;
+                }
+                $scope.width = width;
+                $scope.height = ASPECT_RATIO * $scope.width;
+                select('svg')
+                    .attr('width', $scope.width + $scope.margin.left + $scope.margin.right)
+                    .attr('height', $scope.height + $scope.margin.top + $scope.margin.bottom);
+                plotting.ticks(xAxis, $scope.width, true);
+                plotting.ticks(xAxisGrid, $scope.width, true);
+                plotting.ticks(yAxis, $scope.height, false);
+                plotting.ticks(yAxisGrid, $scope.height, false);
+                xAxisScale.range([0, $scope.width]);
+                yAxisScale.range([$scope.height, 0]);
+                xAxisGrid.tickSize(-$scope.height);
+                yAxisGrid.tickSize(-$scope.width);
+                refresh();
+            };
+        },
+        link: function link(scope, element) {
+            plotting.linkPlot(scope, element);
+        },
+    };
+});
+
+//TODO(pjm): consolidate plot code with plotting service
+SIREPO.app.directive('particle', function(plotting) {
+    return {
+        restrict: 'A',
+        scope: {
+            modelName: '@',
+        },
+        templateUrl: '/static/html/plot2d.html' + SIREPO.SOURCE_CACHE_KEY,
+        controller: function($scope) {
+            var ASPECT_RATIO = 4.0 / 7;
+            $scope.margin = {top: 50, right: 20, bottom: 50, left: 70};
+            $scope.width = $scope.height = 0;
+            $scope.dataCleared = true;
+            var graphLine, xAxis, xAxisGrid, xAxisScale, xDomain, yAxis, yAxisGrid, yAxisScale, yDomain, zoom;
+
+            function refresh() {
+                if (! xDomain) {
+                    return;
+                }
+                var xdom = xAxisScale.domain();
+                var zoomWidth = xdom[1] - xdom[0];
+
+                if (zoomWidth >= (xDomain[1] - xDomain[0])) {
+                    select('.overlay').attr('class', 'overlay mouse-zoom');
+                    xAxisScale.domain(xDomain);
+                    yAxisScale.domain(yDomain).nice();
+                }
+                else {
+                    select('.overlay').attr('class', 'overlay mouse-move-ew');
+                    if (xdom[0] < xDomain[0]) {
+                        xAxisScale.domain([xDomain[0], zoomWidth + xDomain[0]]);
+                    }
+                    if (xdom[1] > xDomain[1]) {
+                        xAxisScale.domain([xDomain[1] - zoomWidth, xDomain[1]]);
+                    }
+                }
+                resetZoom();
+                select('.overlay').call(zoom);
+                select('.x.axis').call(xAxis);
+                select('.x.axis.grid').call(xAxisGrid); // tickLine == gridline
+                select('.y.axis').call(yAxis);
+                select('.y.axis.grid').call(yAxisGrid);
+                select('.plot-viewport').selectAll('.line').attr('d', graphLine);
+            }
+
+            function resetZoom() {
+                zoom = d3.behavior.zoom()
+                    .x(xAxisScale)
+                    .on('zoom', refresh);
+            }
+
+            function select(selector) {
+                var e = d3.select($scope.element);
+                return selector ? e.select(selector) : e;
+            }
+
+            $scope.clearData = function() {
+                $scope.dataCleared = true;
+                xDomain = null;
+            };
+
+            $scope.destroy = function() {
+                zoom.on('zoom', null);
+                $('.overlay').off();
+            };
+
+            $scope.init = function() {
+                select('svg').attr('height', plotting.initialHeight($scope));
+                xAxisScale = d3.scale.linear();
+                yAxisScale = d3.scale.linear();
+                xAxis = plotting.createAxis(xAxisScale, 'bottom');
+                xAxis.tickFormat(plotting.fixFormat($scope, 'x'));
+                xAxisGrid = plotting.createAxis(xAxisScale, 'bottom');
+                yAxis = plotting.createAxis(yAxisScale, 'left');
+                yAxis.tickFormat(plotting.fixFormat($scope, 'y'));
+                yAxisGrid = plotting.createAxis(yAxisScale, 'left');
+                graphLine = d3.svg.line()
+                    .x(function(d) {
+                        return xAxisScale(d[0]);
+                    })
+                    .y(function(d) {
+                        return yAxisScale(d[1]);
+                    });
+                resetZoom();
+            };
+
+            $scope.load = function(json) {
+                $scope.dataCleared = false;
+                // xPoints = json.x_points
+                //     ? json.x_points
+                //     : plotting.linspace(json.x_range[0], json.x_range[1], json.points.length);
+                $scope.xRange = json.x_range;
+                var xdom = [json.x_range[0], json.x_range[1]];
+                xDomain = xdom;
+                xAxisScale.domain(xdom);
+                yDomain = [json.y_range[0], json.y_range[1]];
+                yAxisScale.domain(yDomain).nice();
+                var viewport = select('.plot-viewport');
+                viewport.selectAll('.line').remove();
+                var isFixedX = ! Array.isArray(json.x_points[0]);
+                for (var i = 0; i < json.points.length; i++) {
+                    var p = d3.zip(
+                        isFixedX ? json.x_points : json.x_points[i],
+                        json.points[i]);
+                    viewport.append('path').attr('class', 'line line-7').datum(p);
+                }
+                // json.points.forEach(function(p) {
+                //     viewport.append('path').attr('class', 'line line-7').datum(p);
+                // });
+                select('.y-axis-label').text(plotting.extractUnits($scope, 'y', json.y_label));
+                select('.x-axis-label').text(plotting.extractUnits($scope, 'x', json.x_label));
+                select('.main-title').text(json.title);
+                $scope.resize();
+            };
+
+            $scope.resize = function() {
+                var width = parseInt(select().style('width')) - $scope.margin.left - $scope.margin.right;
+                if (! xDomain || isNaN(width)) {
+                    return;
+                }
+                $scope.width = width;
+                $scope.height = ASPECT_RATIO * $scope.width;
+                select('svg')
+                    .attr('width', $scope.width + $scope.margin.left + $scope.margin.right)
+                    .attr('height', $scope.height + $scope.margin.top + $scope.margin.bottom);
+                plotting.ticks(xAxis, $scope.width, true);
+                plotting.ticks(xAxisGrid, $scope.width, true);
+                plotting.ticks(yAxis, $scope.height, false);
+                plotting.ticks(yAxisGrid, $scope.height, false);
+                xAxisScale.range([0, $scope.width]);
+                yAxisScale.range([$scope.height, 0]);
+                xAxisGrid.tickSize(-$scope.height);
+                yAxisGrid.tickSize(-$scope.width);
+                refresh();
             };
         },
         link: function link(scope, element) {
