66d8863f4fa78c6abf6b819ab1ec14ed4f0cfba0
robnagler <github@q33.us>
2017-06-21 03:26:33 +0000

    Rename old warppba examples and update example link (#773)

--- sirepo/pkcli/db.py
+++ sirepo/pkcli/db.py
@@ -12,8 +12,25 @@ def upgrade():
     from pykern import pkio
     from sirepo import simulation_db
     from sirepo import server
+    import re
+
+    def _inc(m):
+        return m.group(1) + str(int(m.group(2)) + 1)
 
     server.init()
-    to_rename = []
-    for d in pkio.sorted_glob(simulation_db.user_dir_name().join('*/fete')):
-        d.rename(d.new(basename='warpvnd'))
+    for d in pkio.sorted_glob(simulation_db.user_dir_name().join('*/warppba')):
+        for fn in pkio.sorted_glob(d.join('*/sirepo-data.json')):
+            with open(str(fn)) as f:
+                t = f.read()
+            for old, new in (
+                ('"WARP example laser simulation"', '"Laser-Plasma Wakefield"'),
+                ('"Laser Pulse"', '"Laser-Plasma Wakefield"'),
+                ('"WARP example electron beam simulation"', '"Electron Beam"'),
+            ):
+                if not old in t:
+                    continue
+                t = t.replace(old, new)
+                t = re.sub(r'(simulationSerial":\s+)(\d+)', _inc, t)
+                break
+            with open(str(fn), 'w') as f:
+                f.write(t)
