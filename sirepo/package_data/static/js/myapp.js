b159324d4597f4c9a09b4d376c3d2eed556b0b8f
Paul Moeller <moeller@bivio.biz>
2017-08-04 16:57:39 +0000

    restored myapp changes

---
+++ sirepo/package_data/static/js/myapp.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var srlog = SIREPO.srlog;
+var srdbg = SIREPO.srdbg;
+
+SIREPO.app.config(function($routeProvider, localRoutesProvider) {
+    if (SIREPO.IS_LOGGED_OUT) {
+        return;
+    }
+    var localRoutes = localRoutesProvider.$get();
+    $routeProvider
+        .when(localRoutes.source, {
+            controller: 'MyAppSourceController as source',
+            templateUrl: '/static/html/myapp-source.html' + SIREPO.SOURCE_CACHE_KEY,
+        });
+});
+
+SIREPO.app.controller('MyAppSourceController', function () {
+    var self = this;
+});
+
+//TODO: too much boilerplate in the appHeader, move common parts to sirepo-components.js
+SIREPO.app.directive('appHeader', function(appState, panelState) {
+    return {
+        restrict: 'A',
+        scope: {
+            nav: '=appHeader',
+        },
+        template: [
+            '<div class="navbar-header">',
+              '<a class="navbar-brand" href="/#about"><img style="width: 40px; margin-top: -10px;" src="/static/img/radtrack.gif" alt="radiasoft"></a>',
+              '<div class="navbar-brand"><a href data-ng-click="nav.openSection(\'simulations\')">MyApp</a></div>',
+            '</div>',
+            '<div data-app-header-left="nav"></div>',
+            '<ul class="nav navbar-nav navbar-right" data-login-menu=""></ul>',
+            '<ul class="nav navbar-nav navbar-right" data-ng-show="isLoaded()">',
+              '<li data-ng-class="{active: nav.isActive(\'source\')}"><a href data-ng-click="nav.openSection(\'source\')"><span class="glyphicon glyphicon-flash"></span> Source</a></li>',
+            '</ul>',
+            '<ul class="nav navbar-nav navbar-right" data-ng-show="nav.isActive(\'simulations\')">',
+              '<li><a href data-ng-click="showSimulationModal()"><span class="glyphicon glyphicon-plus sr-small-icon"></span><span class="glyphicon glyphicon-file"></span> New Simulation</a></li>',
+              '<li><a href data-ng-click="showNewFolderModal()"><span class="glyphicon glyphicon-plus sr-small-icon"></span><span class="glyphicon glyphicon-folder-close"></span> New Folder</a></li>',
+            '</ul>',
+        ].join(''),
+        controller: function($scope) {
+            $scope.isLoaded = function() {
+                if ($scope.nav.isActive('simulations')) {
+                    return false;
+                }
+                return appState.isLoaded();
+            };
+            $scope.showNewFolderModal = function() {
+                panelState.showModalEditor('simulationFolder');
+            };
+            $scope.showSimulationModal = function() {
+                panelState.showModalEditor('simulation');
+            };
+        },
+    };
+});
