26fe0c0c194448f664ae2b83bfe21b05a1368a8e
robnagler <github@q33.us>
2017-12-06 02:57:53 +0000

    can't have a print() Need to write a "safe" print routine for prompting user

--- sirepo/pkcli/service.py
+++ sirepo/pkcli/service.py
@@ -78,7 +78,6 @@ def nginx_proxy():
     """
     assert pkconfig.channel_in('dev')
     run_dir = _run_dir().join('nginx_proxy').ensure(dir=True)
-    print('Connect http://localhost:8080')
     with pkio.save_chdir(run_dir):
         f = run_dir.join('default.conf')
         values = dict(pkcollections.map_items(cfg))
