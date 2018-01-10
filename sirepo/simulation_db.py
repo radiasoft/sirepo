bc292ffab57d72df17b076a5a2aa991eab24d953
Paul Moeller <moeller@radiasoft.net>
2018-01-10 23:36:54 +0000

    don't validate new example names, avoids a call to iterate_simulation_datafiles()

--- sirepo/simulation_db.py
+++ sirepo/simulation_db.py
@@ -670,14 +670,14 @@ def report_info(data):
 
 def save_new_example(data):
     data.models.simulation.isExample = True
-    return save_new_simulation(fixup_old_data(data)[0])
+    return save_new_simulation(fixup_old_data(data)[0], do_validate=False)
 
 
-def save_new_simulation(data):
+def save_new_simulation(data, do_validate=True):
     d = simulation_dir(data.simulationType)
     sid = _random_id(d, data.simulationType).id
     data.models.simulation.simulationId = sid
-    return save_simulation_json(data)
+    return save_simulation_json(data, do_validate=do_validate)
 
 
 def save_simulation_json(data, do_validate=True):
