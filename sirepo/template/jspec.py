13c9e3d23771dadcbd9bda25f079f5101860dfca
Paul Moeller <moeller@radiasoft.net>
2018-04-09 21:55:25 +0000

    jspec: protect against nan values which can not get converted to json

--- sirepo/template/jspec.py
+++ sirepo/template/jspec.py
@@ -13,6 +13,7 @@ from pykern.pkdebug import pkdc, pkdp
 from sirepo import simulation_db
 from sirepo.template import template_common, sdds_util
 import glob
+import math
 import numpy as np
 import os.path
 import py.path
@@ -283,10 +284,10 @@ def _compute_sdds_range(res):
     for field in res:
         values = sdds.sddsdata.GetColumn(sdds_index, column_names.index(field))
         if len(res[field]):
-            res[field][0] = min(min(values), res[field][0])
-            res[field][1] = max(max(values), res[field][0])
+            res[field][0] = _safe_sdds_value(min(min(values), res[field][0]))
+            res[field][1] = _safe_sdds_value(max(max(values), res[field][0]))
         else:
-            res[field] = [min(values), max(values)]
+            res[field] = [_safe_sdds_value(min(values)), _safe_sdds_value(max(values))]
 
 
 def _elegant_dir():
@@ -452,3 +453,9 @@ def _prepare_twiss_file(data, run_dir):
         if not f.exists():
             raise RuntimeError('elegant twiss output unavailable. Run elegant simulation.')
         f.copy(run_dir)
+
+
+def _safe_sdds_value(v):
+    if isinstance(v, float) and (math.isinf(v) or math.isnan(v)):
+        return 0
+    return v
