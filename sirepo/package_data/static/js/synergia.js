7bb761eee24e291873d7689534c90b82c100beb2
Paul Moeller <moeller@radiasoft.net>
2018-04-25 16:49:30 +0000

    initial synergia implementation

---
+++ sirepo/package_data/static/js/synergia.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var srlog = SIREPO.srlog;
+var srdbg = SIREPO.srdbg;
+
+SIREPO.appLocalRoutes.lattice = '/lattice/:simulationId';
+
+SIREPO.app.config(function($routeProvider, localRoutesProvider) {
+    if (SIREPO.IS_LOGGED_OUT) {
+        return;
+    }
+    var localRoutes = localRoutesProvider.$get();
+    $routeProvider
+        .when(localRoutes.source, {
+            controller: 'SynergiaSourceController as source',
+            templateUrl: '/static/html/synergia-source.html' + SIREPO.SOURCE_CACHE_KEY,
+        })
+        .when(localRoutes.lattice, {
+            controller: 'LatticeController as lattice',
+            templateUrl: '/static/html/synergia-lattice.html' + SIREPO.SOURCE_CACHE_KEY,
+        });
+});
+
+SIREPO.app.controller('LatticeController', function(latticeService) {
+    var self = this;
+    self.latticeService = latticeService;
+    self.activeTab = 'basic';
+
+    self.advancedNames = [];
+
+    self.basicNames = [
+        'DRIFT', 'SBEN',
+    ];
+
+    self.createElement = latticeService.createElement;
+
+    self.elementColor = {};
+
+    self.elementPic = {
+        bend: ['SBEN'],
+        drift: ['DRIFT'],
+    };
+
+    self.titleForName = function(name) {
+        return SIREPO.APP_SCHEMA.view[name].description;
+    };
+});
+
+SIREPO.app.controller('SynergiaSourceController', function () {
+    var self = this;
+});
+
+SIREPO.app.directive('appHeader', function(appState, panelState) {
+    return {
+        restrict: 'A',
+        scope: {
+            nav: '=appHeader',
+        },
+        template: [
+            '<div data-app-header-brand="nav"></div>',
+            '<div data-app-header-left="nav"></div>',
+            '<div data-app-header-right="nav">',
+              '<app-header-right-sim-loaded>',
+		'<div data-sim-sections="">',
+                  '<li class="sim-section" data-ng-class="{active: nav.isActive(\'source\')}"><a href data-ng-click="nav.openSection(\'source\')"><span class="glyphicon glyphicon-flash"></span> Source</a></li>',
+                  '<li class="sim-section" data-ng-class="{active: nav.isActive(\'lattice\')}"><a data-ng-href="{{ nav.sectionURL(\'lattice\') }}"><span class="glyphicon glyphicon-option-horizontal"></span> Lattice</a></li>',
+                '</div>',
+              '</app-header-right-sim-loaded>',
+              '<app-settings>',
+		//  '<div>App-specific setting item</div>',
+	      '</app-settings>',
+              '<app-header-right-sim-list>',
+              '</app-header-right-sim-list>',
+            '</div>',
+	].join(''),
+    };
+});
---
+++ sirepo/pkcli/synergia.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""Wrapper to run myapp from the command line.
+
+:copyright: Copyright (c) 2018 RadiaSoft LLC.  All Rights Reserved.
+:license: http://www.apache.org/licenses/LICENSE-2.0.html
+"""
+from __future__ import absolute_import, division, print_function
+from pykern import pkio
+from pykern.pkdebug import pkdp, pkdc
+from sirepo import simulation_db
+from sirepo.template import template_common
+import numpy as np
+import sirepo.template.synergia as template
+
+_SCHEMA = simulation_db.get_schema(template.SIM_TYPE)
+
+
+def run(cfg_dir):
+    with pkio.save_chdir(cfg_dir):
+        exec(pkio.read_text(template_common.PARAMETERS_PYTHON_FILE), locals(), locals())
+    data = simulation_db.read_json(template_common.INPUT_BASE_NAME)
+    if data['report'] == 'bunchReport':
+        res = _run_bunch_report(data)
+    else:
+        raise RuntimeError('unknown report: {}'.format(data['report']))
+    simulation_db.write_result(res)
+
+
+def _run_bunch_report(data):
+    report = data.models[data['report']]
+    from synergia.bunch import Bunch, populate_6d
+    from synergia.foundation import Four_momentum, Reference_particle, Random_distribution, pconstants
+    from synergia.utils import Commxx
+    import numpy as np
+
+    fm = Four_momentum(pconstants.mp)
+    fm.set_momentum(0.0685)
+    ref = Reference_particle(pconstants.proton_charge, fm)
+    comm = Commxx()
+    bunch = Bunch(ref, 50000, 1.5e+8, comm)
+    bunch.set_z_period_length(0.0673)
+    means = np.array([0, 0, 0, 0, 0, 0])
+    covariance_matrix = np.array([
+        [3.33389840e-06, 1.98159549e-19, 0, 0, 0, 0],
+        [1.98159549e-19, 4.56222662e-06, 0, 0, 0, 0],
+        [0, 0, 4.33096069e-06, -6.34828466e-20, 0, 0],
+        [0, 0, -6.34828466e-20, 3.51192289e-06, 0, 0],
+        [0, 0, 0, 0, 2.44449997e-03, 0],
+        [0, 0, 0, 0, 0, 1.00000000e-06],
+    ])
+    dist = Random_distribution(1415926, comm)
+    populate_6d(dist, bunch, means, covariance_matrix)
+    particles = bunch.get_local_particles()
+    x = particles[:, getattr(bunch, report['x'])]
+    y = particles[:, getattr(bunch, report['y'])]
+    hist, edges = np.histogramdd([x, y], template_common.histogram_bins(200))
+    return {
+        'x_range': [float(edges[0][0]), float(edges[0][-1]), len(hist)],
+        'y_range': [float(edges[1][0]), float(edges[1][-1]), len(hist[0])],
+        'x_label': _label(report['x']),
+        'y_label': _label(report['y']),
+        'title': '{}-{}'.format(report['x'], report['y']),
+        'z_matrix': hist.T.tolist(),
+    }
+
+
+def _label(v):
+    return '{} [m]'.format(v)
