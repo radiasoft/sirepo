86a2c70afc98f21db1ba9c4e9ec273f0bf23c935
Paul Moeller <moeller@radiasoft.net>
2018-07-11 17:56:14 +0000

    #1251 added standard cookie footer to landing page app and main sirepo template

---
+++ sirepo/package_data/static/js/sirepo-common.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Common code shared between the landing page app and the various sirepo apps
+
+window.cookieconsent.initialise({
+    //TODO(pjm): set cookie domain?
+    content: {
+        //TODO(pjm): update with links to terms of service when available
+        message: 'This site uses cookies to deliver our services. By using our site, you acknowledge that you accept our use of cookies.',
+        dismiss: 'I accept',
+        link: null,
+    },
+    palette: {
+        popup: {
+            background: "#000",
+        },
+        button: {
+            background: "#286090",
+        },
+    },
+});
