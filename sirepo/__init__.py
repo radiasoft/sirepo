1ff6ea2ccca42b9c3a5bdfe2544bbeaac975dd64
robnagler <github@q33.us>
2018-10-12 20:03:28 +0000

    added sirepo.__version__

--- sirepo/__init__.py
+++ sirepo/__init__.py
@@ -1,1 +1,10 @@
-#
+# -*- coding: utf-8 -*-
+u"""sirepo package
+
+:copyright: Copyright (c) 2018 RadiaSoft LLC.  All Rights Reserved.
+:license: http://www.apache.org/licenses/LICENSE-2.0.html
+"""
+from __future__ import absolute_import, division, print_function
+import pkg_resources
+
+__version__ = pkg_resources.get_distribution('sirepo').version
