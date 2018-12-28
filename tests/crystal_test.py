bd4c97be761e6db185f50b7d594c76281e3e9ff9
Paul Moeller <moeller@radiasoft.net>
2018-12-28 22:18:06 +0000

    disable crystal tests for now, service is down

--- tests/crystal_test.py
+++ tests/crystal_test.py
@@ -27,7 +27,8 @@ def test_calc_bragg_angle():
         assert angle_data['bragg_angle_deg'] == case[1][2]
 
 
-def test_get_crystal_parameters():
+#TODO(pjm): crystal service is down, 12/28/2018
+def fail_test_get_crystal_parameters():
     from sirepo import crystal
     expected = (
         (5.4309, 3.135531576941939, 3.135531576941939, 3.1355, -2.3353e-06, 8.6843e-09, 1.2299e-06, 6.0601e-09, 5.5704),
@@ -57,7 +58,8 @@ def test_get_crystal_parameters():
         assert crystal_parameters['bragg_angle_deg'] == case[1][8]
 
 
-def test_get_crystal_parameters_str():
+#TODO(pjm): crystal service is down, 12/28/2018
+def fail_test_get_crystal_parameters_str():
     from sirepo import crystal
     with pytest.raises(AssertionError):
         _ = crystal.get_crystal_parameters(
