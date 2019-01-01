6c95a9889a78ead89fa9edb984686d6bfd0914a6
Nathaniel J. Smith <njs@pobox.com>
2019-01-01 01:41:26 -0800

    Incorporate review feedback

--- sirepo/runner/container.py
+++ sirepo/runner/container.py
@@ -1,75 +1,78 @@
+# -*- coding: utf-8 -*-
+u"""Run jobs in Docker.
+
+:copyright: Copyright (c) 2018-2019 RadiaSoft LLC.  All Rights Reserved.
+:license: http://www.apache.org/licenses/LICENSE-2.0.html
+"""
 from sirepo import runner, simulation_db
-from base64 import b64encode
-from contextlib import contextmanager
-from functools import partial
-import logging
-import json
-import trio
+import base64
+import contextlib
 import docker
+import functools
+import json
+import logging
+from pykern.pkdebug import pkdp, pkdc, pkdlog, pkdexc
 import redis
 import requests
-
-__all__ = ["serve", "ContainerJob"]
-
-# XX TODO: what's the right way to do logging in pykern/sirepo?
-log = logging.getLogger(__name__)
+import shlex
+import trio
 
 # The redis key we use for the request list
-REQUEST_LIST_KEY = "seq:job_queue"
+_REQUEST_LIST_KEY = 'seq:job_queue'
 # The redis key we use for the job status hash
-JOB_STATUS_HASH_KEY = "hash:job_status"
+_JOB_STATUS_HASH_KEY = 'hash:job_status'
 
 # How often threaded blocking operations should wake up and check for Trio
 # cancellation
-CANCEL_POLL_INTERVAL = 1
+_CANCEL_POLL_INTERVAL = 1
 
 # Global connection pools (don't worry, they don't actually make any
 # connections until we start using them)
-REDIS = redis.from_url("redis://localhost:6379/0")
-DOCKER = docker.from_env()
+_REDIS = redis.from_url('redis://localhost:6379/0')
+_DOCKER = docker.from_env()
 
 
-def encode_request(request):
-    return json.dumps(request).encode("utf-8")
+def _encode_request(request):
+    return json.dumps(request).encode('utf-8')
 
 
-def decode_request(request):
-    return json.loads(request.decode("utf-8"))
+def _decode_request(request):
+    return json.loads(request.decode('utf-8'))
 
 
-def submit_request(request):
-    print("submitting:", request)
-    REDIS.rpush(REQUEST_LIST_KEY, encode_request(request))
+def _submit_request(request):
+    pkdc('submitting request: {!r}', request)
+    _REDIS.rpush(_REQUEST_LIST_KEY, _encode_request(request))
 
 
-# XX TODO: how to integrate this properly into pykern/sirepo's logging system?
-@contextmanager
-def catch_and_log_errors(exc_type, msg, *args, **kwargs):
+@contextlib.contextmanager
+def _catch_and_log_errors(exc_type, msg, *args, **kwargs):
     try:
         yield
-    except exc_type as exc:
-        log.exception(msg, *args, **kwargs)
+    except exc_type:
+        pkdlog(msg, *args, **kwargs)
+        pkdlog(pkdexc())
 
 
 # Helper to call redis blpop in a async/cancellation-friendly way
-async def pop_encoded_request():
+async def _pop_encoded_request():
     while True:
         retval = await trio.run_sync_in_worker_thread(
-            partial(
-                REDIS.blpop, [REQUEST_LIST_KEY], timeout=CANCEL_POLL_INTERVAL
+            functools.partial(
+                _REDIS.blpop, [_REQUEST_LIST_KEY], timeout=_CANCEL_POLL_INTERVAL
             )
         )
         if retval is not None:
-            _, encoded_request = retval
-            return encoded_request
+            # it's a pair (key, popped value)
+            return retval[1]
 
 
 # Helper to call docker wait in a async/cancellation-friendly way
-async def container_wait(container):
+async def _container_wait(container):
     while True:
         try:
             return await trio.run_sync_in_worker_thread(
-                partial(container.wait, timeout=CANCEL_POLL_INTERVAL)
+                functools.partial(container.wait, timeout=_CANCEL_POLL_INTERVAL)
             )
         # ReadTimeout is what the documentation says this raises.
         # ConnectionError is what it actually raises.
@@ -79,59 +82,58 @@ async def container_wait(container):
 
 
 # The core request handler
-async def handle_request(encoded_request):
-    with catch_and_log_errors(Exception, "error handling request %r", encoded_request):
-        request = decode_request(encoded_request)
-        if request["type"] == "start":
-            jid = request["jid"]
-            # XX TODO should we do any sanitization here? e.g. specifying
-            # arbitrary volumes allows arbitrary access to the host. Maybe
-            # that's fine though.
-            image = request["image"]
-            command = request["command"]
-            working_dir = request["working_dir"]
-            # format: {host path: {"bind": container path, "mode": "rw"}}
-            volumes = request["volumes"]
+async def _handle_request(encoded_request):
+    with _catch_and_log_errors(Exception, 'error handling request {!r}', encoded_request):
+        request = _decode_request(encoded_request)
+        if request['type'] == 'start':
             # Technically a blocking call, but redis is fast and local so we
             # can get away with it
-            REDIS.hset(JOB_STATUS_HASH_KEY, jid, b"running")
+            _REDIS.hset(_JOB_STATUS_HASH_KEY, request['jid'], b'running')
             # Start the container
             container = await trio.run_sync_in_worker_thread(
-                partial(
-                    DOCKER.containers.run,
-                    image,
-                    command,
-                    working_dir=working_dir,
-                    volumes=volumes,
-                    name=jid,
+                functools.partial(
+                    _DOCKER.containers.run,
+                    request['image'],
+                    request['command'],
+                    working_dir=request['working_dir'],
+                    # {host path: {'bind': container path, 'mode': 'rw'}}
+                    volumes=request['volumes'],
+                    name=request['jid'],
                     auto_remove=True,
                     detach=True,
+                    init=True,
                 )
             )
-            # Returns a dict like: {"Error": None, "StatusCode": 0}
-            print(await container_wait(container))
+            # Returns a dict like: {'Error': None, 'StatusCode': 0}
+            # XX TODO but there may be a race condition where if the container
+            # finishes before our call to wait() starts, then it just errors
+            # out with docker.errors.NotFound
+            pkdp(await _container_wait(container))
             # Technically a blocking call
-            REDIS.hset(JOB_STATUS_HASH_KEY, jid, b"finished")
-        elif request["type"] == "cancel":
-            jid = request["jid"]
-            container = DOCKER.containers.get(jid)
+            _REDIS.hset(_JOB_STATUS_HASH_KEY, request['jid'], b'finished')
+        elif request['type'] == 'cancel':
+            # This could fail, if the container doesn't exist or has already
+            # stopped, but _catch_and_log_errors catch the issue. We might
+            # want to catch and ignore expected errors like this, though,
+            # rather than letting them escape to _catch_and_log_errors?
+            container = _DOCKER.containers.get(request['jid'])
             container.stop()
             # Doesn't update status, because that will happen automatically
             # when it actually stops...
         else:
-            raise RuntimeError(f"unknown request type: {request['type']!r}")
+            raise RuntimeError(f'unknown request type: {request["type"]!r}')
 
 
-async def main():
+async def _main():
     async with trio.open_nursery() as nursery:
         while True:
-            encoded_request = await pop_encoded_request()
-            print("handling request:", encoded_request)
-            nursery.start_soon(handle_request, encoded_request)
+            encoded_request = await _pop_encoded_request()
+            pkdc('handling request: {!r}', encoded_request)
+            nursery.start_soon(_handle_request, encoded_request)
 
 
 def serve():
-    trio.run(main)
+    trio.run(_main)
 
 
 ################################################################
@@ -144,56 +146,33 @@ class ContainerJob(runner.JobBase):
         self._sent_start_request = False
 
     def _is_processing(self):
-        if (self._sent_start_request
-            and REDIS.hget(JOB_STATUS_HASH_KEY, self.jid) != b"finished"):
-            return True
-        return False
+        return (self._sent_start_request
+                and _REDIS.hget(_JOB_STATUS_HASH_KEY, self.jid) != b'finished')
 
     def _kill(self):
-        submit_request({"type": "cancel", "jid": self.jid})
+        _submit_request({'type': 'cancel', 'jid': self.jid})
 
     def _start(self):
         # XX TODO I suspect this is not the right place to do this but I don't
         # understand the status tracking flow yet.
         simulation_db.write_status('running', self.run_dir)
-        # XX TODO: this is a temporary hack, to work around sirepo's habit of
-        # hard-coding the host run_dir into the command line. Longer-term we
-        # need to untangle this.
-        cmd = list(self.cmd)
-        for i in range(len(cmd)):
-            if cmd[i] == self.run_dir:
-                cmd[i] = "/job-dir"
-        # XX TODO: this encoding thing is also a gnarly hack
-        # (1) for prototyping we want to work with the existing docker image,
-        # which requires we detour through bash to set up our environment,
-        # (2) self.cmd is already in the structured form we want to ultimately
-        # pass straight through to exec,
-        # (3) this means we need some hack to smuggle the real command
-        # through all the quoting layers.
-        #
-        # base64 is a blunt hammer, but it's easier than figuring out the
-        # details of all the different quoting systems.
-        encoded_cmd = b64encode(json.dumps(cmd).encode("utf-8"))
-        encoded_cmd = encoded_cmd.replace(b"\n", b"").replace(b" ", b"")
-        encoded_cmd = encoded_cmd.decode("ascii")
-        image = "radiasoft/sirepo"
+        # We can't just pass self.cmd directly to docker because we have to
+        # detour through bash to set up our environment. But we do assume that
+        # self.cmd is sufficiently well-behaved that shlex.quote will work.
+        image = 'radiasoft/sirepo'
         docker_command = [
-            "/home/vagrant/.radia-run/tini",
-            "--",
-            "/bin/bash",
-            "-c",
-            """
-            . ~/.bashrc
-            exec python3 -c 'import json, base64, os; cmd = json.loads(base64.b64decode("%s")); os.execvp(cmd[0], cmd)'
-            """ % encoded_cmd,
+            '/bin/bash',
+            '-c',
+            '. ~/.bashrc && ' + ' '.join(shlex.quote(c) for c in self.cmd),
         ]
-        submit_request({
-            "type": "start",
-            "jid": self.jid,
-            "image": image,
-            "command": docker_command,
-            "working_dir": "/job-dir",
-            "volumes": {str(self.run_dir): {"bind": "/job-dir", "mode": "rw"}},
+        _submit_request({
+            'type': 'start',
+            'jid': self.jid,
+            'image': image,
+            'command': docker_command,
+            'working_dir': str(self.run_dir),
+            'volumes': {str(self.run_dir):
+                          {'bind': str(self.run_dir), 'mode': 'rw'}},
         })
         # XX TODO: This is a hack to avoid a race condition where
         # _is_processing would return False for a moment after we call this.
