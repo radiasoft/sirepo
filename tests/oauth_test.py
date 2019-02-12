f01f2abf39b381b6edf6b8d1be332e8851f220a9
robnagler <github@q33.us>
2019-02-12 23:25:11 +0000

    test merge user with oauth

--- tests/oauth_test.py
+++ tests/oauth_test.py
@@ -65,13 +65,125 @@ def test_happy_path(monkeypatch):
     pkre('"loginSession": "logged_in"', t)
     m = re.search('"uid": "([^"]+)"', t)
     uid = m.group(1)
-    r = fc.sr_get('logout', {'simulation_type': sim_type,}, raw_response=True)
+    r = fc.sr_get('logout', {'simulation_type': sim_type}, raw_response=True)
     pkre('/{}$'.format(sim_type), r.headers['Location'])
     t = fc.sr_get('userState', raw_response=True).data
     pkre('"uid": "{}"'.format(uid), t)
     pkre('"userName": null', t)
     pkre('"displayName": null', t)
     pkre('"loginSession": "logged_out"', t)
+
+
+def test_anonymous_merge(monkeypatch):
+    from pykern import pkcollections
+    from pykern import pkconfig
+    from pykern.pkdebug import pkdp
+    from pykern.pkunit import pkfail, pkok, pkeq, pkre
+    from sirepo import srunit
+    import re
+
+    sim_type = 'myapp'
+    fc = srunit.flask_client({
+        'SIREPO_FEATURE_CONFIG_API_MODULES': 'oauth',
+        'SIREPO_FEATURE_CONFIG_SIM_TYPES': sim_type,
+        'SIREPO_OAUTH_GITHUB_CALLBACK_URI': '/uri',
+        'SIREPO_OAUTH_GITHUB_KEY': 'key',
+        'SIREPO_OAUTH_GITHUB_SECRET': 'secret',
+    })
+    from sirepo import oauth
+    oc = _OAuthClient(
+        pkcollections.Dict(
+            access_token='xyzzy',
+            data=pkcollections.Dict(
+                id='9999',
+                name='Joe Blow',
+                login='joeblow',
+            ),
+        ),
+    )
+    monkeypatch.setattr(oauth, '_oauth_client', oc)
+    fc.get('/{}'.format(sim_type))
+    fc.sr_get(
+        'oauthLogin',
+        {
+            'simulation_type': sim_type,
+            'oauth_type': oauth.DEFAULT_OAUTH_TYPE,
+        },
+        raw_response=True,
+    )
+    state = oc.values.state
+    t = fc.sr_get('userState', raw_response=True).data
+    fc.sr_get(
+        'oauthAuthorized',
+        {
+            'oauth_type': oauth.DEFAULT_OAUTH_TYPE,
+        },
+        query=pkcollections.Dict(state=state),
+        raw_response=True,
+    )
+    d = fc.sr_post(
+        'listSimulations',
+        {'simulationType': sim_type, 'search': {'simulationName': 'Scooby Doo'}},
+    )
+    d = d[0].simulation
+    d = fc.sr_post(
+        'copySimulation',
+        dict(
+            simulationId=d.simulationId,
+            simulationType=sim_type,
+            name='oauth-sim',
+        ),
+    )
+    t = fc.sr_get('userState', raw_response=True).data
+    m = re.search('"uid": "([^"]+)"', t)
+    oauth_uid = m.group(1)
+    fc.sr_get(
+        'logout',
+        {'simulation_type': sim_type},
+        query={'anonymous': '1'},
+        raw_response=True,
+    )
+    d = fc.sr_post(
+        'listSimulations',
+        {'simulationType': sim_type},
+    )
+    pkeq(1, len(d), 'expecting only one simulation: data={}', d)
+    d = d[0].simulation
+    d = fc.sr_post(
+        'copySimulation',
+        dict(
+            simulationId=d.simulationId,
+            simulationType=sim_type,
+            name='anon-sim',
+        ),
+    )
+    t = fc.sr_get('userState', raw_response=True).data
+    m = re.search('"uid": "([^"]+)"', t)
+    anon_uid = m.group(1)
+    pkok(anon_uid != oauth_uid, 'anon_uid == oauth_uid={}', oauth_uid)
+    fc.sr_get(
+        'oauthLogin',
+        {
+            'simulation_type': sim_type,
+            'oauth_type': oauth.DEFAULT_OAUTH_TYPE,
+        },
+        raw_response=True,
+    )
+    state = oc.values.state
+    t = fc.sr_get('userState', raw_response=True).data
+    fc.sr_get(
+        'oauthAuthorized',
+        {
+            'oauth_type': oauth.DEFAULT_OAUTH_TYPE,
+        },
+        query=pkcollections.Dict(state=state),
+        raw_response=True,
+    )
+    d = fc.sr_post(
+        'listSimulations',
+        {'simulationType': sim_type},
+    )
+    pkeq([u'Scooby Doo', u'anon-sim', u'oauth-sim'], sorted([x.name for x in d]))
 
 
 ### write a test with email auth will requrie a sep file
