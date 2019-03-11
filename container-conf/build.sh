8a2fdb7368fc05f9a232ef13b5f5c8052776164c
robnagler <github@q33.us>
2019-03-11 21:35:09 +0000

    fix #1548 install sirepo in py3
    need py3 environment setup for runner_test

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -28,18 +28,24 @@ build_as_run_user() {
     . ~/.bashrc
     cd "$build_guest_conf"
     sirepo_boot_init
-    sirepo_fix_srw
-    sirepo_reinstall_pykern
-
-    # sirepo
+    git clone -q --depth=50 https://github.com/radiasoft/pykern
     git clone -q --depth=50 "--branch=${TRAVIS_BRANCH:-master}" \
         https://github.com/radiasoft/sirepo
     cd sirepo
     if [[ ${TRAVIS_COMMIT:+1} ]]; then
         git checkout -qf "$TRAVIS_COMMIT"
     fi
-    pip install -r requirements.txt
-    pip install .
+    local p
+    for p in py3 py2; do
+        pyenv global "$p"
+        sirepo_fix_srw
+        cd ../pykern
+        pip uninstall -y pykern || true
+        pip install .
+        cd ../sirepo
+        pip install -r requirements.txt
+        pip install .
+    done
 
     # test & deploy
     # npm gets ECONNRESET due to a node error, which shouldn't happen
@@ -61,17 +67,12 @@ sirepo_boot_init() {
 sirepo_fix_srw() {
     # Remove print statements from SRW
     # Patch srwlib.py to not print stuff
-    local srwlib="$(python -c 'import srwlib; print srwlib.__file__')"
+    local srwlib="$(python -c 'import srwlib, sys; sys.stdout.write(srwlib.__file__)')"
+    if [[ ! -f $srwlib ]]; then
+        install_err 'failed to find srwlib'
+    fi
     # Trim .pyc to .py (if there)
     perl -pi.bak -e  's/^(\s+)(print)/$1pass#$2/' "${srwlib%c}"
-}
-
-sirepo_reinstall_pykern() {
-    git clone -q --depth=50 https://github.com/radiasoft/pykern
-    cd pykern
-    pip uninstall -y pykern || true
-    pip install .
-    cd ..
 }
 
 build_vars
