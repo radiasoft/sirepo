18240df8622f8fe241fb81638a1c6287fa16aa79
robnagler <github@q33.us>
2019-04-29 18:46:57 +0000

    fix #1610 added uri_router.call_api, which checks perms
    call_api allows data to be passed to APIs through flask.g.sirepo_call_api_data

--- sirepo/status.py
+++ sirepo/status.py
@@ -5,13 +5,13 @@ u"""Sirepo web server status for remote monitoring
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
 from __future__ import absolute_import, division, print_function
-
 from pykern import pkconfig
 from pykern.pkdebug import pkdc, pkdexc, pkdlog, pkdp
 from sirepo import api_perm
 from sirepo import http_reply
 from sirepo import server
 from sirepo import simulation_db
+from sirepo import uri_router
 import datetime
 import re
 import time
@@ -20,8 +20,11 @@ import time
 #: basic auth "app" initilized in `init_apis`
 _basic_auth = None
 
+_MAX_CALLS = 10
 
-@api_perm.require_basic_auth
+_SLEEP = 1
+
+@api_perm.require_auth_basic
 def api_serverStatus():
     """Allow for remote monitoring of the web server status.
 
@@ -42,25 +45,41 @@ def init_apis(*args, **kwargs):
 def _run_tests():
     """Runs the SRW "Undulator Radiation" simulation's initialIntensityReport"""
     simulation_type = 'srw'
-    res = server.api_findByName(simulation_type, 'default', 'Undulator Radiation')
-    m = re.search(r'\/source\/(\w+)"', res)
+    res = uri_router.call_api(
+        server.api_findByName,
+        dict(
+            simulation_type=simulation_type,
+            application_mode='default',
+            simulation_name='Undulator Radiation',
+        ),
+    )
+    m = re.search(r'\/source\/(\w+)"', res.data)
     if not m:
-        raise RuntimeError('status failed to find simulation_id: {}'.format(res))
-    simulation_id = m.group(1)
-    data = simulation_db.read_simulation_json(simulation_type, sid=simulation_id)
-    data.simulationId = simulation_id
-    data.report = 'initialIntensityReport'
-    #TODO(pjm): don't call private server methods directly (_start_simulation() and _simulation_run_status())
-    server._start_simulation(data)
-    max_calls = 10
-    for _ in range(max_calls):
-        time.sleep(1)
-        res = server._simulation_run_status(data)
-        if res['state'] == 'error':
-            raise RuntimeError('status received simulation error: {}'.format(res))
-        if res['state'] == 'completed':
-            min_size = 50
-            if len(res['z_matrix']) < min_size or len(res['z_matrix'][0]) < min_size:
-                raise RuntimeError('status received bad report output')
-            return
-    raise RuntimeError('status simulation failed to complete within {} seconds'.format(max_calls))
+        raise RuntimeError('failed to find sid in resp={}'.format(res.data))
+    i = m.group(1)
+    d = simulation_db.read_simulation_json(simulation_type, sid=i)
+    d.simulationId = i
+    d.report = 'initialIntensityReport'
+    r = None
+    try:
+        uri_router.call_api(server.api_runSimulation, data=d)
+        for _ in range(_MAX_CALLS):
+            time.sleep(_SLEEP)
+            resp = uri_router.call_api(server.api_runStatus, data=d)
+            r = simulation_db.json_load(resp.data)
+            if r.state == 'error':
+                raise RuntimeError('simulation error: resp={}'.format(r))
+            if r.state == 'completed':
+                min_size = 50
+                if len(r.z_matrix) < min_size or len(r.z_matrix[0]) < min_size:
+                    raise RuntimeError('received bad report output: resp={}', r)
+                return
+            d = r.nextRequest
+        raise RuntimeError(
+            'simulation timed out: seconds={} resp='.format(_MAX_CALLS * _SLEEP, r),
+        )
+    finally:
+        try:
+            uri_router.call_api(server.api_runCancel, data=d)
+        except Exception:
+            pass
