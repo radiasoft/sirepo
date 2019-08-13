6181e841044c3a0a0a9e57c788d888c35d45c460
robnagler <github@q33.us>
2019-08-13 21:08:40 +0000

    cookie expiry is at most one day allowing cfg changes
    db is always checked for actual expiration. Since this is an exception,
    it shouldn't happen more than once unless the user refreshes.

--- sirepo/auth/guest.py
+++ sirepo/auth/guest.py
@@ -26,8 +26,10 @@ AUTH_METHOD_VISIBLE = True
 #: module handle
 this_module = pkinspect.this_module()
 
-#: time that login expires (prefix is "sraz", because github is "srag")
+#: time to recheck login against db (prefix is "sraz", because github is "srag")
 _COOKIE_EXPIRY_TIMESTAMP = 'srazt'
+
+_ONE_DAY = datetime.timedelta(days=1)
 
 
 @api_perm.require_cookie_sentinel
@@ -57,22 +59,26 @@ def validate_login():
     """
     if not cfg.expiry_days:
         return None
-    ts = cookie.unchecked_get_value(_COOKIE_EXPIRY_TIMESTAMP)
-    if not ts:
-        u = auth.user_registration(auth.logged_in_user())
-        ts = (u.created + cfg.expiry_days) - datetime.datetime.utcfromtimestamp(0)
-        ts = int(ts.total_seconds())
-        cookie.set_value(_COOKIE_EXPIRY_TIMESTAMP, str(ts))
-    ts = int(ts)
     n = int(srtime.utc_now_as_float())
-    if n <= ts:
+    t = int(cookie.unchecked_get_value(_COOKIE_EXPIRY_TIMESTAMP, 0))
+    if n <= t:
+        # cached timestamp less than expiry
         return None
-    pkdlog(
-        'expired uid={}, timestamp={} now={}',
-        auth.logged_in_user(),
-        datetime.datetime.utcfromtimestamp(ts),
-        datetime.datetime.utcfromtimestamp(n),
-    )
+    # db expiry at most one day from now so we can change expiry_days
+    # and (in any event) ensure expiry is checked once a day. This
+    # would also allow us to extend the expired period in the db.
+    u = auth.logged_in_user()
+    r = auth.user_registration(u)
+    t = r.created + cfg.expiry_days
+    n = srtime.utc_now()
+    if n <= t:
+        t2 = n + _ONE_DAY
+        if t2 < t:
+            t = t2
+        t -= datetime.datetime.utcfromtimestamp(0)
+        cookie.set_value(_COOKIE_EXPIRY_TIMESTAMP, int(t.total_seconds()))
+        return None
+    pkdlog('expired uid={}, timestamp={} now={}', u, t, n)
     return http_reply.gen_sr_exception(
         'loginFail',
         {
