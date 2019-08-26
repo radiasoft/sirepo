6f1ebe788818e584bc588040967ed7d24e5f35ec
Paul Moeller <moeller@radiasoft.net>
2019-08-26 17:54:57 +0000

    added admin.move_user_sims()

--- sirepo/pkcli/admin.py
+++ sirepo/pkcli/admin.py
@@ -6,8 +6,8 @@ u"""?
 """
 from __future__ import absolute_import, division, print_function
 
-import re
 from pykern import pkio
+from pykern.pkdebug import pkdc, pkdexc, pkdlog, pkdp
 from sirepo import auth
 from sirepo import auth_db
 from sirepo import feature_config
@@ -15,6 +15,11 @@ from sirepo import server
 from sirepo import simulation_db
 from sirepo.template import template_common
 import datetime
+import glob
+import json
+import os.path
+import re
+import shutil
 
 
 def create_examples():
@@ -37,6 +42,40 @@ def create_examples():
             for example in simulation_db.examples(sim_type):
                 if example.models.simulation.name not in names:
                     _create_example(example)
+
+
+def move_user_sims(target_uid=''):
+    """Moves non-example sims and lib files into the target user's directory.
+    Must be run in the source uid directory."""
+    assert target_uid, 'missing target_uid'
+    assert os.path.exists('srw/lib'), 'must run in user dir'
+    assert os.path.exists('../{}'.format(target_uid)), 'missing target user dir: ../{}'.format(target_uid)
+    sim_dirs = []
+    lib_files = []
+
+    for path in glob.glob('*/*/sirepo-data.json'):
+        with open(path) as f:
+            data = json.loads(f.read())
+        sim = data['models']['simulation']
+        if 'isExample' in sim and sim['isExample']:
+            continue
+        sim_dirs.append(os.path.dirname(path))
+
+    for path in glob.glob('*/lib/*'):
+        lib_files.append(path)
+
+    for sim_dir in sim_dirs:
+        target = '../{}/{}'.format(target_uid, sim_dir)
+        assert not os.path.exists(target), 'target sim already exists: {}'.format(target)
+        pkdlog(sim_dir)
+        shutil.move(sim_dir, target)
+
+    for lib_file in lib_files:
+        target = '../{}/{}'.format(target_uid, lib_file)
+        if os.path.exists(target):
+            continue
+        pkdlog(lib_file)
+        shutil.move(lib_file, target)
 
 
 def purge_guest_users(days=180, confirm=False):
