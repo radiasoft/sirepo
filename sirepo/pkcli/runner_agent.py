50b8c5499c68f60dbf9148dab2e30a5261295749
Evan Carlin <evan@carlin.com>
2019-08-30 20:27:36 +0000

    Checkpoint.

--- sirepo/pkcli/runner_agent.py
+++ sirepo/pkcli/runner_agent.py
@@ -7,8 +7,12 @@
 from __future__ import absolute_import, division, print_function
 
 import asks
+import async_generator
+from pykern import pkio
 from pykern import pkjson
+from pykern import pkcollections
 from pykern.pkdebug import pkdp
+from sirepo import runner_client
 from sirepo import runner_daemon
 import trio
 
@@ -22,7 +26,9 @@ def start():
 async def _call_daemon(body):
     try:
         response = await asks.post('http://localhost:8080', json=body)
-        return pkjson.load_any(response.content)
+        pkdp(f'Dameon responded with: {response.content}')
+        return pkcollections.Dict(pkjson.load_any(response.content))
+        # return pkjson.load_any(response.content)
     except Exception as e:
         pkdp(f'Exception with _call_daemon(). Caused by: {e}')
         return {}
@@ -42,25 +48,184 @@ async def _call_daemon_with_result(result):
     await _call_daemon(body) # TODO(e-carlin): Do nothing with response from this call?
 
 async def _main():
-    while True:
-        daemon_request = await _call_daemon_ready_for_work()
-        action = daemon_request.get('action', 'no_op')
-        action_types = {
-            'no_op': _perform_no_op,
-            'start_report_job': _start_report_job,
-        }
-        await action_types[action](daemon_request)
+    async with trio.open_nursery() as nursery:
+        job_tracker = _JobTracker(nursery)
+        while True: # TODO(e-carlin): there has to be something more clever than this
+            request = await _call_daemon_ready_for_work()
+            action = request.get('action', 'no_op') # TODO(e-carlin): Defaulting to no-op isn't right
+            action_types = {
+                'no_op': _perform_no_op,
+                'start_report_job': _start_report_job,
+            }
 
-async def _perform_no_op(daemon_request):
+            await action_types[action](job_tracker, request)
+
+async def _perform_no_op(job_tracker, request):
     pkdp('Daemon requested no_op. Going to sleep for a bit.')
     await trio.sleep(2) # TODO(e-carlin): Exponential backoff?
 
-async def _start_report_job(daemon_request):
-    pkdp(f'Daemon requested start_report_job {daemon_request}. ls -l instead.')
-    await trio.open_process(['ls', '-l'])
-    result = {
-        'foo': 'bar'
-    }
-    await _call_daemon_with_result(result)
+async def _start_report_job(job_tracker, request):
+    pkdp(f'Daemon requested start_report_job: {request}')
+
+    async with job_tracker.locks[request.run_dir]:
+        await job_tracker.start_report_job(
+            request.run_dir, request.jhash,
+            request.backend,
+            request.cmd, pkio.py_path(request.tmp_dir),
+        )
+        return {}
 
 
+
+class _JobInfo:
+    def __init__(self, run_dir, jhash, status, report_job):
+        self.run_dir = run_dir
+        self.jhash = jhash
+        self.status = status
+        self.report_job = report_job
+        self.cancel_requested = False
+
+# TODO(e-carlin): Understand what this actually does and why it is useful
+class _LockDict:
+    def __init__(self):
+        # {key: ParkingLot}
+        # lock is held iff the key exists
+        self._waiters = {}
+
+    @async_generator.asynccontextmanager
+    async def __getitem__(self, key):
+        # acquire
+        if key not in self._waiters:
+            # lock is unheld; adding a key makes it held
+            self._waiters[key] = trio.hazmat.ParkingLot()
+        else:
+            # lock is held; wait for someone to pass it to us
+            await self._waiters[key].park()
+        try:
+            yield
+        finally:
+            # release
+            if self._waiters[key]:
+                # someone is waiting, so pass them the lock
+                self._waiters[key].unpark()
+            else:
+                # no-one is waiting, so mark the lock unheld
+                del self._waiters[key]
+
+class _JobTracker:
+    def __init__(self, nursery):
+        self.report_jobs = {}
+        self.locks = _LockDict()
+        self._nursery = nursery
+
+    async def start_report_job(self, run_dir, jhash, backend, cmd, tmp_dir):
+        pkdp(f'&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& {backend}')
+        import os
+        import re
+        _EXEC_ENV_REMOVE = re.compile('^(OMPI_|PMIX_|SIREPO_|PYKERN_)')
+        env = dict(os.environ)
+        for k in list(env):
+            if _EXEC_ENV_REMOVE.search(k):
+                del env[k]
+        # env['SIREPO_MPI_CORES'] = str(mpi.cfg.cores)
+
+
+        env['PYENV_VERSION'] = 'py2'
+        cmd = ['pyenv', 'exec'] + cmd
+        # TODO(e-carlin): Actually handle the tmp vs run dir. See start_report_job()
+        # in pkcli/runner.py
+        ###
+        pkio.unchecked_remove(run_dir)
+        tmp_dir = pkio.py_path(tmp_dir)
+        tmp_dir.rename(run_dir)
+        ####
+        cmd = ['python', 'long_run.py']
+        p = await trio.open_process(
+            cmd,
+            cwd='/home/vagrant/src/radiasoft/sirepo/sirepo/pkcli'
+            # cwd=run_dir,
+            # env=env
+        )
+        pkdp(f'Report job started')
+        report_job = _LocalReportJob(p, {'pid': p.pid})
+        job_info = _JobInfo(
+            run_dir, jhash, runner_client.JobStatus.RUNNING, report_job
+        )
+        self.report_jobs[run_dir] = job_info
+        async def _supervise_job(run_dir, jhash, job_info):
+            pkdp(f'Starting to wait on jhash {jhash}')
+            returncode = await job_info.report_job.wait()
+            pkdp(f'jhash {jhash} finished with exit code {returncode}')
+
+        self._nursery.start_soon(
+            _supervise_job, run_dir, jhash, job_info
+        )
+        """
+        # First make sure there's no-one else using the run_dir
+        current_jhash, current_status = self.run_dir_status(run_dir)
+        if current_status is runner_client.JobStatus.RUNNING:
+            # Something's running.
+            if current_jhash == jhash:
+                # It's already the requested job, so we have nothing to
+                # do. Throw away the tmp_dir and move on.
+                pkdlog(
+                    'job is already running; skipping (run_dir={}, jhash={}, tmp_dir={})',
+                    run_dir, jhash, tmp_dir,
+                )
+                pkio.unchecked_remove(tmp_dir)
+                return
+            else:
+                # It's some other job. Better kill it before doing
+                # anything else.
+                # XX TODO: should we check some kind of sequence number
+                # here? I don't know how those work.
+                pkdlog(
+                    'stale job is still running; killing it (run_dir={}, jhash={})',
+                    run_dir, jhash,
+                )
+                await self.kill_all(run_dir)
+
+        # Okay, now we have the dir to ourselves. Set up the new run_dir:
+        assert run_dir not in self.report_jobs
+        pkio.unchecked_remove(run_dir)
+        tmp_dir.rename(run_dir)
+        # Start the job:
+        # report_job = await _BACKENDS[backend].start_report_job(run_dir, cmd)
+        # And update our records so we know it's running:
+        job_info = _JobInfo(
+            run_dir, jhash, runner_client.JobStatus.RUNNING, report_job,
+        )
+        self.report_jobs[run_dir] = job_info
+        pkjson.dump_pretty(
+            {
+                'version': 1,
+                'backend': backend,
+                'backend_info': report_job.backend_info,
+            },
+            filename=run_dir.join(_RUNNER_INFO_BASENAME),
+        )
+
+        # And finally, start a background task to watch over it.
+        self._nursery.start_soon(
+            self._supervise_report_job, run_dir, jhash, job_info,
+        )
+        """
+
+
+
+class _LocalReportJob:
+    def __init__(self, trio_process, backend_info):
+        self._trio_process = trio_process
+        self.backend_info = backend_info
+
+    async def kill(self, grace_period):
+        # Everything here is a no-op if the process is already dead
+        self._trio_process.terminate()
+        with trio.move_on_after(grace_period):
+            await self._trio_process.wait()
+        self._trio_process.kill()
+        await self._trio_process.wait()
+
+    async def wait(self):
+        await self._trio_process.wait()
+        return self._trio_process.returncode
--- sirepo/pkcli/runner_daemon.py
+++ sirepo/pkcli/runner_daemon.py
@@ -7,28 +7,23 @@
 from __future__ import absolute_import, division, print_function
 
 import aenum
+from collections import deque
 from functools import partial
 from hypercorn.config import Config
 from hypercorn.trio import serve
 from pykern import pkjson
 from pykern.pkdebug import pkdp
 import queue
+from sirepo import simulation_db
+from sirepo.template import template_common
 import trio
+import uuid
 
-
-ACTION_START_REPORT_JOB = 'start_report_job'
 ACTION_NO_OP = 'no_op'
 
-_WORK_QUEUE = queue.Queue()
+_WORK_QUEUE = deque([])
 
-def main():
-    for i in range(0, 50):
-        _WORK_QUEUE.put(
-            {
-                'action': ACTION_START_REPORT_JOB if i%2 == 0  else  ACTION_NO_OP,
-                'data': i,
-            }
-        )
+def start():
     config = Config()
     config.bind = ['localhost:8080']
     trio.run(partial(serve, _app, config))
@@ -41,32 +36,6 @@ async def _app(scope, receive, send):
     }
     await scope_types[scope['type']](receive, send)
 
-async def _process_request_body(body):
-    pkdp(f'Received {body} from agent')
-    async def _action_ready_for_work(data):
-        # TODO: pop off queue
-        pkdp(f'Agent is ready for work. Popping from work queue')
-        if not _WORK_QUEUE.empty():
-            return {
-                'action': _WORK_QUEUE.get()['action'],
-            }
-        else:
-            return {
-                'action': ACTION_NO_OP,
-            }
-    async def _action_process_result(data):
-        pkdp(f'Agent returned result. Processing it and telling them no-op')
-        return {
-            'action': ACTION_NO_OP
-        }
-
-    action_types = {
-        'ready_for_work': _action_ready_for_work,
-        'process_result': _action_process_result,
-
-    }
-    return await action_types[body['action']](body['data'])
-
 async def _handle_scope_type_http(receive, send):
     request_body = await _http_receive(receive)
     response_body = await _process_request_body(request_body)
@@ -74,21 +43,21 @@ async def _handle_scope_type_http(receive, send):
 
 
 async def _handle_scope_type_lifespan(receive, send):
-    async def _handle_startup():
-        await send({'type': 'lifespan.startup.complete'})
-    async def _hanlde_shutdown():
-        await send({'type': 'lifespan.shutdown.complete'})
+    async def _handle_complete(lifespan):
+        await send({'type': f'{lifespan}.complete'})
 
     # TODO(e-carlin): Understand this while True. W/o it we get trio.ClosedResourceError
     # on C-c kill of app. It seems that only the first "lifetime" event is passed
     # through _app(3). All subsequent lifetime events come trhough receive()
     while True:
         message = await receive()
+        message_type = message['type']
         message_types = {
-            'lifespan.startup': _handle_startup,
-            'lifespan.shutdown': _hanlde_shutdown,
+            'lifespan.startup': _handle_complete,
+            'lifespan.shutdown': _handle_complete,
         }
-        await message_types[message['type']]()
+
+        await message_types[message_type](message_type)
 
 
 async def _http_receive(receive):
@@ -103,7 +72,7 @@ async def _http_receive(receive):
 
 
 async def _http_send(response_body, send):
-    pkdp(f'body is {response_body}')
+    pkdp(f'Sending to agent: {response_body}')
     await send({
         'type': 'http.response.start',
         'status': 200,
@@ -116,3 +85,27 @@ async def _http_send(response_body, send):
         'type': 'http.response.body',
         'body': pkjson.dump_bytes(response_body),
     })
+
+
+async def _process_request_body(body):
+    action_types = {
+        'ready_for_work': _action_ready_for_work,
+        'start_report_job': _action_start_report_job,
+    }
+    return action_types[body['action']](body)
+
+def _action_ready_for_work(body):
+    pkdp(f'Agent is ready for work')
+    if _WORK_QUEUE:
+        action = _WORK_QUEUE.popleft()
+        return action
+    else:
+        pkdp('Action is no_op')
+        return {
+            'action': ACTION_NO_OP,
+        }
+
+def _action_start_report_job(request_body):
+    pkdp('Start report job requested')
+    _WORK_QUEUE.append(request_body)
+    return {}
