4d8792a9d8d610b3bd78de6c18832b486005a1ad
Evan Carlin <evan@carlin.com>
2019-09-10 20:56:34 +0000

    Rename request to req

--- sirepo/pkcli/job_supervisor.py
+++ sirepo/pkcli/job_supervisor.py
@@ -87,11 +87,11 @@ class _DriverClient():
         await getattr(self, f'process_{source}_request')(req, reply_writer)
         
 
-    async def process_driver_request(self, request, send_to_driver):
-        pkdc('Processing driver request. Request: {}', request)
+    async def process_driver_request(self, req, send_to_driver):
+        pkdc('Processing driver request. Request: {}', req)
 
         # Driver is ready for work. Give it work when we have some.
-        if request.action == job.ACTION_DRIVER_READY_FOR_WORK:
+        if req.action == job.ACTION_DRIVER_READY_FOR_WORK:
             work_to_do = await self._driver_work_q.get()
             pkdc('Received work item for driver. Sending to driver. Work: {}', work_to_do)
             #TODO(e-carlin): Handle errors.
@@ -100,26 +100,26 @@ class _DriverClient():
             return
 
         # Driver has the results of work. Send them to the server.
-        pkdc('Sending driver results to server: {}', request)
-        server_reply = self._server_responses[request.request_id]
-        _http_send(request, server_reply.send)
+        pkdc('Sending driver results to server: {}', req)
+        server_reply = self._server_responses[req.request_id]
+        _http_send(req, server_reply.send)
         assert not server_reply.reply_sent.is_set()
         server_reply.reply_sent.set()
         _http_send({}, send_to_driver)
         return
 
-    async def process_srserver_request(self, request, send_to_server):
-        pkdc('Processing server request. Request: {}', request)
+    async def process_srserver_request(self, req, send_to_server):
+        pkdc('Processing server request. Request: {}', req)
         reply_sent = tornado.locks.Event()
-        request.request_id = str(uuid.uuid4())
-        work_to_do = pkcollections.Dict(request)
-        self._server_responses[request.request_id] = pkcollections.Dict({
+        req.request_id = str(uuid.uuid4())
+        work_to_do = pkcollections.Dict(req)
+        self._server_responses[req.request_id] = pkcollections.Dict({
             'send': send_to_server,
             'reply_sent': reply_sent,
         })
         await self._driver_work_q.put(work_to_do)
         await reply_sent.wait()
-        del self._server_responses[request.request_id]
+        del self._server_responses[req.request_id]
 
 
 def _http_send(body, write):
