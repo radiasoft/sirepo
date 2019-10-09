2f1ca1097fe5ab0a94e0fffbd442f982d433ef34
robnagler <github@q33.us>
2019-10-09 16:31:08 +0000

    remove runner_daemon=

--- sirepo/feature_config.py
+++ sirepo/feature_config.py
@@ -58,7 +58,6 @@ def _codes(want_all=None):
 cfg = pkconfig.init(
     api_modules=((), set, 'optional api modules, e.g. status'),
     job_supervisor=(False, bool, 'job supervisor to manage running of jobs'),
-    runner_daemon=(False, bool, 'use the runner daemon'),
     #TODO(robnagler) make sim_type config
     rs4pi_dose_calc=(False, bool, 'run the real dose calculator'),
     sim_types=(None, _cfg_sim_types, 'simulation types (codes) to be imported'),
