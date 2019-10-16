7b7fa56e7f38eab9b76329fa1396d965f93bba42
mkeilman <mkeilman@radiasoft.net>
2019-10-16 11:50:41 -0600

    Removed strict int validation

--- sirepo/template/template_common.py
+++ sirepo/template/template_common.py
@@ -492,7 +492,6 @@ def validate_model(model_data, model_schema, enum_info):
                             item, field_type, k
                         )
         elif field_type == 'Float':
-            # convert all falsy values
             v = 0. if not value else float(value)
             if re.search('\[m(m|rad)\]', label) or re.search('\[Lines/mm', label):
                 v /= 1000.
@@ -505,7 +504,8 @@ def validate_model(model_data, model_schema, enum_info):
                 v /= 1e6
             model_data[k] = v
         elif field_type == 'Integer':
-            # bomb on anything but ints
+            if not value:
+                value = 0
             model_data[k] = int(value)
         else:
             model_data[k] = _escape(value)
