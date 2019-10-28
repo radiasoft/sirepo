c00632b333e19547505ccdb1a868933079a07aa6
Paul Moeller <moeller@radiasoft.net>
2019-10-28 16:28:28 +0000

    api_runSimulation moved from server to runner_api

--- sirepo/status.py
+++ sirepo/status.py
@@ -9,6 +9,7 @@ from pykern import pkconfig
 from pykern.pkdebug import pkdc, pkdexc, pkdlog, pkdp
 from sirepo import api_perm
 from sirepo import http_reply
+from sirepo import runner_api
 from sirepo import server
 from sirepo import simulation_db
 from sirepo import uri_router
@@ -68,10 +69,10 @@ def _run_tests():
     d.report = _SIM_REPORT
     r = None
     try:
-        uri_router.call_api(server.api_runSimulation, data=d)
+        uri_router.call_api(runner_api.api_runSimulation, data=d)
         for _ in range(_MAX_CALLS):
             time.sleep(_SLEEP)
-            resp = uri_router.call_api(server.api_runStatus, data=d)
+            resp = uri_router.call_api(runner_api.api_runStatus, data=d)
             r = simulation_db.json_load(resp.data)
             if r.state == 'error':
                 raise RuntimeError('simulation error: resp={}'.format(r))
