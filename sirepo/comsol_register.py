e4f20bf28f02f0b691790d3432dd7657689d2ea8
robnagler <github@q33.us>
2019-10-30 23:10:31 +0000

    server.javascript_redirect => http_reply.gen_redirect_for_anchor

--- sirepo/comsol_register.py
+++ sirepo/comsol_register.py
@@ -19,7 +19,7 @@ import flask_mail
 
 @api_perm.allow_visitor
 def api_comsol():
-    return server.javascript_redirect('/old#/comsol')
+    return http_reply.gen_redirect_for_anchor('/old#/comsol')
 
 
 @api_perm.allow_visitor
