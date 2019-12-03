3538c7cffefcb1774f1431891095c8e2f31f5aa5
robnagler <github@q33.us>
2019-12-03 21:59:50 +0000

    fpc
    fpc

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ pykern.pksetup.setup(
         'pykern',
         'pytz==2015.7',
         # requests-oauthlib 1.3.0 has requirement oauthlib>=3.0.0, but you'll have oauthlib 2.1.0 which is incompatible
-        'requests-oauthlib==1.1.0; python_version < "3"'
+        'requests-oauthlib==1.1.0; python_version < "3"',
         # for Flask-OAuthlib
         'oauthlib!=2.0.3,!=2.0.4,!=2.0.5,<3.0.0,>=1.1.2; python_version < "3"',
         'scikit-learn==0.20',
