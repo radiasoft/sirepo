a22804e9ce208d41ac126ee0655ce7112bcb5140
robnagler <github@q33.us>
2019-12-03 21:55:01 +0000

    try to fix:  requests-oauthlib 1.3.0 has requirement oauthlib>=3.0.0, but you'll have oauthlib 2.1.0 which is incompatible

--- setup.py
+++ setup.py
@@ -31,14 +31,15 @@ pykern.pksetup.setup(
         'pyIsEmail',
         'pykern',
         'pytz==2015.7',
+        # requests-oauthlib 1.3.0 has requirement oauthlib>=3.0.0, but you'll have oauthlib 2.1.0 which is incompatible
+        'requests-oauthlib==1.1.0; python_version < "3"'
         # for Flask-OAuthlib
-        'oauthlib!=2.0.3,!=2.0.4,!=2.0.5,<3.0.0,>=1.1.2',
+        'oauthlib!=2.0.3,!=2.0.4,!=2.0.5,<3.0.0,>=1.1.2; python_version < "3"',
         'scikit-learn==0.20',
         'sympy; python_version < "3"',
         'user-agents',
         'uwsgi',
         'werkzeug',
-#        'docker; python_version >= "3"',
         'tornado >= 6; python_version >= "3"',
    ],
     license='http://www.apache.org/licenses/LICENSE-2.0.html',
