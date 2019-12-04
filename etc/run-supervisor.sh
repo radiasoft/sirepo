7d62bbce0accc1b3e377ded8651f2e9dfe10225f
robnagler <github@q33.us>
2019-12-04 23:27:17 +0000

    conditional on "nersc" argument and print which sbatch

--- etc/run-supervisor.sh
+++ etc/run-supervisor.sh
@@ -5,13 +5,14 @@ export PYKERN_PKDEBUG_WANT_PID_TIME=1
 export SIREPO_FEATURE_CONFIG_JOB_SUPERVISOR=1
 export SIREPO_JOB_DRIVER_MODULES=local:sbatch
 
-if false; then
+if [[ ${1:-} != nersc ]]; then
     export SIREPO_JOB_DRIVER_SBATCH_CORES=4
     export SIREPO_JOB_DRIVER_SBATCH_HOST=v8.radia.run
     export SIREPO_JOB_DRIVER_SBATCH_SIREPO_CMD=/home/vagrant/.pyenv/versions/py3/bin/sirepo
     export SIREPO_JOB_DRIVER_SBATCH_SRDB_ROOT='/var/tmp/{sbatch_user}/sirepo'
     export SIREPO_JOB_DRIVER_SBATCH_USER=vagrant
     export SIREPO_SIMULATION_DB_SBATCH_DISPLAY='V8 Cluster'
+    export SIREPO_JOB_SUPERVISOR_URI=http://v.radia.run:8001
     export SIREPO_JOB_SUPERVISOR_SBATCH_POLL_SECS=5
 else
     export SIREPO_JOB_DRIVER_SBATCH_HOST=cori.nersc.gov
@@ -23,6 +24,7 @@ else
     export SIREPO_SIMULATION_DB_SBATCH_DISPLAY='Cori@NERSC'
     export SIREPO_JOB_SUPERVISOR_SBATCH_POLL_SECS=15
 fi
+echo "$SIREPO_SIMULATION_DB_SBATCH_DISPLAY"
 export SIREPO_JOB_DRIVER_SBATCH_HOST_KEY="$(grep ^$SIREPO_JOB_DRIVER_SBATCH_HOST ~/.ssh/known_hosts || true)"
 if [[ ! $SIREPO_JOB_DRIVER_SBATCH_HOST_KEY ]]; then
     cat <<EOF 1>&2
