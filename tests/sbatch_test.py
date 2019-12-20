a59b18807be722faabf9fd7e199fe2ef4f0c919f
Evan Carlin <evan@carlin.com>
2019-12-20 17:36:46 +0000

    skipif sbatch not present or SIREPO_FEATURE_CONFIG_JOB != 1

--- tests/sbatch_test.py
+++ tests/sbatch_test.py
@@ -6,7 +6,30 @@ u"""test running of animations through sbatch
 """
 from __future__ import absolute_import, division, print_function
 from pykern.pkcollections import PKDict
+import os
 import pytest
+
+
+def _slurm_not_installed():
+    import subprocess
+    try:
+        subprocess.check_output(('sbatch', '--help'))
+    except OSError:
+        return True
+    return False
+
+
+pytestmark = [
+    pytest.mark.skipif(
+        _slurm_not_installed(),
+        reason="slurm not installed, skipping sbatch tests"
+    ),
+    pytest.mark.skipif(
+        not os.environ.get('SIREPO_FEATURE_CONFIG_JOB') == '1',
+        reason="SIREPO_FEATURE_CONFIG_JOB != 1"
+    ),
+]
+
 
 
 def test_warppba_no_creds(sbatch_animation_fc):
@@ -64,5 +87,4 @@ def test_warppba_login(fc):
             username='vagrant',
         )
     )
-    print('eeeeeeeeeeeeeeeeeeeeeeeeeee')
     fc.sr_run_sim(data, c, expect_completed=False)
