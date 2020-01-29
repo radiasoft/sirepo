08edd2d2ba38efaa49dffe7878405922c969677f
robnagler <github@q33.us>
2020-01-29 18:01:40 +0000

    ckp: working simply

--- sirepo/pkcli/test_http.py
+++ sirepo/pkcli/test_http.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
-u"""test driver for remote tests
+u"""multi-threaded requests to server over http
+
 :copyright: Copyright (c) 2020 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
-from pykern.pkcollections import PKDict
+from __future__ import absolute_import, division, print_function
 from pykern import pkconfig
 from pykern import pkjson
+from pykern.pkcollections import PKDict
 from pykern.pkdebug import pkdlog, pkdp, pkdexc, pkdc
+import copy
 import re
 import requests
 import subprocess
 import threading
+import time
 
 
 cfg = None
@@ -19,7 +23,7 @@ cfg = None
 def run_all():
     l = []
     for a in (
-        ('a@b.c', 'myapp', 'Scooby Doo'),
+#        ('a@b.c', 'myapp', 'Scooby Doo'),
         ('a@b.c', 'srw', "Young's Double Slit Experiment"),
     ):
         t = threading.Thread(target=run, args=a)
@@ -30,9 +34,7 @@ def run_all():
 
 
 def run(email, sim_type, *sim_names):
-    s = _Session(email, sim_type)
-    s.sr_login()
-    run_sequential_parallel(s)
+    run_sequential_parallel(_Client(email=email, sim_type=sim_type).login())
     '''
     o = list(sim_names)
     for x in l:
@@ -40,120 +42,147 @@ def run(email, sim_type, *sim_names):
             o.remove(x.name)
     assert not o, \
         'sim_names={} not found in list={}'.format(o, l)
-    d = s.sr_get(
-        '/simulation/{}/{}/0'.format(s.sr_sim_type, x.simulationId),
+    d = s.get(
+        '/simulation/{}/{}/0'.format(s.sim_type, x.simulationId),
     )
     x = d.models.simulation
     pkdlog('sid={} name={}', x.simulationId, x.name)
     '''
 
 
-def run_sequential_parallel(session):
-    sim_run('multiElectronAnimation'
-        'brillianceReport',
-        'fluxReport',
-        'initialIntensityReport',
-        'intensityReport',
-        'powerDensityReport',
-        'sirepo-data.json',
-        'sourceIntensityReport',
-        'trajectoryReport',
-        'watchpointReport6',
-        'watchpointReport7',
-    ):
-        d = sessions.sr_sim_data(r)
+def run_sequential_parallel(client):
+    #t = client.sim_run("Young's Double Slit Experiment", 'multiElectronAnimation')
+    t = client.sim_run("Young's Double Slit Experiment", 'intensityReport')
+    t.join()
+#        'brillianceReport',
+    #     'fluxReport',
+    #     'initialIntensityReport',
+    #     'intensityReport',
+    #     'powerDensityReport',
+    #     'sirepo-data.json',
+    #     'sourceIntensityReport',
+    #     'trajectoryReport',
+    #     'watchpointReport6',
+    #     'watchpointReport7',
+    # ):
 
 
-        session.
 
 
-class _Session(requests.Session):
+class _Client(PKDict):
 
     __global_lock = threading.Lock()
 
     __login_locks = PKDict()
 
-    def __init__(self, email, sim_type):
-        super(_Session, self).__init__()
+    def __init__(self, **kwargs):
+        super(_Client, self).__init__(**kwargs)
         _init()
-        self.sr_email = email
-        self.sr_sim_type = sim_type
+        self._session = requests.Session()
+        self._session.verify = False
 
-    def sr_get(self, uri):
-        return self.sr_parse_response(
-            self.get(
-                url=self.sr_uri(uri),
-                verify=False,
-            ),
+    def copy(self):
+        n = type(self)()
+        # reaches inside requests.Session
+        n._session.cookies = self._session.cookies.copy()
+        for k, v in self.items():
+            if k not in n:
+                n[k] = copy.deepcopy(v)
+        return n
+
+    def get(self, uri):
+        return self.parse_response(
+            self._session.get(url=self.uri(uri)),
         )
 
-    def sr_login(self):
-        r = self.sr_post('/simulation-list', PKDict())
+    def login(self):
+        r = self.post('/simulation-list', PKDict())
         assert r.srException.routeName == 'missingCookies'
-        r = self.sr_post('/simulation-list', PKDict())
+        r = self.post('/simulation-list', PKDict())
         assert r.srException.routeName == 'login'
         with self.__global_lock:
-            self.__login_locks.pksetdefault(self.sr_email, threading.Lock)
-        with self.__login_locks[self.sr_email]:
-            r = self.sr_post('/auth-email-login', PKDict(email=self.sr_email))
-            r = self.sr_post(
+            self.__login_locks.pksetdefault(self.email, threading.Lock)
+        with self.__login_locks[self.email]:
+            r = self.post('/auth-email-login', PKDict(email=self.email))
+            r = self.post(
                 r.url,
-                data=PKDict(token=r.url.split('/').pop(), email=self.sr_email),
+                data=PKDict(token=r.url.split('/').pop(), email=self.email),
             )
             m = re.search('location = "(/[^"]+)', r)
             if m and 'complete' in m.group(1):
-                r = self.sr_post(
+                r = self.post(
                     '/auth-complete-registration',
-                    PKDict(displayName=self.sr_email),
+                    PKDict(displayName=self.email),
                 )
 
-        r = self.sr_post('/simulation-list', PKDict())
-        self.sr_sid = PKDict([(x.name, x.simulationId) for x in r])
-        return r
+        r = self.post('/simulation-list', PKDict())
+        self._sid = PKDict([(x.name, x.simulationId) for x in r])
+        self._sim_data = PKDict()
+        return self
 
-    def sr_parse_response(self, resp):
+    def parse_response(self, resp):
         resp.raise_for_status()
         if 'json' in resp.headers['content-type']:
             return pkjson.load_any(resp.content)
         return resp.content
 
-    def sr_post(self, uri, data):
-        data.simulationType = self.sr_sim_type
-        return self.sr_parse_response(
-            self.post(
-                url=self.sr_uri(uri),
+    def post(self, uri, data):
+        data.simulationType = self.sim_type
+        return self.parse_response(
+            self._session.post(
+                url=self.uri(uri),
                 data=pkjson.dump_bytes(data),
                 headers=PKDict({'Content-type': 'application/json'}),
-                verify=False,
             ),
         )
 
-    def sr_sim_data(self, sim_name, simulations):
-        return self.sr_sim_db.pksetdefault(
+    def sim_data(self, sim_name):
+        return self._sim_data.pksetdefault(
             sim_name,
-            lambda: self.sr_get(
-                '/simulation/{}/{}/0'.format(self.sr_sim_type, self.sr_sid[sim_name]),
+            lambda: self.get(
+                '/simulation/{}/{}/0'.format(self.sim_type, self._sid[sim_name]),
             ),
         )[sim_name]
 
-    def sr_sim_run(self, sim_name, report, timeout=None):
-        def _run():
-            d = self.
-            self.sr_post(
+    def sim_run(self, name, report, timeout=10):
+
+        def _run(client):
+            c = None
+            i = client._sid[name]
+            r = client.post(
                 '/run-simulation',
                 PKDict(
-                    models=d,
+                    models=client.sim_data(name).models,
                     report=report,
-                    simulationId=data.models.simulation.simulationId,
-                simulationType=data.simulationType,
-            ).pkupdate(**post_args)
+                    simulationId=i,
+                    simulationType=client.sim_type,
+                ),
+            )
+            try:
+                if r.state == 'completed':
+                    return
+                c = r.get('nextRequest')
+                for _ in range(timeout):
+                    pkdp('here')
+                    if r.state in ('completed', 'error'):
+                        c = None
+                        break
+                    r = self.post('/run-status', r.nextRequest)
+                    pkdp(r.state)
+                    time.sleep(1)
+                else:
+                    pkdlog('sid={} report={} timeout={}', i, report, timeout)
+            finally:
+                if c:
+                    self.post('/run-cancel', c)
+                pkdlog('sid={} report={} state={}', i, report, 'cancel' if c else r.state)
 
-        new session object
-        t = threading.Thread(target=_run)
+        t = threading.Thread(target=_run, args=[self.copy()])
         t.start()
         return t
 
-    def sr_uri(self, uri):
+
+    def uri(self, uri):
         if uri.startswith('http'):
             return uri
         assert uri.startswith('/')
