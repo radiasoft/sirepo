002e4625b5728c4b3e0917dcce2c7a59dc32fb24
Paul Moeller <moeller@radiasoft.net>
2020-02-05 16:29:38 +0000

    srw: fixup old examples with bad electronBeamPosition.drift (HXN and CSX beamlines)

--- sirepo/sim_data/srw.py
+++ sirepo/sim_data/srw.py
@@ -217,6 +217,13 @@ class SimData(sirepo.sim_data.SimDataBase):
                     r += [0, 0, 0, 0, 0, 0, 0, 0]
         if 'electronBeams' in dm:
             del dm['electronBeams']
+        # special case for old examples with incorrect electronBeam.drift
+        if dm.simulation.isExample and dm.simulation.name in (
+                'NSLS-II HXN beamline',
+                'NSLS-II HXN beamline: SSA closer',
+                'NSLS-II CSX-1 beamline'):
+            dm.electronBeamPosition.driftCalculationMethod = 'manual'
+            dm.electronBeamPosition.drift = -1.8 if 'HXN' in dm.simulation.name else -1.0234
         cls._organize_example(data)
 
     @classmethod
