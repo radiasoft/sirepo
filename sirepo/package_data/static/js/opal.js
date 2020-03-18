64a37db9cb587580f0cfd7070f4b05c9aaa23783
Paul Moeller <moeller@radiasoft.net>
2020-03-18 22:19:58 +0000

    added missing Opal import menu item

--- sirepo/package_data/static/js/opal.js
+++ sirepo/package_data/static/js/opal.js
@@ -176,6 +176,7 @@ SIREPO.app.directive('appFooter', function() {
 	},
         template: [
             '<div data-common-footer="nav"></div>',
+            '<div data-import-dialog=""></div>',
 	].join(''),
     };
 });
@@ -203,6 +204,9 @@ SIREPO.app.directive('appHeader', function(appState, latticeService, opalService
 		//  '<div>App-specific setting item</div>',
               '</app-settings>',
               '<app-header-right-sim-list>',
+                '<ul class="nav navbar-nav sr-navbar-right">',
+                  '<li><a href data-ng-click="nav.showImportModal()"><span class="glyphicon glyphicon-cloud-upload"></span> Import</a></li>',
+                '</ul>',
               '</app-header-right-sim-list>',
             '</div>',
 	].join(''),
