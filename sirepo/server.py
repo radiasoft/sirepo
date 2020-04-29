8c68bc5d8ae4eb938eaf7496af63724ea1420534
mkeilman <mkeilman@radiasoft.net>
2020-04-29 13:44:15 -0600

    Allow passing of content type

--- sirepo/server.py
+++ sirepo/server.py
@@ -252,7 +252,11 @@ def api_getApplicationData(filename=None):
         if 'filename' in req:
             assert isinstance(res, pkconst.PY_PATH_LOCAL_TYPE), \
                 '{}: template did not return a file'.format(res)
-            return http_reply.gen_file_as_attachment(res, filename=req.filename)
+            return http_reply.gen_file_as_attachment(
+                res,
+                filename=req.filename,
+                content_type=req.req_data.get('contentType', None)
+            )
         return http_reply.gen_json(res)
 
 
