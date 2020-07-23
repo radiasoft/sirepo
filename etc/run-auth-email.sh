4e37df45cadae099b3e902a53c65633701d3ebf3
robnagler <github@q33.us>
2020-07-23 16:45:37 +0000

    doc environment vars

--- etc/run-auth-email.sh
+++ etc/run-auth-email.sh
@@ -26,13 +26,15 @@ exit
 echo hello | sendmail vagrant
 # to test sasl
 (sleep 1; echo EHLO localhost; sleep 1; echo AUTH PLAIN AHZhZ3JhbnQAdmFncmFudA==; sleep 1; echo QUIT) | telnet localhost 25
+# then use
+export SIREPO_AUTH_EMAIL_SMTP_SERVER='localhost'
 EOF
 export SIREPO_AUTH_EMAIL_FROM_EMAIL='support@radiasoft.net'
 export SIREPO_AUTH_EMAIL_FROM_NAME='RadiaSoft Support'
-export SIREPO_AUTH_EMAIL_SMTP_PASSWORD='n/a'
+export SIREPO_AUTH_EMAIL_SMTP_PASSWORD='vagrant'
 # POSIT: same as sirepo.auth.email._DEV_SMTP_SERVER
 export SIREPO_AUTH_EMAIL_SMTP_SERVER='dev'
-export SIREPO_AUTH_EMAIL_SMTP_USER='n/a'
+export SIREPO_AUTH_EMAIL_SMTP_USER='vagrant'
 export SIREPO_AUTH_METHODS='email:guest'
 export PYKERN_PKDEBUG_WANT_PID_TIME=1
 exec sirepo service http
