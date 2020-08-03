a887899c77c8a58a2712063ae1abbbf9da7b2b32
robnagler <github@q33.us>
2020-08-03 17:32:16 +0000

    fix #2726 removed git etc/git-hooks; use travis

--- etc/git-hooks/update-git-hooks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-b="/home/vagrant/src/radiasoft/sirepo"
-for f in 'merge' 'commit'; do
-    ln -fs "$b/etc/git-hooks/post-master-change" "$b/.git/hooks/post-$f"
-    chmod +x "$b/.git/hooks/post-$f"
-done
