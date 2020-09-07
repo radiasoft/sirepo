80ad0b98c9105d7ce29641fdf95d9fdcdd3e3c42
robnagler <github@q33.us>
2020-09-07 22:38:00 +0000

    Fix #2923 doubled timeout in report_test and sr_run_sim sr_run_sim timeout is 20s (was 10) report_test (for shared_model) is 2s (was 1)

--- tests/report_test.py
+++ tests/report_test.py
@@ -77,5 +77,5 @@ def _r(fc, sim_name, analysis_model, shared_model=None):
     data = fc.sr_sim_data(sim_name)
     r = fc.sr_run_sim(data, analysis_model)
     if shared_model:
-        r = fc.sr_run_sim(data, shared_model, timeout=1, forceRun=False)
+        r = fc.sr_run_sim(data, shared_model, timeout=2, forceRun=False)
         pkunit.pkeq('completed',  r.state)
