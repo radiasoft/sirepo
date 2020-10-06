83e0a8c03f1de00c24b6da73463982a7875e7364
e-carlin <evan@carlin.com>
2020-10-06 21:51:48 +0000

    use double quote so variables are expanded

--- etc/run-jupyterhub.sh
+++ etc/run-jupyterhub.sh
@@ -9,11 +9,11 @@ if [[ "${1:-}" ]]; then
         echo 'Must supply no args or excatly 2 args (GitHub key and secret).'
         exit 1
     fi
-    export SIREPO_AUTH_GITHUB_KEY='$1'
-    export SIREPO_AUTH_GITHUB_METHOD_VISIBLE=''
-    export SIREPO_AUTH_GITHUB_SECRET='$2'
+    export SIREPO_AUTH_GITHUB_KEY="$1"
+    export SIREPO_AUTH_GITHUB_METHOD_VISIBLE=""
+    export SIREPO_AUTH_GITHUB_SECRET="$2"
     export SIREPO_AUTH_METHODS="$SIREPO_AUTH_METHODS:github"
-    export SIREPO_SIM_API_JUPYTERHUBLOGIN_RS_JUPYTER_MIGRATE='1'
+    export SIREPO_SIM_API_JUPYTERHUBLOGIN_RS_JUPYTER_MIGRATE="1"
 fi
 
 sirepo service nginx-proxy &
