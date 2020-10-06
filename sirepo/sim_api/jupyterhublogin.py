b2b9b1ff4ef221a483c0a8b4eca3204e27712ba3
e-carlin <evan@carlin.com>
2020-10-06 21:56:00 +0000

    parse_json returns a PKDict

--- sirepo/sim_api/jupyterhublogin.py
+++ sirepo/sim_api/jupyterhublogin.py
@@ -34,7 +34,7 @@ JupyterhubUser = None
 def api_migrateJupyterhub():
     if not cfg.rs_jupyter_migrate:
         sirepo.util.raise_forbidden('migrate not enabled')
-    d = PKDict(**sirepo.http_request.parse_json())
+    d = sirepo.http_request.parse_json()
     if not d.doMigration:
         return sirepo.http_reply.gen_redirect('jupyterHub')
     return sirepo.uri_router.call_api(
