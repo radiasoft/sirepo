180c5b8ed03516be8bd8c3599f3827eb5bad8cda
Paul Moeller <moeller@radiasoft.net>
2021-03-29 20:17:27 +0000

    added additional plot line colors

--- sirepo/template/template_common.py
+++ sirepo/template/template_common.py
@@ -47,7 +47,7 @@ RUN_LOG = 'run.log'
 
 _HISTOGRAM_BINS_MAX = 500
 
-_PLOT_LINE_COLOR = ['#1f77b4', '#ff7f0e', '#2ca02c']
+_PLOT_LINE_COLOR = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
 
 class ModelUnits():
