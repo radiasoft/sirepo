48c364ab4d6f621a039ef2ac5432d66a61e68d9f
mkeilman <mkeilman@radiasoft.net>
2021-03-31 11:25:41 -0600

    WIP #3406 - allow style attr in sanitizer

--- sirepo/package_data/static/js/sirepo.js
+++ sirepo/package_data/static/js/sirepo.js
@@ -176,7 +176,7 @@ SIREPO.app.config(function(localRoutesProvider, $compileProvider, $locationProvi
     $compileProvider.commentDirectivesEnabled(false);
     $compileProvider.cssClassDirectivesEnabled(false);
     $sanitizeProvider.enableSvg(true);
-    $sanitizeProvider.addValidAttrs(['id', ]);
+    $sanitizeProvider.addValidAttrs(['id', 'style']);
     SIREPO.appFieldEditors = '';
 
     let defaultRoute = null;
