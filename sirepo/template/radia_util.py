32d399cd04a2f981dc992556e7c22cbb99a287b1
mkeilman <mkeilman@radiasoft.net>
2021-05-06 09:53:59 -0600

    #2347 - minor change to radia_util header

--- sirepo/template/radia_util.py
+++ sirepo/template/radia_util.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-u"""Toolkit and/or utils for calling Radia functions
+u"""Utilities and wrappers for calling Radia functions
 
 :copyright: Copyright (c) 2017-2021 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
