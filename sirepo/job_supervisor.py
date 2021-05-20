43797f40ee00b870d1c61777e73c206a2e4b3be3
e-carlin <evan@carlin.com>
2021-05-20 20:39:11 +0000

    try to eliminate repeated code

--- sirepo/job_supervisor.py
+++ sirepo/job_supervisor.py
@@ -16,8 +16,10 @@ import asyncio
 import contextlib
 import copy
 import datetime
+import inspect
 import os
 import pykern.pkio
+import re
 import sirepo.auth
 import sirepo.auth_db
 import sirepo.http_reply
@@ -751,18 +753,10 @@ class _ComputeJob(PKDict):
         )
 
     async def _receive_api_statefulCompute(self, req):
-        return await self._send_with_single_reply(
-            job.OP_ANALYSIS,
-            req,
-            jobCmd='stateful_compute'
-        )
+        return await self._send_simulation_compute(req)
 
     async def _receive_api_statelessCompute(self, req):
-        return await self._send_with_single_reply(
-            job.OP_ANALYSIS,
-            req,
-            jobCmd='stateless_compute'
-        )
+        return await self._send_simulation_compute(req)
 
     def _create_op(self, opName, req, **kwargs):
 #TODO(robnagler) kind should be set earlier in the queuing process.
@@ -843,6 +837,16 @@ class _ComputeJob(PKDict):
                 )
         finally:
             op.destroy(cancel=False)
+
+    async def _send_simulation_compute(self, req):
+        f = inspect.currentframe().f_back.f_code.co_name
+        m = re.search(f'^_receive_api_([a-z]+)Compute$', f)
+        assert m, f'unrecognized caller function={f}'
+        return await self._send_with_single_reply(
+            job.OP_ANALYSIS,
+            req,
+            jobCmd=f'{m.group(1)}_compute',
+        )
 
     async def _send_with_single_reply(self, opName, req, **kwargs):
         o = self._create_op(opName, req, **kwargs)
