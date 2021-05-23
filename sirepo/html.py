fecacb9a1b3c2f99855e7c7eff982198dfd22e2a
robnagler <github@q33.us>
2021-05-23 18:25:20 +0000

    wip #3627

--- sirepo/html.py
+++ sirepo/html.py
@@ -70,11 +70,13 @@ def _widget_supported_codes(ctx):
               </button>
               <ul class="dropdown-menu" aria-labelledby="sr-landing-supported-codes">
     '''
-    for s in sorted([
-            PKDict(ml='activait', jupyterhublogin='jupyter').get(s, s)
-            for s in sirepo.feature_config.cfg().sim_types
-    ]):
-        res += f'<li><a href="/ml">{s}</a></li>'
+    x = PKDict([(s, s) for s in sirepo.feature_config.cfg().sim_types])
+    x.pkupdate(
+        activait='ml',
+        jupyter='jupyterhublogin',
+    )
+    for k in sorted(x.keys()):
+        res += f'<li><a href="/{x[k]}">{k}</a></li>'
     return res + '</ul></li>'
 
 _init()
