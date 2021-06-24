816cc55004faf6fbfad97c8dca6aba271b25baf8
mkeilman <mkeilman@radiasoft.net>
2021-06-24 10:50:25 -0600

    #3576 - sim name includes reason for invalidity

--- sirepo/template/controls.py
+++ sirepo/template/controls.py
@@ -51,7 +51,7 @@ def get_application_data(data, **kwargs):
             res.append(PKDict(
                 name=m.simulation.name,
                 simulationId=m.simulation.simulationId,
-                invalid=not _has_kickers(m)
+                invalidMsg=None if _has_kickers(m) else 'No beamlines' if not _has_beamline(m) else 'No kickers'
             ))
         return PKDict(simList=res)
     elif data.method == 'get_external_lattice':
@@ -195,8 +195,12 @@ def _get_external_lattice(simulation_id):
     )
 
 
+def _has_beamline(model):
+    return model.elements and model.beamlines
+
+
 def _has_kickers(model):
-    if not model.elements or not model.beamlines:
+    if not _has_beamline(model):
         return False
     k_ids = [e._id for e in model.elements if 'KICKER' in e.type]
     if not k_ids:
