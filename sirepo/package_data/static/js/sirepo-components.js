22b610907636743af13483419b18bf85df614fbe
mkeilman <mkeilman@radiasoft.net>
2021-10-14 11:50:27 -0600

    WIP #3659 - restore fieldEditor

--- sirepo/package_data/static/js/sirepo-components.js
+++ sirepo/package_data/static/js/sirepo-components.js
@@ -726,23 +726,11 @@ SIREPO.app.directive('fieldEditor', function(appState, keypressService, panelSta
             }
             $scope.enum = SIREPO.APP_SCHEMA.enum;
             // field def: [label, type]
-
-            let m = $scope.modelName;
-            let mInfo = appState.modelInfo(m);
-            let f = $scope.field;
-
-            const x = appState.parseModelField($scope.field);
-            if (x && x.length === 2) {
-                // should be of the form 'model.<other model name>'
-                m = mInfo[x[0]][SIREPO.INFO_INDEX_TYPE].split('.')[1];
-                f = x[1];
-                mInfo = appState.modelInfo(m);
-            }
-            $scope.info = $scope.customInfo || mInfo[f];
+            $scope.info = $scope.customInfo || appState.modelInfo($scope.modelName)[$scope.field];
             if (! $scope.info) {
                 throw new Error('invalid model field: ' + $scope.modelName + '.' + $scope.field);
             }
-            $scope.fieldProps = appState.fieldProperties(m, f);
+            $scope.fieldProps = appState.fieldProperties($scope.modelName, $scope.field);
 
             // wait until the switch gets fully evaluated, then set event handlers for input fields
             // to disable keypress listener set by plots
@@ -1444,12 +1432,21 @@ SIREPO.app.directive('modelField', function(appState) {
             if (modelField) {
                 modelName = modelField[0];
                 field = modelField[1];
-                //const x = appState.parseModelField(field);
-                //if (x.length === 2) {
-                //    obj = x[0];
-                //    field = x[1];
-                //}
+                const x = appState.parseModelField(field);
+                if (x && x.length === 2) {
+                    obj = x[0];
+                    field = x[1];
+                }
             }
+
+            //const x = appState.parseModelField($scope.field);
+            //if (x && x.length === 2) {
+            //    // should be of the form 'model.<other model name>'
+            //    objField = x[0];
+            //    m = mInfo[objField][SIREPO.INFO_INDEX_TYPE].split('.')[1];
+            //    f = x[1];
+            //    mInfo = appState.modelInfo(m);
+            //}
 
             // allow views to have fields like <model1>.<field 1>.<field 2> where field 1 should have a "type" that is
             // another model, and field 2 is a field of that model, e.g.
