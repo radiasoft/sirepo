de98e3c230e70b487aa0bf37242f9497f2e22d46
robnagler <github@q33.us>
2022-05-21 22:39:18 +0000

    build_run_user is defaulted correctly

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -11,7 +11,6 @@ build_vars() {
     sirepo_boot=$boot_dir/start
     build_is_public=1
     build_docker_cmd='["'"$sirepo_boot"'"]'
-    build_dockerfile_aux="USER $build_run_user"
 }
 
 build_as_root() {
