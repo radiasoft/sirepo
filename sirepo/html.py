7a2bfff75aa54d822bd869209549ceaf8f5257ad
gurhar1133 <ghns1133@gmail.com>
2022-06-03 22:12:55 +0000

    Fix #4443: diff landing + notes

--- sirepo/html.py
+++ sirepo/html.py
@@ -72,6 +72,11 @@ def _widget_supported_codes(ctx):
     '''
     # TODO(e-carlin): https://git.radiasoft.org/sirepo/issues/3632
     x = PKDict([(s, s) for s in sirepo.feature_config.PROD_FOSS_CODES])
+    # TODO (gurhar1133):
+    # - use feature_config.cfg().sim_types instead of FOSS_CODES
+    # - populate with res but not dropdown (maybe buttons)
+    # - clear out en/
+    # - contact Jessie about intro (is dev?) and styling
     x.pkupdate(
         activait='ml',
         jupyter='jupyterhublogin',
