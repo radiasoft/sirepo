a3b71b1bd3e9ea7d7996af57149a14271ab7a80e
gurhar1133 <ghns1133@gmail.com>
2022-07-14 17:09:07 +0000

    Fix #4395: cleanup

--- sirepo/pkcli/admin.py
+++ sirepo/pkcli/admin.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-u"""?
+"""?
 
 :copyright: Copyright (c) 2017 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
@@ -26,7 +26,7 @@ import re
 import shutil
 
 
-_MILISECONDS_PER_MONTH = 2.628e+9
+_MILISECONDS_PER_MONTH = 2.628e9
 _MAXIMUM_SIM_AGE_IN_MONTHS = 6
 
 
@@ -54,10 +54,12 @@ def create_examples():
 
     def _create():
         for sim_type in feature_config.cfg().sim_types:
-            names = [x.name for x in simulation_db.iterate_simulation_datafiles(
+            names = [
+                x.name
+                for x in simulation_db.iterate_simulation_datafiles(
                     sim_type,
                     simulation_db.process_simulation_list,
-                    {'simulation.isExample': True},
+                    {"simulation.isExample": True},
                 )
             ]
             for example in simulation_db.examples(sim_type):
@@ -72,25 +74,27 @@ def _access_sim_db_and_callback(callback):
     import sirepo.server
 
     sirepo.server.init()
-    for d in pkio.sorted_glob(simulation_db.user_path().join('*')):
+    for d in pkio.sorted_glob(simulation_db.user_path().join("*")):
         if _is_src_dir(d):
-            continue;
+            continue
         uid = simulation_db.uid_from_dir_name(d)
-        with sirepo.auth_db.session_and_lock(), \
-             auth.set_user_outside_of_http_request(uid):
+        with sirepo.auth_db.session_and_lock(), auth.set_user_outside_of_http_request(
+            uid
+        ):
             callback()
 
 
 def reset_examples():
-
     def _reset():
         c = []
         r = []
         for sim_type in feature_config.cfg().sim_types:
-            sims = [x for x in simulation_db.iterate_simulation_datafiles(
+            sims = [
+                x
+                for x in simulation_db.iterate_simulation_datafiles(
                     sim_type,
                     simulation_db.process_simulation_list,
-                    {'simulation.isExample': True},
+                    {"simulation.isExample": True},
                 )
             ]
             _build_delete_and_revert_lists(c, r, sims, sim_type)
@@ -103,24 +107,22 @@ def reset_examples():
 def _build_delete_and_revert_lists(delete_list, revert_list, simulations, sim_type):
     n = [n.models.simulation.name for n in simulation_db.examples(sim_type)]
     for sim in simulations:
-        t = (srtime.utc_now_as_milliseconds() - sim.simulation.lastModified) / _MILISECONDS_PER_MONTH
+        t = (
+            srtime.utc_now_as_milliseconds() - sim.simulation.lastModified
+        ) / _MILISECONDS_PER_MONTH
         if sim.name not in n:
             delete_list.append((sim, sim_type))
         elif t > _MAXIMUM_SIM_AGE_IN_MONTHS:
-                revert_list.append((sim.name, sim_type))
-                delete_list.append((sim, sim_type))
+            revert_list.append((sim.name, sim_type))
+            delete_list.append((sim, sim_type))
 
 
 def _revert_sims(revert_list):
-    print(f'REVERT LIST: {revert_list}')
     for n, t in revert_list:
-        _create_example(
-            _get_example_by_name(n, t)
-        )
+        _create_example(_get_example_by_name(n, t))
 
 
 def _delete_sims(delete_list):
-    print(f'DELETE LIST: {delete_list}')
     for s, t in delete_list:
         simulation_db.delete_simulation(t, s.simulationId)
 
@@ -147,8 +149,9 @@ def delete_user(uid):
         if auth.unchecked_get_user(uid) is None:
             return
         with auth.set_user_outside_of_http_request(uid):
-            if sirepo.template.is_sim_type('jupyterhublogin'):
+            if sirepo.template.is_sim_type("jupyterhublogin"):
                 from sirepo.sim_api import jupyterhublogin
+
                 jupyterhublogin.delete_user_dir(uid)
             simulation_db.delete_user(uid)
             # This needs to be done last so we have access to the records in
@@ -156,34 +159,38 @@ def delete_user(uid):
             auth_db.UserDbBase.delete_user(uid)
 
 
-def move_user_sims(target_uid=''):
+def move_user_sims(target_uid=""):
     """Moves non-example sims and lib files into the target user's directory.
     Must be run in the source uid directory."""
-    assert target_uid, 'missing target_uid'
-    assert os.path.exists('srw/lib'), 'must run in user dir'
-    assert os.path.exists('../{}'.format(target_uid)), 'missing target user dir: ../{}'.format(target_uid)
+    assert target_uid, "missing target_uid"
+    assert os.path.exists("srw/lib"), "must run in user dir"
+    assert os.path.exists(
+        "../{}".format(target_uid)
+    ), "missing target user dir: ../{}".format(target_uid)
     sim_dirs = []
     lib_files = []
 
-    for path in glob.glob('*/*/sirepo-data.json'):
+    for path in glob.glob("*/*/sirepo-data.json"):
         with open(path) as f:
             data = json.loads(f.read())
-        sim = data['models']['simulation']
-        if 'isExample' in sim and sim['isExample']:
+        sim = data["models"]["simulation"]
+        if "isExample" in sim and sim["isExample"]:
             continue
         sim_dirs.append(os.path.dirname(path))
 
-    for path in glob.glob('*/lib/*'):
+    for path in glob.glob("*/lib/*"):
         lib_files.append(path)
 
     for sim_dir in sim_dirs:
-        target = '../{}/{}'.format(target_uid, sim_dir)
-        assert not os.path.exists(target), 'target sim already exists: {}'.format(target)
+        target = "../{}/{}".format(target_uid, sim_dir)
+        assert not os.path.exists(target), "target sim already exists: {}".format(
+            target
+        )
         pkdlog(sim_dir)
         shutil.move(sim_dir, target)
 
     for lib_file in lib_files:
-        target = '../{}/{}'.format(target_uid, lib_file)
+        target = "../{}/{}".format(target_uid, lib_file)
         if os.path.exists(target):
             continue
         pkdlog(lib_file)
@@ -202,4 +209,4 @@ def _get_example_by_name(name, sim_type):
 
 
 def _is_src_dir(d):
-    return re.search(r'/src$', str(d))
+    return re.search(r"/src$", str(d))
