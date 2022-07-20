1a321257dfb67538d1911e0b2d2be267e5301279
robnagler <github@q33.us>
2022-07-20 18:52:57 +0000

    fix #4627 do not allow override of uid

--- tests/adm_and_own_jobs_test.py
+++ tests/adm_and_own_jobs_test.py
@@ -4,7 +4,6 @@
 :copyright: Copyright (c) 2020 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
-from __future__ import absolute_import, division, print_function
 import pytest
 import os
 from pykern.pkcollections import PKDict
