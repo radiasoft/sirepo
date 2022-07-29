19ed6b0e2cd2f2b76ff60dc292eb411d1bc668d0
gurhar1133 <ghns1133@gmail.com>
2022-07-29 20:35:54 +0000

    Fix #4648: fmt

--- sirepo/template/srw.py
+++ sirepo/template/srw.py
@@ -510,8 +510,7 @@ def get_data_file(run_dir, model, frame, options):
     if options.suffix == _PROGRESS_LOG_DIR:
         d = run_dir.join(_PROGRESS_LOG_DIR)
         return template_common.text_data_file(
-            _get_most_recent_log_file(pkio.sorted_glob(d.join("*.log"))),
-            d
+            _get_most_recent_log_file(pkio.sorted_glob(d.join("*.log"))), d
         )
     return get_filename_for_model(model)
 
