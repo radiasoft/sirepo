2378f362c79caa96017a965384644aa3c22ac628
garsuga <gsugarbaker@gmail.com>
2022-08-08 20:39:26 +0000

    revert changes to _start function

--- sirepo/pkcli/service.py
+++ sirepo/pkcli/service.py
@@ -95,7 +95,7 @@ def http():
             proc.kill()
 
     def _start(service, extra_environ, cwd=".", prefix=("pyenv", "exec", "sirepo")):
-        env = extra_environ.pkupdate(os.environ)
+        env = PKDict(os.environ).pkmerge(extra_environ)
         processes.append(
             subprocess.Popen(
                 prefix + service,
