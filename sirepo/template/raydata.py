f69cb0460a842c98b329b6e5cbe841843fbbb762
rorour <ravenmarieorourke@gmail.com>
2022-09-27 17:12:46 +0000

    changes from comments

--- sirepo/template/raydata.py
+++ sirepo/template/raydata.py
@@ -90,7 +90,7 @@ def stateless_compute_catalog_names(data):
     )
 
 
-def stateless_compute_replay_scans(data):
+def stateless_compute_begin_replay(data):
     return PKDict(data=_request_scan_monitor(PKDict(method="begin_replay", data=data)))
 
 
