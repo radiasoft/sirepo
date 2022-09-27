e80ddebeaed4493e940a0f493f61352a311354fe
Ben Gur <70293945+Benbenbenin0@users.noreply.github.com>
2022-09-27 13:23:30 -0600

    4549 browser log viewer (#4842)
    fix #4549 show log file text in a browser dialog with the option to download

--- sirepo/package_data/static/js/elegant.js
+++ sirepo/package_data/static/js/elegant.js
@@ -1443,3 +1443,63 @@ SIREPO.app.directive('srBunchEditor', function(appState, panelState) {
         },
     };
 });
+
+SIREPO.app.directive('viewLogIframe', function(appState, requestSender) {
+    return {
+        restrict: 'A',
+        scope: {},
+        template: `
+            <a href data-ng-click="viewLog()">View Log</a>
+            <div class="modal fade" id="sr-iframe-text-view" tabindex="-1" role="dialog">
+              <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                  <div class="modal-header bg-warning">
+                    <span class="lead modal-title text-info">Log</span>
+                    <div class="sr-panel-options pull-right">
+                      <a data-ng-href="{{ downloadLog() }}" target="_blank">
+                        <span class="sr-panel-heading glyphicon glyphicon-cloud-download"></span>
+                      </a>
+                      <button type="button" class="close" data-dismiss="modal" style="margin-left: 10px">
+                        <span>&times;</span>
+                      </button>
+                    </div>
+                  </div>
+                  <div class="modal-body" style="padding: 0">
+                    <iframe id="sr-text-iframe"
+                      style="border: 0; width: 100%; height: 80vh" src=""></iframe>
+                  </div>
+                </div>
+              </div>
+            </div>
+        `,
+        controller: function(appState, elegantService, requestSender, $scope) {
+
+            $scope.viewLog = function(model, filename) {
+                requestSender.sendAnalysisJob(
+                    appState,
+                    (data) => {
+                        $('#sr-text-iframe').attr("srcdoc", data.html);
+                    },
+                    {
+                        method: 'log_to_html',
+                        computeModel: elegantService.computeModel(),
+                        simulationId: appState.models.simulation.simulationId
+                    });
+                $('#sr-iframe-text-view').modal('show');
+            };
+
+            $scope.downloadLog = function() {
+                var m = appState.models.simulationStatus.animation.computeModel;
+                if (! m) {
+                    return '';
+                }
+                return requestSender.formatUrl('downloadDataFile', {
+                    '<simulation_id>': appState.models.simulation.simulationId,
+                    '<simulation_type>': SIREPO.APP_SCHEMA.simulationType,
+                    '<model>': m,
+                    '<frame>': -1,
+                });
+            };
+        },
+    };
+});
--- sirepo/template/elegant.py
+++ sirepo/template/elegant.py
@@ -24,6 +24,9 @@ import math
 import os
 import os.path
 import py.path
+import pygments
+import pygments.formatters
+import pygments.lexers
 import re
 import sirepo.lib
 import sirepo.sim_data
@@ -1453,3 +1456,24 @@ def _sdds_init():
     _x = getattr(_s, "SDDS_LONGDOUBLE", None)
     _SDDS_DOUBLE_TYPES = [_s.SDDS_DOUBLE, _s.SDDS_FLOAT] + ([_x] if _x else [])
     _SDDS_STRING_TYPE = _s.SDDS_STRING
+
+
+def analysis_job_log_to_html(data):
+    r = simulation_db.simulation_run_dir(
+        PKDict(
+            simulationType=data.simulationType,
+            simulationId=data.simulationId,
+            report=data.computeModel,
+        )
+    )
+
+    return PKDict(
+        html=pygments.highlight(
+            pkio.read_text(r.join(ELEGANT_LOG_FILE)),
+            pygments.lexers.get_lexer_by_name("text"),
+            pygments.formatters.HtmlFormatter(
+                noclasses=False,
+                linenos=False,
+            ),
+        )
+    )
