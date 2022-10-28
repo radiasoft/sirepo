b5bc5fdda23df3bb6d52f39b1e91c085cd911322
garsuga <gsugarbaker@gmail.com>
2022-10-28 22:00:44 +0000

    add formstate types

---
+++ react/src/store/formState.ts
@@ -0,0 +1,45 @@
+import { createSlice, Slice } from '@reduxjs/toolkit';
+
+export type FormFieldState<T> = {
+    valid: boolean,
+    value: T,
+    touched: boolean,
+    active: boolean
+}
+
+export type FormState = { 
+    [fieldName: string]: FormFieldState<any>
+}
+
+export type FormStateUpdate = {
+    name: string, // TODO rename to modelName
+    value: FormState
+}
+
+export type FormFieldStateUpdate<T> = {
+    name: string, // TODO rename to modelName
+    field: string, // TODO rename to fieldName
+    value: FormFieldState<T>
+}
+
+export const formStatesSlice: Slice<{
+    [modelName: string]: {
+        
+    }
+}> = createSlice({
+    name: 'formStates',
+    initialState: {},
+    reducers: {
+        updateFormState: (state, {payload: {name, value}}: {payload: FormStateUpdate}) => {
+            state[name] = value;
+        },
+        updateFormFieldState: (state, {payload: {name, value, field}}: {payload: FormFieldStateUpdate<any>}) => {
+            state[name] = {...state[name]};
+            state[name][field] = value;
+        }
+    }
+});
+
+export const selectFormState = (name) => (state) => state[formStatesSlice.name][name] || null;
+
+export const { updateFormState, updateFormFieldState } = formStatesSlice.actions;
