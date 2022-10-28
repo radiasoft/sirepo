a331b096091006497816a048a975e3376008f6a5
garsuga <gsugarbaker@gmail.com>
2022-10-28 22:48:51 +0000

    place selectors and actions within typed object

--- react/src/store/models.ts
+++ react/src/store/models.ts
@@ -29,16 +29,24 @@ export const modelsSlice: Slice<ModelsStoreState> = createSlice({
     }
 });
 
-export const { updateModel } = modelsSlice.actions;
+let { updateModel } = modelsSlice.actions;
 
-export const selectIsLoaded: (state: any) => boolean = (state: any) => {
-    return state[modelsSlice.name].isLoaded;
+export let modelActions = {
+    updateModel
 }
 
-export const selectModels: (state: any) => ModelStates = (state: any) => {
-    return state[modelsSlice.name].models;
-}
-
-export const selectModel: (state: any) => ModelState = (name: string) => {
-    return (state: ModelsStoreState) => state[modelsSlice.name].models[name];
+export let modelSelectors: {
+    selectIsLoaded: (state: any) => boolean,
+    selectModels: (state: any) => ModelStates,
+    selectModel: (state: any) => ModelState
+} = {
+    selectIsLoaded: (state: any) => {
+        return state[modelsSlice.name].isLoaded;
+    },
+    selectModels: (state: any) => {
+        return state[modelsSlice.name].models;
+    },
+    selectModel: (name: string) => {
+        return (state: ModelsStoreState) => state[modelsSlice.name].models[name];
+    }
 }
