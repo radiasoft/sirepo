4d48e55a9d7d5e7053a796d120603093a46163a3
garsuga <gsugarbaker@gmail.com>
2022-11-01 22:29:04 +0000

    remove impossible inheritance for typescript

--- react/src/layout/layout.ts
+++ react/src/layout/layout.ts
@@ -1,6 +1,5 @@
 import React from "react";
 import { Dependency } from "../data/dependency";
-import { useRenderCount } from "../hook/debug";
 import { SchemaView } from "../utility/schema";
 import { Layouts } from "./layouts";
 
@@ -11,30 +10,7 @@ export abstract class View {
     constructor(layoutsWrapper: Layouts) {
         this.layoutsWrapper = layoutsWrapper;
 
-        this.name = this.constructor.name;
-
-        /*
-         * This process allows the extension of functionality
-         * without requiring a name difference between definition
-         * and caller. Component is both the overridden method
-         * and called method but can be extended backwards by
-         * the parent class.
-         * 
-         * In the future this can be used for baking in error
-         * recovery points and other debugging as a standard
-         * part of views.
-         */
-        let originalComponentDef = this.component;
-        this.component = (props) => {
-            let newProps = this._componentBaseInit(props);
-            return originalComponentDef(newProps);
-        }
-    }
-
-    _componentBaseInit(props) {
-        let renderCountFn = useRenderCount;
-        renderCountFn(this.name);
-        return props;
+        this.name = this.constructor.name; // this probably will always return 'View' with typescript
     }
 
     /**
