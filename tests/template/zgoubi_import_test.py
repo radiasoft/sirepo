0959c7d9a8c1ea605f18057593496e113589b060
robnagler <github@q33.us>
2022-11-01 22:25:43 +0000

    zgoubi_import_test

--- tests/template/zgoubi_import_test.py
+++ tests/template/zgoubi_import_test.py
@@ -22,9 +22,13 @@ def test_importer(import_req):
     with pkunit.save_chdir_work() as w:
         for fn in pkio.sorted_glob(pkunit.data_dir().join("*.dat")):
             error = None
+            req = import_req(fn)
             try:
-                data = zgoubi.import_file(import_req(fn), unit_test_mode=True)
-                sirepo.sim_data.get_class("zgoubi").fixup_old_data(data, qcall=None)
+                data = zgoubi.import_file(req, unit_test_mode=True)
+                sirepo.sim_data.get_class("zgoubi").fixup_old_data(
+                    data,
+                    qcall=req.qcall,
+                )
                 # TODO(pjm): easier way to convert nested dict to pkcollections.Dict?
                 data = pkcollections.json_load_any(pkjson.dump_pretty(data))
             except Exception as e:
@@ -33,7 +37,7 @@ def test_importer(import_req):
             if error:
                 actual = error
             else:
-                actual = zgoubi.python_source_for_model(data)
+                actual = zgoubi.python_source_for_model(data, qcall=req.qcall)
             outfile = fn.basename + ".txt"
             pkio.write_text(outfile, actual)
             e = pkunit.data_dir().join(outfile)
