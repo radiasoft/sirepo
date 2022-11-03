ca7ac774c9e2399220c37a0e901cfd8cd25a68a3
git-user <git-email>
2022-11-03 22:01:29 +0000

    Added test for binary file check

--- sirepo/template/radia.py
+++ sirepo/template/radia.py
@@ -578,9 +578,19 @@ def _build_undulator_objects(geom_objs, model, **kwargs):
     return _update_geom_from_undulator(geom_objs, model, **kwargs)
 
 
+def _is_binary(file_path):
+    textchars = bytearray({7, 8, 9, 10, 12, 13, 27} | set(range(0x20, 0x100)) - {0x7F})
+    is_binary_string = lambda bytes: bool(bytes.translate(None, textchars))
+    return is_binary_string(open(file_path, "rb").read(1024))
+
+
 def _create_stl_trimesh(file_path):
-    with open(file_path, "r") as f:
-        return trimesh.load(f, file_type="stl", force="mesh", process=True)
+    readParam = "r"
+    if _is_binary(file_path):
+        readParam = "rb"
+    with open(file_path, readParam) as f:
+        m = trimesh.exchange.stl.load_stl(file_obj=f)
+        return trimesh.Trimesh(vertices=m["vertices"], faces=m["faces"], process=True)
 
 
 # deep copy of an object, but with a new id
