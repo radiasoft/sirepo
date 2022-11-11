55adeede90e44e04c8d5662a4cfef81e995d8adc
Ben Gur <70293945+Benbenbenin0@users.noreply.github.com>
2022-11-11 08:17:12 -0700

    Fix #4848 Added STL Importing (#4967)

--- sirepo/package_data/static/js/radia.js
+++ sirepo/package_data/static/js/radia.js
@@ -3699,7 +3699,7 @@ SIREPO.app.directive('shapeButton', function(appState, geometry, panelState, plo
 
 SIREPO.app.directive('shapeSelector', function(appState, panelState, plotting, radiaService, utilities) {
 
-    const availableShapes = ['cuboid', 'cylinder', 'ell', 'cee', 'jay', 'extrudedPoints',];
+    const availableShapes = ['cuboid', 'cylinder', 'ell', 'cee', 'jay', 'extrudedPoints', 'stl'];
     const sel = new SIREPO.DOM.UISelect('', [
         new SIREPO.DOM.UIAttribute('data-ng-model', 'field'),
     ]);
