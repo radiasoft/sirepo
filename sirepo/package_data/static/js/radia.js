6aaf4e70916f5583483939250cdd7f7053b8cc11
mkeilman <mkeilman@radiasoft.net>
2022-11-14 20:51:50 -0700

    #5046 - updated css

--- sirepo/package_data/static/js/radia.js
+++ sirepo/package_data/static/js/radia.js
@@ -1226,7 +1226,7 @@ SIREPO.app.directive('bevelTable', function(appState, panelState, radiaService) 
         },
 
         template: `
-            <table class="table table-hover">
+            <table class="table radia-table-hover">
               <colgroup>
                 <col span="5" style="width: 20ex">
               </colgroup>
@@ -1362,7 +1362,7 @@ SIREPO.app.directive('filletTable', function(appState, panelState, radiaService)
             object: '=',
         },
         template: `
-            <table class="table table-hover">
+            <table class="table radia-table-hover">
               <colgroup>
                 <col span="4" style="width: 20ex">
               </colgroup>
@@ -1959,7 +1959,7 @@ SIREPO.app.directive('fieldIntegralTable', function(appState, panelState, plotti
                         </div>
                     </div>
                     <div class="panel-body">
-                        <table data-ng-if="hasPaths()" style="width: 100%; table-layout: fixed; margin-bottom: 10px" class="table table-hover">
+                        <table data-ng-if="hasPaths()" style="width: 100%; table-layout: fixed; margin-bottom: 10px" class="table radia-table-hover">
                           <colgroup>
                             <col style="width: 20ex">
                             <col>
@@ -2047,7 +2047,7 @@ SIREPO.app.directive('fieldPathTable', function(appState, geometry, panelState, 
             paths: '='
         },
         template: `
-            <table data-ng-if="hasPaths()" style="width: 100%; table-layout: fixed; margin-bottom: 10px" class="table table-hover">
+            <table data-ng-if="hasPaths()" style="width: 100%; table-layout: fixed; margin-bottom: 10px" class="table radia-table-hover">
               <colgroup>
                 <col style="width: 20ex">
                 <col style="width: 10ex">
@@ -2149,7 +2149,7 @@ SIREPO.app.directive('groupEditor', function(appState, radiaService) {
         },
         template: `
             <div style="height: 100px; overflow-y: scroll; overflow-x: hidden;">
-            <table style="table-layout: fixed;" class="table table-hover">
+            <table style="table-layout: fixed;" class="table radia-table-hover">
                 <tr style="background-color: lightgray;" data-ng-show="field.length > 0">
                   <th>Members</th>
                   <th></th>
@@ -2260,7 +2260,7 @@ SIREPO.app.directive('terminationTable', function(appState, panelState, radiaSer
         },
 
         template: `
-            <table class="table table-hover">
+            <table class="table radia-table-hover">
               <colgroup>
                 <col style="width: 20ex">
                 <col style="width: 20ex">
@@ -2395,7 +2395,7 @@ SIREPO.app.directive('transformTable', function(appState, panelState, radiaServi
             <div class="sr-object-table">
               <p class="lead text-center"><small><em>drag and drop {{ itemClass.toLowerCase() }}s or use arrows to reorder the list</em></small></p>
               <div style="overflow-y: scroll; overflow-x: hidden; height: 100px;">
-              <table class="table table-hover" style="width: 100%; height: 15%; table-layout: fixed;">
+              <table class="table radia-table-hover" style="width: 100%; height: 15%; table-layout: fixed;">
                 <tr data-ng-repeat="item in loadItems()">
                   <td data-ng-drop="true" data-ng-drop-success="dropItem($index, $data)" data-ng-drag-start="selectItem($data)">
                     <div class="sr-button-bar-parent pull-right"><div class="sr-button-bar"><button class="btn btn-info btn-xs"  data-ng-disabled="$index == 0" data-ng-click="moveItem(-1, item)"><span class="glyphicon glyphicon-arrow-up"></span></button> <button class="btn btn-info btn-xs" data-ng-disabled="$index == items.length - 1" data-ng-click="moveItem(1, item)"><span class="glyphicon glyphicon-arrow-down"></span></button> <button class="btn btn-info btn-xs sr-hover-button" data-ng-click="editItem(item)">Edit</button> <button data-ng-click="toggleExpand(item)" class="btn btn-info btn-xs"><span class="glyphicon" data-ng-class="{\'glyphicon-chevron-up\': isExpanded(item), \'glyphicon-chevron-down\': ! isExpanded(item)}"></span></button> <button data-ng-click="deleteItem(item)" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span></button></div></div>
