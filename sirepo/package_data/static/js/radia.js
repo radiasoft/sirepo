a2c4c56f3b0cf0339cb551a7114f592f15f2307d
mkeilman <mkeilman@radiasoft.net>
2022-12-07 10:24:16 -0700

    #4711 - ckp

--- sirepo/package_data/static/js/radia.js
+++ sirepo/package_data/static/js/radia.js
@@ -2413,9 +2413,9 @@ SIREPO.app.directive('transformTable', function(appState, panelState, radiaServi
         template: `
             <div data-toolbar="toolbarSections" data-item-filter="itemFilter" data-parent-controller="parentController"></div>
             <div class="sr-object-table">
-              <p class="lead text-center"><small><em>drag and drop {{ itemClass.toLowerCase() }}s or use arrows to reorder the list</em></small></p>
+              <p class="lead text-center"><small><em>use arrows to reorder {{ itemClass.toLowerCase() }}s</em></small></p>
               <div style="overflow-y: scroll; overflow-x: hidden; height: 100px;">
-              <table class="table radia-table-hover" style="width: 100%; height: 15%; table-layout: fixed;">
+              <table class="table table-striped radia-table-hover" style="width: 100%; height: 15%; table-layout: fixed;">
                 <tr data-ng-repeat="item in loadItems()">
                   <td data-ng-drop="true" data-ng-drop-success="dropItem($index, $data)" data-ng-drag-start="selectItem($data)">
                     <div class="sr-button-bar-parent pull-right"><div class="sr-button-bar"><button class="btn btn-info btn-xs"  data-ng-disabled="$index == 0" data-ng-click="moveItem(-1, item)"><span class="glyphicon glyphicon-arrow-up"></span></button> <button class="btn btn-info btn-xs" data-ng-disabled="$index == items.length - 1" data-ng-click="moveItem(1, item)"><span class="glyphicon glyphicon-arrow-down"></span></button> <button class="btn btn-info btn-xs sr-hover-button" data-ng-click="editItem(item)">Edit</button> <button data-ng-click="toggleExpand(item)" class="btn btn-info btn-xs"><span class="glyphicon" data-ng-class="{\'glyphicon-chevron-up\': isExpanded(item), \'glyphicon-chevron-down\': ! isExpanded(item)}"></span></button> <button data-ng-click="deleteItem(item)" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span></button></div></div>
@@ -2426,7 +2426,13 @@ SIREPO.app.directive('transformTable', function(appState, panelState, radiaServi
                     <div data-ng-show="isExpanded(item) && itemDetails(item)" style="color: #777; margin-left: 3em; white-space: pre-wrap">{{ itemDetails(item) }}</div>
                   </td>
                 </tr>
-                <tr><td style="height: 3em; text-align: center; color: #aaaaaa;" data-ng-drop="true" data-ng-drop-success="dropLast($data)"><em>*drop here*</em></td></tr>
+                <tr>
+                  <td>
+                    <b>Add Transform</b>
+                      <select class="form-control" data-ng-model="selectedItem" data-ng-options="item[0] as item[1] for item in toolbarItems" data-ng-change="addItem()"></select>
+                  </td>
+               </tr>
+                <!--<tr><td style="height: 3em; text-align: center; color: #aaaaaa;" data-ng-drop="true" data-ng-drop-success="dropLast($data)"><em>*drop here*</em></td></tr>-->
               </table>
             </div>
             </div>
@@ -2460,8 +2466,19 @@ SIREPO.app.directive('transformTable', function(appState, panelState, radiaServi
                 return $scope.items.indexOf(data);
             }
 
+            $scope.addLayer = () => {
+                const m = appState.setModelDefaults({}, stringsService.lcfirst($scope.selectedLayer));
+                m.layer = $scope.selectedLayer;
+                neuralNet.push(m);
+                $scope.selectedLayer = '';
+            };
+
             $scope.addItem = item => {
-                $scope.editItem(item, true);
+                if (! $scope.selectedItem) {
+                    return;
+                }
+
+                //$scope.editItem(item, true);
             };
 
             $scope.deleteItem = item => {
