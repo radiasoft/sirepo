097296f0fb87b1b4813ed835a5a06e6bae5e597e
Benbenbein0 <bengur@radiasoft.com>
2022-12-09 00:07:14 +0000

    formatter

--- sirepo/auth/ldap.py
+++ sirepo/auth/ldap.py
@@ -12,32 +12,34 @@ AUTH_METHOD = "ldap"
 #: User can see it
 AUTH_METHOD_VISIBLE = True
 
+
 class API(sirepo.quest.API):
     @sirepo.quest.Spec("require_cookie_sentinel")
     def api_authLdapAuthorized(self):
         """Python-LDAP client authentication
         Bind to server and authenticate
         """
-        #Test User Credentials:
-        #username: Archimedes of Syracuse
-        #password: eureka
+        # Test User Credentials:
+        # username: Archimedes of Syracuse
+        # password: eureka
 
-        LDAP_SERVER = 'ldap://10.10.10.10:389'
-        BASE_DN = 'dc=example,dc=com'
-        OBJECT_TO_SEARCH = 'objectClass=inetOrgPerson'
-        ATTRIBUTES_TO_SEARCH = ['uid']
+        LDAP_SERVER = "ldap://10.10.10.10:389"
+        BASE_DN = "dc=example,dc=com"
+        OBJECT_TO_SEARCH = "objectClass=inetOrgPerson"
+        ATTRIBUTES_TO_SEARCH = ["uid"]
 
         req = self.parse_post(type=False)
         pkdp("req: {}", req.req_data.username)
 
-        #initialize our connect to ldap-server
+        # initialize our connect to ldap-server
         connect = ldap.initialize(LDAP_SERVER)
 
-        #bind to server and login
-        connect.simple_bind_s(self._get_login(req.req_data.username), req.req_data.password)
+        # bind to server and login
+        connect.simple_bind_s(
+            self._get_login(req.req_data.username), req.req_data.password
+        )
 
         return PKDict()
 
     def _get_login(self, username):
-        return 'cn=' + username + ',ou=users,dc=example,dc=com'
-    
+        return "cn=" + username + ",ou=users,dc=example,dc=com"
