b03afa094fa2bfdd6842d6dd7eaec0a90035380c
git-user <git-email>
2022-12-15 20:54:05 +0000

    checkpoint

--- etc/run-auth-ldap.sh
+++ etc/run-auth-ldap.sh
@@ -10,20 +10,44 @@ export SIREPO_AUTH_METHODS='email:guest:ldap'
 export PYKERN_PKDEBUG_WANT_PID_TIME=1
 
 # primary cause of failed wheel generation
-exec pip install --upgrade pip
+pip install --upgrade pip
+echo "upgraded pip"
 
 # install prerequisites
-exec yum groupinstall "Development tools"
-exec yum install openldap-devel python-devel
+sudo yum groupinstall "Development tools"
+sudo yum install openldap-devel python-devel
+echo "installed prerequisites"
 
 # secondary cause of failed wheel generation
-exec pip install --upgrade setuptools wheel
+pip install --upgrade setuptools wheel
+echo "installed wheel setup"
 
 # main installs
-exec pip install python-ldap
-exec yum -y install openldap-clients openldap-servers
+pip install python-ldap
+sudo yum -y install openldap-clients openldap-servers
+echo "installed ldap"
 
-# create user
-exec sudo ldapadd -f vagrant_user.ldif -x -D cn=admin,dc=example,dc=com -w supersecret
+sudo systemctl start slapd
+sudo systemctl enable slapd
 
-exec sirepo service http
+# assign default root and appended distunguished name
+sudo ldapmodify -Y EXTERNAL -H ldapi:/// -f /home/vagrant/src/radiasoft/sirepo/etc/ldap_ldifs/config.ldif
+
+# add admin password to backend
+sudo ldapmodify -Y EXTERNAL -H ldapi:/// -f /home/vagrant/src/radiasoft/sirepo/etc/ldap_ldifs/admin_password.ldif
+
+# allow admin database access
+sudo ldapmodify -Y EXTERNAL -H ldapi:/// -f /home/vagrant/src/radiasoft/sirepo/etc/ldap_ldifs/admin_access.ldif
+
+# sudo ldapadd -f /home/vagrant/src/radiasoft/sirepo/etc/ldap_ldifs/test_dn.ldif -D cn=admin,dc=example,dc=com -w vagrant
+
+# create organizational unit (ou) for users (assigns permissions)
+sudo ldapadd -f /home/vagrant/src/radiasoft/sirepo/etc/ldap_ldifs/test_ou.ldif -D cn=admin,dc=example,dc=com -w vagrant
+
+# add prequisite schema to core schema
+sudo ldapadd -Y EXTERNAL -H ldapi:// -f /etc/openldap/schema/cosine.ldif
+sudo ldapadd -Y EXTERNAL -H ldapi:// -f /etc/openldap/schema/inetorgperson.ldif
+sudo ldapadd -Y EXTERNAL -H ldapi:// -f /etc/openldap/schema/nis.ldif
+
+# add user to ou
+sudo ldapadd -f /home/vagrant/src/radiasoft/sirepo/etc/ldap_ldifs/test_user.ldif -x -D cn=admin,dc=example,dc=com -w vagrant
