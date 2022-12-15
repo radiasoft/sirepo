17bac8fdeb36bea5a2b38d08b19f3ff6db8bfa77
git-user <git-email>
2022-12-15 23:18:52 +0000

    formatter

--- sirepo/auth/ldap.py
+++ sirepo/auth/ldap.py
@@ -14,6 +14,7 @@ AUTH_METHOD_VISIBLE = True
 
 _cfg = None
 
+
 class API(sirepo.quest.API):
     @sirepo.quest.Spec("require_cookie_sentinel")
     def api_authLdapAuthorized(self):
@@ -23,34 +24,33 @@ class API(sirepo.quest.API):
         p = req.req_data.password
 
         c = ldap.initialize(_cfg.ldap_server)
-        
-        #TODO(BG) Get char length checking working on api side
-        #if(len(u) > 256 or len(p > 256)):
-            #raise Exception("login user/pass greater than 256 chars")
-        
-        c.simple_bind_s(
-            self._get_escaped(u), p
-        )
+
+        # TODO(BG) Get char length checking working on api side
+        # if(len(u) > 256 or len(p > 256)):
+        # raise Exception("login user/pass greater than 256 chars")
+
+        c.simple_bind_s(self._get_escaped(u), p)
 
         return PKDict()
-    
+
     # escapes special chars for ldap validity
     def _get_escaped(self, username):
-        e = ['+', ';', ',', '\\', '\"', '<', '>', '#']
+        e = ["+", ";", ",", "\\", '"', "<", ">", "#"]
         v = ""
-            
+
         for c in username:
-            if(c in e):
-                v += '\\' + c
+            if c in e:
+                v += "\\" + c
             else:
                 v += c
         return "mail=" + v + _cfg.base_dn
-    
+
+
 def init_apis(*args, **kwargs):
     global _cfg
-    
+
     pkdp("calling to _cfg")
     _cfg = pkconfig.init(
         ldap_server=("ldap://192.168.121.164:389", str, " ldap address:port"),
-        base_dn=(",ou=users,dc=example,dc=com", str, "base dn")
+        base_dn=(",ou=users,dc=example,dc=com", str, "base dn"),
     )
