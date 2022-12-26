40641b18fdd024a931faeacc6bdd090e42795ba7
robnagler <github@q33.us>
2022-12-26 21:50:40 +0000

    ckp

--- etc/run-auth-bluesky.sh
+++ etc/run-auth-bluesky.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 set -eou pipefail
-# See run-auth-moderate.sh for using local mail delivery.
-# This echo's the URL to the logs (SIREPO_SMTP_SERVER=dev)
-export SIREPO_AUTH_BLUESKY_SECRET=bluesky-secret
-export SIREPO_AUTH_METHODS=email:bluesky
-export SIREPO_FROM_EMAIL=support@radiasoft.net
-export SIREPO_FROM_NAME='RadiaSoft Support'
-export SIREPO_SMTP_PASSWORD=vagrant
-# POSIT: same as sirepo.smtp.DEV_SMTP_SERVER
-export SIREPO_SMTP_SERVER=dev
-export SIREPO_SMTP_USER=vagrant
-cat <<EOF
-To test, you need a simulation:
 
-curl -H 'Content-Type: application/json' -D - -X POST http://localhost:8000/auth-bluesky-login -d '{"simulationId": "kRfyDC2q", "simulationType": "srw"}'
-
-EOF
-exec sirepo service http
+SIREPO_AUTH_METHODS=bluesky \
+    SIREPO_AUTH_BLUESKY_SECRET=bluesky \
+    bash -l etc/run-jupyterhub.sh
