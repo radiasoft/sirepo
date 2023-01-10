80cba50f06f661802e8969912f29c160052b66c1
gurhar1133 <ghns1133@gmail.com>
2023-01-10 17:43:15 +0000

    Fix #4844: stop no reason no UI

--- sirepo/package_data/static/js/sirepo-components.js
+++ sirepo/package_data/static/js/sirepo-components.js
@@ -3636,7 +3636,7 @@ SIREPO.app.directive('moderationRequest', function(appState, errorService, panel
               <label for="requestAccessExplanation">Please describe your reason for requesting access:</label>
               <textarea data-ng-show="!submitted" data-ng-model="data.reason" id="requestAccessExplanation" class="form-control" rows="4" cols="50" required></textarea>
             </div>
-            <button data-ng-show="!submitted" type="submit" class="btn btn-primary" data-ng-click="submitRequest()">Submit aaa</button>
+            <button data-ng-show="!submitted" type="submit" class="btn btn-primary" data-ng-click="submitRequest()">Submit</button>
           </form>
           <div data-ng-show="submitted">Response submitted.</div>
         `,
@@ -3650,6 +3650,9 @@ SIREPO.app.directive('moderationRequest', function(appState, errorService, panel
                     }
                     $scope.submitted = true;
                 };
+                if (!$scope.data.reason) {
+                    return;
+                }
                 requestSender.sendRequest(
                     'saveModerationReason',
                     handleResponse,
