276dc3eb5b6607cebac829c9a315ec77d05afa6e
git-user <git-email>
2023-01-19 21:33:43 +0000

    checkpoint, mostly ldap.py changes

--- sirepo/auth/ldap.py
+++ sirepo/auth/ldap.py
@@ -1,8 +1,9 @@
-"""LDAP login
+"""LDAP authentication support
 :copyright: Copyright (c) 2018-2022 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
 from pykern import pkconfig
+from pykern import pkinspect
 from pykern.pkdebug import pkdlog, pkdp
 from pykern.pkcollections import PKDict
 import ldap
@@ -14,30 +15,24 @@ AUTH_METHOD_VISIBLE = True
 
 _cfg = None
 
+#: module handle
+this_module = pkinspect.this_module()
+
 class API(sirepo.quest.API):
     @sirepo.quest.Spec("require_cookie_sentinel")
-    def api_authLdapAuthorized(self):
-
-        # TODO(BG): pass simulation type as method parameter to assign to 'type', currently passing simulation_type to req and leaving 'type' False
-        req = self.parse_post(type=False)
-        u = req.req_data.username
-        p = req.req_data.password
-
+    def api_authLdapLogin(self):
+        req = self.parse_post()
         c = ldap.initialize(_cfg.ldap_server)
-
-        if(len(u) > 256 or len(p) > 256):
-            raise Exception("login user/pass greater than 256 chars")
-
-        c.simple_bind_s(self._get_escaped(u), p)
-        
-        raise sirepo.util.Redirect(sirepo.uri.local_route(req.req_data.simulation_type))
-        return PKDict()
+        if(len(req.req_data.username) > 256 or len(req.req_data.password) > 256):
+            raise AssertionError("login user/pass greater than 256 chars")
+        c.simple_bind_s(self._get_escaped(req.req_data.username), req.req_data.password)
+        #TODO(BG): Search auth.db (LDAP email? model?) before registering new user, check with Rob/Evan but likely need to determine model and make None if new user
+        self.auth.login(this_module, sim_type=req.type, want_redirect=True)
 
     # escapes special chars for ldap validity
     def _get_escaped(self, username):
         e = ["+", ";", ",", "\\", '"', "<", ">", "#"]
         v = ""
-
         for c in username:
             if c in e:
                 v += "\\" + c
@@ -48,7 +43,6 @@ class API(sirepo.quest.API):
 
 def init_apis(*args, **kwargs):
     global _cfg
-
     pkdp("calling to _cfg")
     _cfg = pkconfig.init(
         ldap_server=("ldap://192.168.121.164:389", str, " ldap address:port"),
