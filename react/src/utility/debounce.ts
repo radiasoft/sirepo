db096f444ed6f31a119d2257b2d20610bb7b2469
garsuga <gsugarbaker@gmail.com>
2023-02-11 00:14:27 +0000

    refactor debounce

--- react/src/utility/debounce.ts
+++ react/src/utility/debounce.ts
@@ -1,12 +1,10 @@
-//TODO(pjm): use a library?
-export function debounce(fn, ms) {
-    let timer;
+export function debounce(fn: Function, ms: number) {
+    let timer: number;
     // this must be a function() for "this" and "arguments" to work below
     return function(...args) {
         clearTimeout(timer);
         timer = setTimeout(_ => {
-            timer = null;
-            //fn.apply(this, arguments);
+            timer = undefined;
             fn(...args);
         }, ms);
     };
