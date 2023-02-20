93de9e31ab133095bd80a906c53332a688fbd95c
mkeilman <mkeilman@radiasoft.net>
2023-02-20 10:47:40 -0700

    #5491 - ckp

--- tests/job_test.py
+++ tests/job_test.py
@@ -7,7 +7,6 @@
 from __future__ import absolute_import, division, print_function
 import pytest
 import os
-from pykern.pkcollections import PKDict
 
 # TODO(e-carlin): Tests that need to be implemented
 #   - agent never starts
