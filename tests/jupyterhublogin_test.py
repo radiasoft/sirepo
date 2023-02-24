5d494ff9d0d23412cfe2986ee8a4a3d884c9e9a8
Evan Carlin <evan@carlin.com>
2023-02-24 05:22:07 +0000

    add test

---
+++ tests/jupyterhublogin_test.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""Test jupyterhublogin
+
+:copyright: Copyright (c) 2023 RadiaSoft LLC.  All Rights Reserved.
+:license: http://www.apache.org/licenses/LICENSE-2.0.html
+"""
+import pytest
+import os
+
+
+def setup_module(module):
+    os.environ.update(
+        SIREPO_FEATURE_CONFIG_PROPRIETARY_SIM_TYPES="jupyterhublogin",
+    )
+
+
+def test_logout(auth_fc):
+    # Clears third party (jupyterhub) cookies
+    fc = auth_fc
+
+    from pykern.pkdebug import pkdp
+
+    fc.sr_email_login("a@b.c")
+    fc.sr_logout()
