68202bf6fa6467564e24b4ed719180924d36f6ab
Evan Carlin <evan@carlin.com>
2023-02-27 22:31:44 +0000

    feature_config.is_fedora_36

--- sirepo/feature_config.py
+++ sirepo/feature_config.py
@@ -110,6 +110,26 @@ def proprietary_sim_types():
     )
 
 
+def _is_fedora_36():
+    from pykern import pkio
+    import subprocess
+
+    p = pkio.py_path("/etc/os-release")
+    if not p.check():
+        return False
+    for f, v in ("${ID,,}", "fedora"), ("$VERSION_ID", "36"):
+        # SECURITY: shell=True is ok because there is no input from the user
+        try:
+            o = subprocess.check_output(
+                (f'source "{p}"; echo "{f}"'), shell=True, text=True
+            ).strip()
+            if o != v:
+                return False
+        except subprocess.CalledProcessError:
+            return False
+    return True
+
+
 def _init():
     from pykern import pkconfig
     from pykern import pkio
@@ -222,6 +242,7 @@ def _init():
             s.add(v[1])
     _cfg.sim_types = frozenset(s)
     _check_packages(_cfg.package_path)
+    _cfg.is_fedora_36 = _is_fedora_36()
     return _cfg
 
 
