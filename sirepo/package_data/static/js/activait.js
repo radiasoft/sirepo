672b8565541eab97211e2b8e68297d64d86bdaac
gurhar1133 <ghns1133@gmail.com>
2023-03-07 00:21:13 +0000

    Fix #5383: hide partition method for isImage

--- sirepo/package_data/static/js/activait.js
+++ sirepo/package_data/static/js/activait.js
@@ -2116,9 +2116,8 @@ SIREPO.viewLogic('mlModelView', function(appState, panelState, requestSender, $s
 });
 
 SIREPO.viewLogic('partitionView', function(activaitService, appState, panelState, $scope) {
-
     function updatePartitionMethod() {
-        panelState.showField('partition', 'method', activaitService.isAppMode('regression'));
+        panelState.showField('partition', 'method', activaitService.isAppMode('regression') && !activaitService.isImageData());
         const partition = appState.models.partition;
         ['training', 'testing'].forEach(f => {
             panelState.showField(
