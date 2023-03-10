4e30f7bb5ab5aba7b061ec053136af3683de515b
Paul Moeller <moeller@radiasoft.net>
2023-03-10 15:16:16 -0700

    fix #5462 multipanel as png (#5547)
    - Download python source link text now configurable by application
    - Added simple Column layout for use by mulitipanel
    - Multipanel now shows a download PNG button
    - FieldList layout now has an option shown and heading config

--- react/src/layout/layouts.ts
+++ react/src/layout/layouts.ts
@@ -14,6 +14,7 @@ import { LayoutType, Layout } from "./layout";
 import { SchemaLayout } from "../utility/schema";
 import { TextLayout } from "./text";
 import { HorizontalStackLayout, VerticalStackLayout } from "./arrange/stack";
+import { ColumnLayout } from "./arrange/column";
 import { WaterfallLayout } from "./arrange/waterfall";
 
 
@@ -24,7 +25,7 @@ class LayoutWrapper {
         fieldList: LayoutWithSpacing(FieldListLayout),
         fieldTable: LayoutWithSpacing(FieldGridLayout),
         panel: LayoutWithFormController(PanelLayout),
-        multiPanel: MultiPanelLayout,
+        multiPanel: LayoutWithDownloadButton(MultiPanelLayout),
         navbarModalButton: LayoutWithFormController(NavBarModalButton),
         autoRunReport: AutoRunReportLayout,
         manualRunReport: ManualRunReportLayout,
@@ -36,6 +37,7 @@ class LayoutWrapper {
         table: TableFromApi,
         startSimulation: SimulationStartLayout,
         text: TextLayout,
+        column: ColumnLayout,
         hStack: HorizontalStackLayout,
         vStack: VerticalStackLayout,
         waterfall: WaterfallLayout
