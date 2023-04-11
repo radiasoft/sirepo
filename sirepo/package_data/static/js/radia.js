2575026a8431f04947f0bb3db45b4fb9a7f63c82
Michael Keilman <30352290+mkeilman@users.noreply.github.com>
2023-04-10 19:10:52 -0600

    Fix #5597 radia visualization (#5648)

--- sirepo/package_data/static/js/radia.js
+++ sirepo/package_data/static/js/radia.js
@@ -1597,9 +1597,10 @@ SIREPO.app.directive('fieldIntegralTable', function(appState, panelState, plotti
                     $scope.integrals = data;
                 }, true);
             }
-
-            $scope.$on('radiaViewer.loaded', updateTable);
+            
             $scope.$on('fieldPaths.saved', updateTable);
+
+            updateTable();
         },
     };
 });
@@ -2801,20 +2802,10 @@ SIREPO.app.directive('radiaViewer', function(appState, errorService, frameCache,
                 }
             });
 
-            $scope.$on('magnetDisplay.changed',  (e, d) => {
-                // does not seem the best way...
-                let interval = null;
-                interval = $interval(function() {
-                    if (interval) {
-                        $interval.cancel(interval);
-                        interval = null;
-                    }
-                    // only fetch if we need different view or field
-                    if (didDisplayValsChange()) {
-                        updateViewer();
-                    }
-                }, 500, 1);
-
+            $scope.$on('magnetDisplay.saved', () => {
+                if (didDisplayValsChange()) {
+                    updateViewer();
+                }
             });
 
             $scope.$on('solve.complete', (e, d) => {
