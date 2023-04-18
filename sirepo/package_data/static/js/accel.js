7375471a215d1aa753e5fba543d65b12f55ee717
git-user <git-email>
2023-04-18 19:58:20 +0000

    for #5655 static epics value fields

--- sirepo/package_data/static/js/accel.js
+++ sirepo/package_data/static/js/accel.js
@@ -3,47 +3,107 @@
 var srlog = SIREPO.srlog;
 var srdbg = SIREPO.srdbg;
 
+SIREPO.app.config(function() {
+    SIREPO.appFieldEditors += `
+        <div data-ng-switch-when="EpicsFloat">
+          <div class="col-sm-3">
+            <input data-string-to-number="" data-ng-model="model[field]" data-min="info[4]" data-max="info[5]" class="form-control" style="text-align: right" data-lpignore="true" required />
+          </div>
+          <div data-epics-value="" data-model-name="modelName" data-field="field"></div>
+        </div>
+        <div data-ng-switch-when="EpicsInteger">
+          <div class="col-sm-3">
+            <input data-string-to-number="integer" data-ng-model="model[field]" data-min="info[4]" data-max="info[5]" class="form-control" style="text-align: right" data-lpignore="true" required />
+          </div>
+          <div data-epics-value="" data-model-name="modelName" data-field="field"></div>
+        </div>
+        <div data-ng-switch-when="ReadOnlyFloat" class="col-sm-3">
+          <div class="text-right" data-epics-value="" data-model-name="modelName" data-field="field"></div>
+        </div>
+        <div data-ng-switch-when="EpicsEnum">
+          <div class="col-sm-3">
+            <select class="form-control" data-ng-model="model[field]" data-ng-options="item[0] as item[1] for item in enum[field]"></select>
+          </div>
+          <div data-epics-value="" data-model-name="modelName" data-field="field"></div>
+        </div>
+    `;
+});
+
 SIREPO.app.factory('accelService', function(appState) {
     const self = {};
+    let epicsData;
     self.computeModel = () => 'animation';
+
+    self.getEpicsValue = (modelName, field) => {
+        if (epicsData) {
+            return epicsData[`${modelName}:${field}`];
+        }
+        return null;
+    };
+
+    self.setEpicsData = epics => {
+        epicsData = epics;
+    };
+
     appState.setAppService(self);
     return self;
 });
 
 
-SIREPO.app.controller('accelController', function (appState, panelState, errorService, persistentSimulation, $scope, $rootScope) {
+SIREPO.app.controller('accelController', function (accelService, appState, panelState, errorService, persistentSimulation, $scope, $rootScope) {
     const self = this;
+    let prevEpicsData;
     self.simScope = $scope;
+
+    function fieldType(modelName, field) {
+        if (SIREPO.APP_SCHEMA.model[modelName] && SIREPO.APP_SCHEMA.model[modelName][field]) {
+            return SIREPO.APP_SCHEMA.model[modelName][field][1];
+        }
+        return null;
+    }
+
     self.simHandleStatus = data => {
-        if (data.epicsData) {
+        if (data.epicsData && Object.keys(data.epicsData).length) {
             if (data.epicsData.error) {
                 errorService.alertText(
                     data.epicsData.error
                 );
                 return;
             }
-            const updated = [];
+            accelService.setEpicsData(data.epicsData);
+            const changed = [];
             for (const f in data.epicsData) {
-                let modelName, field;
-                [modelName, field] = f.split(':');
+                let [modelName, field] = f.split(':');
                 const m = appState.models[modelName];
-                if (m) {
-                    if (field in m) {
-                        if (m[field] != data.epicsData[f]) {
-                            m[field] = data.epicsData[f];
-                            if (! updated.includes(modelName)) {
-                                updated.push(modelName);
-                            }
+                if (m && fieldType(modelName, field) == 'ReadOnlyFloatArray') {
+                    if (
+                        (
+                            prevEpicsData
+                            && appState.deepEquals(prevEpicsData[f], m[field])
+                            && ! appState.deepEquals(data.epicsData[f], m[field])
+                        )
+                        || ! prevEpicsData
+                    ) {
+                        m[field] = data.epicsData[f];
+                        appState.applicationState()[modelName][field] = m[field];
+                        changed.push(f);
+                    }
+                }
+            }
+            for (const f of changed) {
+                for (const [r, v] of Object.entries(SIREPO.APP_SCHEMA.constants.epicsPlots)) {
+                    for (const [dim, field] of Object.entries(v)) {
+                        if (field == f) {
+                            $scope.$broadcast('sr-accel-waveform', [
+                                r,
+                                data.epicsData[v.x].slice(1),
+                                data.epicsData[v.y].slice(1),
+                            ]);
                         }
                     }
                 }
             }
-            if (updated) {
-                appState.saveQuietly(updated);
-                const x = appState.models.MTEST.TimeBase.slice(1);
-                const y = appState.models.MTEST.Waveform.slice(1);
-                $scope.$broadcast('sr-accel-waveform', [x, y]);
-            }
+            prevEpicsData = data.epicsData;
         }
     };
 
@@ -88,10 +148,28 @@ SIREPO.app.directive('appHeader', function(appState, panelState) {
     };
 });
 
+SIREPO.app.directive('epicsValue', function(accelService) {
+    return {
+        restrict: 'A',
+        scope: {
+            modelName: '=',
+            field: '=',
+            },
+        template: `
+          <div class="form-control-static">{{ accelService.getEpicsValue(modelName, field) }}</div>
+        `,
+        controller: function($scope) {
+            $scope.accelService = accelService;
+        },
+    };
+});
+
 SIREPO.app.directive('waveformLoader', function(appState, panelState) {
     return {
         restrict: 'A',
-        scope: {},
+        scope: {
+            modelName: '@',
+        },
         transclude: true,
         template: `
             <div>
@@ -102,8 +180,8 @@ SIREPO.app.directive('waveformLoader', function(appState, panelState) {
             let plotData, plotScope;
 
             function updatePlot() {
-                const x = plotData[0];
-                const y = plotData[1];
+                const x = plotData[1];
+                const y = plotData[2];
                 plotData = null;
                 if (plotScope) {
                     plotScope.clearData();
@@ -132,14 +210,17 @@ SIREPO.app.directive('waveformLoader', function(appState, panelState) {
 
             $scope.$on('sr-plotLinked', event => {
                 plotScope = event.targetScope;
+                plotScope.isClientOnly = true;
                 if (plotData) {
                     updatePlot();
                 }
             });
             $scope.$on('sr-accel-waveform', (event, data) => {
-                plotData = data;
-                if (plotScope) {
-                    updatePlot();
+                if (data[0] == $scope.modelName) {
+                    plotData = data;
+                    if (plotScope) {
+                        updatePlot();
+                    }
                 }
             });
         },
