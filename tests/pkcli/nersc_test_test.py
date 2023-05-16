7bc72b31fade22ad46b147a9f9dac3a042ef540d
gurhar1133 <ghns1133@gmail.com>
2023-05-16 16:16:04 +0000

    Fix #5810: test update

--- tests/pkcli/nersc_test_test.py
+++ tests/pkcli/nersc_test_test.py
@@ -8,14 +8,30 @@ import pytest
 import pykern.pkio
 import pykern.pkunit
 import sirepo.pkcli.nersc_test
+import pykern.pksubprocess
 
 
 def test_sequential():
     w = pykern.pkunit.work_dir()
-    d = pykern.pkunit.data_dir()
     with pykern.pkio.save_chdir(w):
-        sirepo.pkcli.nersc_test.sequential()
+        pykern.pksubprocess.check_call_with_signals(
+            ["sirepo", "nersc_test", "sequential"]
+        )
+        o = list(sorted([w.bestrelpath(f) for f in pykern.pkio.walk_tree(w)]))
+    pykern.pkunit.pkeq(
+        o,
+        [
+            "sirepo_run_dir/in.json",
+            "sirepo_run_dir/nersc_sequential.log",
+            "sirepo_run_dir/out.json",
+            "sirepo_run_dir/parameters.py",
+            "sirepo_run_dir/res_int_se.dat",
+            "sirepo_run_dir/run.log",
+            "sirepo_run_dir/sequential_test.sh",
+            "sirepo_run_dir/sequential_test.sh.jinja",
+        ],
+    )
     pykern.pkunit.file_eq(
         expect_path=w.join("sirepo_run_dir").join("sequential_test.sh"),
-        actual_path=d.join("expected_sequential_test.sh"),
+        actual_path=pykern.pkunit.data_dir().join("expected_sequential_test.sh"),
     )
