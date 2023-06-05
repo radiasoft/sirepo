cb935871604c49011711dd4f53210d858722955b
robnagler <github@q33.us>
2023-06-05 21:42:45 +0000

    Review comments as code: - Simplified nersc_test_test.py to not use a class. There's only one   function required so not necessary to hold state in an instance. - Used subprocess instead of pksubprocess, because interface is better   for tests. pksubprocess is for wrapping processes that run for a   long time and may be signaled to terminate. - Use pkcli/argh named argument so don't have to parse arguments. - Avoid passing data through when there is an instance, and instead   assign to instance variable

--- sirepo/pkcli/nersc_test.py
+++ sirepo/pkcli/nersc_test.py
@@ -8,15 +8,15 @@ from pykern.pkcollections import PKDict
 from pykern.pkdebug import pkdc, pkdexc, pkdp, pkdlog
 import os
 import pykern.pkio
-import pykern.pkjson
-import pykern.pksubprocess
 import pykern.pkjinja
-import sirepo.sim_data
+import pykern.pkjson
 import sirepo.const
 import sirepo.resource
+import sirepo.sim_data
+import subprocess
 
 
-def sequential(*args):
+def sequential(pkunit_deviance=None):
     """Test sequential process for use by NERSC inside SHIFTER
 
     Simulates the operation that Sirepo does when it executes a
@@ -24,12 +24,15 @@ def sequential(*args):
 
     On success, will return (output) ``nersc_test.sequential PASS``
 
+    Args:
+        pkunit_deviance (str): used only by internal unit test to test deviance case
+
     Returns:
         str: PASS or fail with diagnostic information
     """
     s = _Sequential()
     try:
-        s.prepare(args)
+        s.prepare(pkunit_deviance)
         s.execute()
         return "nersc_test.sequential PASS"
     except Exception as e:
@@ -45,21 +48,25 @@ class _Sequential(PKDict):
     RUN_DIR = "sirepo_run_dir"
     RUN_FILE = "sequential_run.sh"
 
-    def prepare(self, args=None):
+    def prepare(self, pkunit_deviance):
+        self.pkunit_deviance = pkunit_deviance
         self.run_dir = pykern.pkio.py_path(self.RUN_DIR)
         pykern.pkio.unchecked_remove(self.run_dir)
         self.run_dir.ensure(dir=True)
         self.result_file = self.run_dir.join(self.RESULT_FILE)
         self.user = sirepo.const.MOCK_UID
-        self.job_cmd_file = self._render_resource(self.JOB_CMD_FILE, args=args)
+        # job_cmd_file must be first, because used by _render_resource
+        self.job_cmd_file = self._job_cmd_file()
         self.run_file = self._render_resource(self.RUN_FILE)
 
     def execute(self):
-        pykern.pksubprocess.check_call_with_signals(
-            ["bash", self.run_file],
-            output=str(self.result_file),
-        )
-        self.result_text = pykern.pkio.read_text(self.result_file)
+        p = subprocess.run(["bash", self.run_file], capture_output=True, text=True)
+        if p.returncode != 0:
+            raise RuntimeError(
+                f"unexpected returncode={p.returncode} stderr={p.stderr}"
+            )
+        self.result_file.write(p.stdout)
+        self.result_text = p.stdout
         self.result_parsed = pykern.pkjson.load_any(self.result_text)
         if self.result_parsed.state != "completed":
             raise RuntimeError(f"unexpected result state={self.result_parsed.state}")
@@ -68,6 +75,11 @@ class _Sequential(PKDict):
         return sirepo.resource.file_path(
             self.RESOURCE_DIR + filename + pykern.pkjinja.RESOURCE_SUFFIX
         )
+
+    def _job_cmd_file(self):
+        if self.pkunit_deviance:
+            return pykern.pkio.py_path(self.pkunit_deviance)
+        return self._render_resource(self.JOB_CMD_FILE)
 
     def __str__(self):
         res = "Internal state:\n"
@@ -80,11 +92,10 @@ class _Sequential(PKDict):
                 res += "result_text=" + self.result_text
         return res
 
-    def _render_resource(self, filename, args=None):
+    def _render_resource(self, filename):
         res = self.run_dir.join(filename)
-        p = self._valid_args(args)
         pykern.pkjinja.render_file(
-            p if p else self._file_path(filename),
+            self._file_path(filename),
             PKDict(
                 job_cmd_file=self.get("job_cmd_file"),
                 run_dir=self.run_dir,
@@ -93,11 +104,3 @@ class _Sequential(PKDict):
             output=res,
         )
         return res
-
-    def _valid_args(self, args):
-        if not args:
-            return None
-        a = args[0].split("=")
-        if a[0] == "job_cmd_in_path":
-            return a[1]
-        raise RuntimeError(f"Invalid argument {a[0]} passed to sequential")
--- tests/pkcli/nersc_test_test.py
+++ tests/pkcli/nersc_test_test.py
@@ -7,57 +7,39 @@
 import pytest
 
 
-class _SequentialTest:
-    def __init__(self):
-        from pykern import pkunit
-
-        self._work_dir = pkunit.work_dir()
-        self._data_dir = pkunit.data_dir()
-        self._output = "result.log"
-
-    def conformance(self):
-        self._test(
-            "nersc_test.sequential PASS",
-            lambda s: s.strip(),
-        )
-
-    def deviance(self):
-        self._test(
-            "nersc_test sequential fail: error=unexpected result state=error",
-            lambda s: s.split("\n")[0],
-            cmd_arg=f"job_cmd_in_path={self._data_dir.join('failure_in.json.jinja')}",
-        )
-
-    def _test(self, expect, out_fn, cmd_arg=None):
-        from pykern import pkio
-        from pykern import pkunit
-
-        self._exec_cmd(cmd_arg)
-        pkunit.pkeq(
-            out_fn(pkio.read_text(self._work_dir.join(self._output))),
-            expect,
-        )
-
-    def _cmd(self, cmd_arg=None):
-        c = ["sirepo", "nersc_test", "sequential"]
-        if cmd_arg:
-            c.append(cmd_arg)
-        return c
-
-    def _exec_cmd(self, cmd_arg=None):
-        from pykern import pksubprocess
-        from pykern import pkio
-
-        with pkio.save_chdir(self._work_dir):
-            pksubprocess.check_call_with_signals(
-                self._cmd(cmd_arg),
-                output=self._output,
-            )
-
-
 def test_conformance():
-    _SequentialTest().conformance()
+    _do(
+        "nersc_test.sequential PASS",
+        lambda s: s.strip(),
+        [],
+    )
 
 
 def test_deviance():
-    _SequentialTest().deviance()
+    from pykern import pkunit
+
+    _do(
+        "nersc_test sequential fail: error=unexpected result state=error",
+        lambda s: s.split("\n")[0],
+        [f"--pkunit-deviance={pkunit.data_dir().join('failure_in.json.jinja')}"],
+    )
+
+
+def _do(expect, out_fn, cmd_arg):
+    import subprocess
+    from pykern import pkunit
+
+    with pkunit.save_chdir_work():
+        p = subprocess.run(
+            ["sirepo", "nersc_test", "sequential"] + cmd_arg,
+            capture_output=True,
+            text=True,
+        )
+        pkunit.pkeq(
+            expect,
+            out_fn(p.stdout),
+            "returncode={}\nstdout={}\nstderr={}",
+            p.returncode,
+            p.stdout,
+            p.stderr,
+        )
