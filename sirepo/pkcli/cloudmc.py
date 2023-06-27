c9b5f292ebf6eeab78fa43b5df26a3c9cbe45242
moellep <moeller@radiasoft.net>
2023-06-27 23:20:26 +0000

    for #5829 replace mbconvert with moab calls

--- sirepo/pkcli/cloudmc.py
+++ sirepo/pkcli/cloudmc.py
@@ -7,11 +7,11 @@
 from pykern import pkio
 from pykern.pkcollections import PKDict
 from pykern.pkdebug import pkdp, pkdlog
+from pymoab.rng import Range
 from sirepo.template import template_common
 import array
 import copy
 import json
-import os
 import py.path
 import pymoab.core
 import pymoab.types
@@ -22,26 +22,12 @@ import sirepo.template.cloudmc
 import sirepo.util
 import uuid
 
-import sirepo.template.cloudmc as template
-
 
 _DATA_DIR = "data"
 _VTI_TEMPLATE = PKDict(
     CellData=PKDict(),
     FieldData=PKDict(),
     vtkClass="vtkPolyData",
-    lines=PKDict(
-        name="_lines",
-        numberOfComponents=1,
-        dataType="Uint32Array",
-        vtkClass="vtkCellArray",
-        ref=PKDict(
-            encode="LittleEndian",
-            basepath=_DATA_DIR,
-            id=None,
-        ),
-        size=None,
-    ),
     polys=PKDict(
         name="_polys",
         numberOfComponents=1,
@@ -74,16 +60,27 @@ _VTI_TEMPLATE = PKDict(
 
 
 def extract_dagmc(dagmc_filename):
-    mat = _extract_volumes(dagmc_filename)
-    for vol in mat.values():
-        _vtk_to_bin(vol.volId)
-    return mat
+    mb = pymoab.core.Core()
+    mb.load_file(dagmc_filename)
+    groups = _groups_and_volumes(mb)
+    visited = set()
+    res = PKDict()
+    for name, volumes in groups.items():
+        if _visited_volume(volumes, visited):
+            continue
+        geom = _get_points_and_polys(mb, volumes)
+        _write_vti(geom)
+        res[name] = PKDict(
+            name=name,
+            volId=geom.vol_id,
+        )
+    return res
 
 
 def run(cfg_dir):
     template_common.exec_parameters()
     data = sirepo.simulation_db.read_json(template_common.INPUT_BASE_NAME)
-    template.extract_report_data(pkio.py_path(cfg_dir), data)
+    sirepo.template.cloudmc.extract_report_data(pkio.py_path(cfg_dir), data)
 
 
 def run_background(cfg_dir):
@@ -105,44 +102,62 @@ def _array_from_list(arr, arr_type):
     return res
 
 
-def _assert_size(res, name):
-    n = f"{name}s"
-    if res[n] is None:
-        raise AssertionError(f"Missing {n}")
-    c = res[f"{name}_count"]
-    if len(res[n]) != c:
-        raise AssertionError(f"{n} do not match count: {len(res[n])} != {c}")
+def _get_tag_value(mb, tag, handle):
+    return str(mb.tag_get_data(tag, handle).flat[0])
 
 
-def _extract_volumes(filename):
+def _get_points_and_polys(mb, volumes):
+    verticies = Range()
+    triangles = Range()
+    for h in volumes:
+        _get_verticies_and_triangles(mb, h, verticies, triangles)
+    m = {}
+    for i, v in enumerate(verticies):
+        m[v] = i
+    polys = []
+    for t in triangles:
+        polys.append(3)
+        polys += [m[vert] for vert in mb.get_connectivity(t)]
+    return PKDict(
+        vol_id=_get_tag_value(
+            mb,
+            mb.tag_get_handle(pymoab.types.GLOBAL_ID_TAG_NAME),
+            volumes[0],
+        ),
+        points=_array_from_list(list(mb.get_coords(verticies)), "f"),
+        polys=_array_from_list(polys, "I"),
+    )
+
+
+def _get_verticies_and_triangles(mb, handle, verticies, triangles, visited=None):
+    if visited is None:
+        visited = set()
+    verticies.merge(mb.get_entities_by_type(handle, pymoab.types.MBVERTEX))
+    triangles.merge(mb.get_entities_by_type(handle, pymoab.types.MBTRI))
+    for c in mb.get_child_meshsets(handle):
+        if c in visited:
+            continue
+        visited.add(c)
+        _get_verticies_and_triangles(mb, c, verticies, triangles, visited)
+
+
+def _groups_and_volumes(mb):
     res = PKDict()
-    visited = PKDict()
-    for v in _parse_volumes(filename).values():
-        name = _parse_entity_name(v.name)
+    name_h = mb.tag_get_handle(pymoab.types.NAME_TAG_NAME)
+    for group in mb.get_entities_by_type_and_tag(
+        mb.get_root_set(),
+        pymoab.types.MBENTITYSET,
+        [mb.tag_get_handle(pymoab.types.CATEGORY_TAG_NAME)],
+        ["Group"],
+    ):
+        name = _parse_entity_name(_get_tag_value(mb, name_h, group))
         if not name:
             continue
-        skip_volume = False
-        for i in v.volumes:
-            if i in visited:
-                pkdlog(f"skipping volume used in multiple groups: {i} {name}")
-                skip_volume = True
-            visited[i] = True
-        if skip_volume:
-            continue
-        if not res.get(name):
-            res[name] = PKDict(
-                volId=v.volumes[0],
-                volumes=v.volumes,
-            )
+        v = [h for h in mb.get_entities_by_handle(group)]
+        if name in res:
+            res[name] = v + res[name]
         else:
-            res[name].volumes += v.volumes
-            res[name].volId = v.volumes[0]
-
-    for n in res:
-        os.system(
-            f'mbconvert -v {",".join(res[n].volumes)} {filename} {res[n].volId}.vtk'
-        )
-        del res[n]["volumes"]
+            res[name] = v
     return res
 
 
@@ -153,107 +168,32 @@ def _parse_entity_name(name):
     return None
 
 
-def _parse_volumes(filename):
-    def get_tag_value(mb, tag, handle):
-        return str(mb.tag_get_data(tag, handle).flat[0])
-
-    mb = pymoab.core.Core()
-    mb.load_file(filename)
-    name_h = mb.tag_get_handle(pymoab.types.NAME_TAG_NAME)
-    id_h = mb.tag_get_handle(pymoab.types.GLOBAL_ID_TAG_NAME)
-    res = PKDict()
-    for group in mb.get_entities_by_type_and_tag(
-        mb.get_root_set(),
-        pymoab.types.MBENTITYSET,
-        [mb.tag_get_handle(pymoab.types.CATEGORY_TAG_NAME)],
-        ["Group"],
-    ):
-        name = get_tag_value(mb, name_h, group)
-        if not re.search(r"^mat:", name):
-            continue
-        assert not res.get(group)
-        res[group] = PKDict(
-            name=name,
-            volumes=[],
-        )
-        for volume in mb.get_entities_by_handle(group):
-            res[group].volumes.append(get_tag_value(mb, id_h, volume))
-    return res
+def _visited_volume(volumes, visited):
+    for h in volumes:
+        if h in visited:
+            pkdlog(f"skipping volume used in multiple groups: {h} {name}")
+            return True
+        visited.add(h)
+    return False
 
 
-def _parse_vtk(filename):
-    res = PKDict(
-        points=[],
-        point_count=None,
-        lines=[],
-        line_count=0,
-        polys=[],
-        poly_count=0,
-        cell_count=None,
-    )
-    state = "header"
-    with pkio.open_text(str(filename)) as f:
-        for line in f:
-            if state == "header":
-                m = re.match(r"^POINTS (\d+)", line)
-                if m:
-                    res.point_count = int(m.group(1)) * 3
-                    state = "points"
-            elif state == "points":
-                m = re.match(r"^CELLS \d+ (\d+)", line)
-                if m:
-                    res.cell_count = int(m.group(1))
-                    state = "cells"
-                    continue
-                p = [float(v) for v in line.split(" ")]
-                if len(p) != 3:
-                    raise AssertionError(f"Invalid point: {p}")
-                res.points += p
-            elif state == "cells":
-                m = re.match(r"^CELL_TYPES", line)
-                if m:
-                    break
-                p = [int(v) for v in line.split(" ")]
-                if p[0] == 2:
-                    res.lines += p
-                    res.line_count += len(p)
-                else:
-                    res.polys += p
-                    res.poly_count += len(p)
-    _assert_size(res, "point")
-    _assert_size(res, "line")
-    _assert_size(res, "poly")
-    if res.line_count + res.poly_count != res.cell_count:
-        raise AssertionError("line and poly count != cell count")
-    return PKDict(
-        points=_array_from_list(res.points, "f"),
-        lines=_array_from_list(res.lines, "I"),
-        polys=_array_from_list(res.polys, "I"),
-    )
-
-
-def _vtk_to_bin(vol_id):
-    filename = f"{vol_id}.vtk"
-    v = _parse_vtk(filename)
+def _write_vti(geometry):
     pkio.unchecked_remove(_DATA_DIR)
     pkio.mkdir_parent(_DATA_DIR)
     vti = copy.deepcopy(_VTI_TEMPLATE)
-    vti.metadata.name = f"{vol_id}.vtp"
-    if not len(v.lines):
-        del vti["lines"]
+    vti.metadata.name = f"{geometry.vol_id}.vtp"
     fns = []
-    for n in ("lines", "polys", "points"):
+    for n in ("polys", "points"):
         if n not in vti:
             continue
         fn = str(uuid.uuid1()).replace("-", "")
         with open(f"{_DATA_DIR}/{fn}", "wb") as f:
-            v[n].tofile(f)
+            geometry[n].tofile(f)
         vti[n].ref.id = fn
-        vti[n].size = int(len(v[n]))
+        vti[n].size = int(len(geometry[n]))
         fns.append(f"{_DATA_DIR}/{fn}")
-    with sirepo.util.write_zip(f"{vol_id}.zip") as f:
+    with sirepo.util.write_zip(f"{geometry.vol_id}.zip") as f:
         f.writestr("index.json", json.dumps(vti))
         for fn in fns:
             f.write(fn)
     pkio.unchecked_remove(_DATA_DIR)
-    pkio.unchecked_remove(filename)
