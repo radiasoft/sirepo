db20cd523c3ddaa6b1fbbce0274cca1cb13f7dd9
git-user <git-email>
2023-07-20 17:20:25 +0000

    Fix #6143: WIP

--- sirepo/template/silas.py
+++ sirepo/template/silas.py
@@ -161,6 +161,12 @@ def stateful_compute_n0n2_plot(data, **kwargs):
             pop_inversion_pump_offset_y=data.model.pump_offset_y,
         )
     ).calc_n0n2(set_n=True, mesh_density=data.model.mesh_density)
+    d = _determinant(n)
+    if abs(d - 1) > _ABCD_DELTA:
+        # TODO (gurhar1133): test this
+        raise AssertionError(
+            f"The determinant of ABCD matrix should be 1, got determinant={d}"
+        )
     p = pkio.py_path("n0n2_plot.png")
     plt.clf()
     fig, axes = plt.subplots(2)
@@ -171,9 +177,6 @@ def stateful_compute_n0n2_plot(data, **kwargs):
     axes[1].set_ylabel("N2")
     plt.xlabel("Slice")
     plt.savefig(p)
-    d = _determinant(n)
-    if abs(d - 1) > _ABCD_DELTA:
-        raise AssertionError(f"The determinant of ABCD matrix should be 1, got determinant={d}")
     return PKDict(
         uri=_data_url(p),
         A=round(n[2][0][0], 9),
