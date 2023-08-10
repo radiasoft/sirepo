0039fa26c359dcbf547a2c82badab7278cc18cc9
mkeilman <mkeilman@radiasoft.net>
2023-08-10 08:23:39 -0600

    #6022 - added test_srw_resize_3d

--- tests/template/srw_resize_3d_test.py
+++ tests/template/srw_resize_3d_test.py
@@ -4,26 +4,28 @@
 :copyright: Copyright (c) 2023 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
-import pytest
 from pykern.pkcollections import PKDict
+from pykern.pkunit import pkeq, pkok
+
+_N_BINS = 100
+
+def setup_module(module):
+    import os
+
+    # Set max to something reasonable for testing
+    os.environ.update(
+        SIREPO_JOB_MAX_MESSAGE_BYTES=str(_N_BINS * _N_BINS),
+    )
 
 
 def test_srw_resize_3d(fc):
     from sirepo.template import srw
-    from pykern.pkdebug import pkdlog
     import numpy
-    import os
 
-    env = PKDict(os.environ)
-    cfg = PKDict(
-        SIREPO_JOB_MAX_MESSAGE_BYTES="1e4",
+    a, xr, yr = srw._reshape_3d(
+        numpy.random.rand(srw._MAX_MESSAGE_BYTES),
+        [0, 0, 0, 0.0, 1.0, _N_BINS, 0.0, 1.0, _N_BINS],
+        PKDict()
     )
-    env.pkupdate(**cfg)
-    pkdlog("MaX B {}", srw._MAX_MESSAGE_BYTES)
-    assert 0
-
-    #a = numpy.random.rand(srw._MAX_MESSAGE_BYTES)
-    #r = PKDict()
-    #srw._reshape_3d(a, [], r)
-
+    pkok(xr[2] < _N_BINS and yr[2] < _N_BINS, "did not reduce bins nx={} ny={}", xr[2], yr[2])
 
