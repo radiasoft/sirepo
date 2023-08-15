1ea66091dbe17af154e68dd809fec35065fb9889
gurhar1133 <ghns1133@gmail.com>
2023-08-15 23:12:28 +0000

    Fix #6123: WIP

--- sirepo/package_data/static/js/sirepo-components.js
+++ sirepo/package_data/static/js/sirepo-components.js
@@ -2599,7 +2599,6 @@ SIREPO.app.directive('importDialog', function(appState, fileManager, fileUpload,
                       <form name="importForm">
                         <div class="form-group">
                           <div data-ng-show="! hideMainImportSelector">
-                          aaa
                             <label>{{ description }}</label>
                             <input id="file-import" type="file" data-file-model="inputFile" data-ng-attr-accept="{{ fileFormats }}">
                             <br />
@@ -2611,7 +2610,7 @@ SIREPO.app.directive('importDialog', function(appState, fileManager, fileUpload,
                         <div class="clearfix"></div>
                         <div class="col-sm-6 pull-right">
                           <button data-ng-click="importFile(inputFile)" class="btn btn-primary" data-ng-disabled="! inputFile || isUploading">Import File</button>
-                           <button data-ng-click="cancel()" data-dismiss="modal" class="btn btn-default" data-ng-disabled="isUploading">Cancel</button>
+                           <button data-ng-click="inputFile = null" data-dismiss="modal" class="btn btn-default" data-ng-disabled="isUploading">Cancel</button>
                         </div>
                       </form>
                     </div>
@@ -2621,19 +2620,12 @@ SIREPO.app.directive('importDialog', function(appState, fileManager, fileUpload,
             </div>
         `,
         controller: function($element, $scope) {
-            // TODO (gurhar1133): fix cancellation error
             $scope.fileUploadError = '';
             // used by sub componenets to possibly hide the "main" file import selector
             $scope.hideMainImportSelector = false;
             $scope.isUploading = false;
             $scope.title = $scope.title || 'Import ZIP File';
             $scope.description = $scope.description || 'Select File';
-            $scope.cancel = () => {
-                $scope.importFile = null;
-                // $scope.errorData = null;
-                srdbg("cancelled and errorData={}", $scope.errorData);
-                // $scope.hideMainImportSelector = false;
-            }
             $scope.importFile = function(inputFile) {
                 if (! inputFile) {
                     return;
@@ -2669,6 +2661,7 @@ SIREPO.app.directive('importDialog', function(appState, fileManager, fileUpload,
         link: function(scope, element) {
             $(element).on('show.bs.modal', function() {
                 $('#file-import').val(null);
+                scope.hideMainImportSelector = false;
                 scope.fileUploadError = '';
                 delete scope.errorData;
                 scope.isUploading = false;
@@ -2773,7 +2766,6 @@ SIREPO.app.directive('importOptions', function(fileUpload, requestSender) {
                 else {
                     $scope.missingFiles = null;
                 }
-                srdbg($scope.missingFiles);
                 return $scope.missingFiles && $scope.missingFiles.length;
             };
         },
