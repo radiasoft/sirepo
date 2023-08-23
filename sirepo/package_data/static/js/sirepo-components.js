b18c67f48fdf1e307ec81e6fdc42f536afe505bb
git-user <git-email>
2023-08-23 17:11:47 +0000

    #5807 - added warning field to ints

--- sirepo/package_data/static/js/sirepo-components.js
+++ sirepo/package_data/static/js/sirepo-components.js
@@ -725,6 +725,7 @@ SIREPO.app.directive('fieldEditor', function(appState, keypressService, panelSta
             <div data-ng-switch="info[1]">
               <div data-ng-switch-when="Integer" data-ng-class="fieldClass">
                 <input data-string-to-number="integer" data-ng-model="model[field]" data-min="info[4]" data-max="info[5]" class="form-control" style="text-align: right" data-lpignore="true" required />
+                <div class="sr-input-warning"></div>
               </div>
               <div data-ng-switch-when="Float" data-ng-class="fieldClass">
                 <input data-string-to-number="" data-ng-model="model[field]" data-min="info[4]" data-max="info[5]" class="form-control" style="text-align: right" data-lpignore="true" required />
