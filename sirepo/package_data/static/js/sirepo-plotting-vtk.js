7c2246fe4458f245ec08d4f1683f3c6022419427
git-user <git-email>
2023-09-19 21:06:35 +0000

    Fix #6326 - set objectEquality true in watch

--- sirepo/package_data/static/js/sirepo-plotting-vtk.js
+++ sirepo/package_data/static/js/sirepo-plotting-vtk.js
@@ -3025,7 +3025,7 @@ SIREPO.app.directive('vtkAxes', function(appState, frameCache, panelState, reque
                     rebuildAxes();
                     refresh();
                 }
-            });
+            }, true);
 
             init();
         },
