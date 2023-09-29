9cf9e4cee6f3f0084d1592a105741f4bad7f587b
e-carlin <evan@carlin.com>
2023-09-29 19:31:55 +0000

    pr comments

--- etc/home-env-funcs.sh
+++ etc/home-env-funcs.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-sirepo_add_admin() {
+_sirepo_dev_mail=~/mail
+_sirepo_dev_src=~/src/radiasoft/sirepo
+
+sirepo_dev_add_admin() {
     declare u
     # POSIT: only uids in user and they don't have any specials
-    for u in $(ls ~/src/radiasoft/sirepo/run/user); do
+    for u in $(ls "$_sirepo_dev_src"/run/user); do
         sirepo roles add "$u" adm
     done
 }
 
-sirepo_mail() {
-    # xargs trims whitespace
-    grep 'http://' $(ls -dt ~/mail/* | head -n1) | xargs
+sirepo_dev_mail() {
+    perl -n -e 'm{(http://\S+)} && print("$1\n")' $(ls -dt "$_sirepo_dev_mail"/* | head -n1)'"")'
 }
