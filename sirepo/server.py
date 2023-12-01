2b8513d43267657b63c537e89df3fbe4ddbd1d03
git-user <git-email>
2023-12-01 20:34:32 +0000

    Fix #6555: WIP

--- sirepo/server.py
+++ sirepo/server.py
@@ -590,6 +590,7 @@ class API(sirepo.quest.API):
         confirm="Bool optional",
     )
     async def api_uploadFile(self, simulation_type, simulation_id, file_type):
+        assert 0, "TESTING"
         f = self.sreq.form_file_get()
         req = self.parse_params(
             file_type=file_type,
