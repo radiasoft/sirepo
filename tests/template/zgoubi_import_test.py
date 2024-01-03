b57356de526fe1e652dfd7d639230571cc44b843
robnagler <github@q33.us>
2024-01-03 19:59:12 +0000

    note: previous ckp was starting convert zgoubi stateful_compute_import_file
    lib_file_exists can now execute agent side

--- tests/template/zgoubi_import_test.py
+++ tests/template/zgoubi_import_test.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
-"""PyTest for :mod:`sirepo.importer`
+"""PyTest for :mod:`sirepo.template.zgoubi_importer`
 
 :copyright: Copyright (c) 2019 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
-import pytest
 
 
 def test_importer(fc):
