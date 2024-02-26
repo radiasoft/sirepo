0b8cace2f4a3526576e53d218cdcbd8338d87bc5
robnagler <github@q33.us>
2024-02-26 22:15:47 +0000

    cruft

--- sirepo/sirepo_console.py
+++ sirepo/sirepo_console.py
@@ -1,16 +1,11 @@
-# -*- coding: utf-8 -*-
-"""SiRepo front-end command line for :mod:`pykern.pkcli`.
+"""Sirepo front-end command line for :mod:`pykern.pkcli`.
 
 Example:
 
 :copyright: Copyright (c) 2015 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
-from __future__ import absolute_import, division, print_function, unicode_literals
-from io import open
-
 import sys
-
 from pykern import pkcli
 
 
