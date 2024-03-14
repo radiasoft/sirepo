628ebdb1d0c13c3ca8a870a487b882e25e86f62c
Michael Keilman <30352290+mkeilman@users.noreply.github.com>
2024-03-14 16:00:46 -0600

    Fix #6892 - watch planePos.numSteps (#6894)

--- sirepo/package_data/static/js/cloudmc.js
+++ sirepo/package_data/static/js/cloudmc.js
@@ -2911,6 +2911,7 @@ SIREPO.viewLogic('tallySettingsView', function(appState, cloudmcService, panelSt
         ], autoUpdate,
         ['openmcAnimation.tally'], validateTally,
         [
+            'tallyReport.planePos.numSteps',
             'tallyReport.selectedGeometry',
             'openmcAnimation.score',
             'openmcAnimation.showSources',
