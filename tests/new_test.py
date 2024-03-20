89753052b13f796214c4e2391056d3c9b86b173a
rorour <ravenmarieorourke@gmail.com>
2024-03-20 14:45:27 -0600

    findByName

--- tests/new_test.py
+++ tests/new_test.py
@@ -13,3 +13,20 @@ def test_favicon(fc):
         with open(sirepo.resource.static("img", p), "rb") as f:
             s = f.read()
             pkeq(len(s), len(r.data))
+
+
+def test_find_by_name(fc):
+    from pykern.pkcollections import PKDict
+    from pykern.pkunit import pkeq
+
+    r = fc.sr_get(
+        "findByName",
+        PKDict(
+            simulation_type="srw",
+            application_mode="default",
+            simulation_name="Undulator Radiation",
+        ),
+        redirect=False,
+    )
+    r.assert_http_status(302)
+    pkeq(r.header_get("Location"), "/srw#/findByName/default/Undulator%20Radiation")
