f9642ef85e5a43cd011d734ccdee91f07dc3eb34
robnagler <github@q33.us>
2024-04-15 18:08:33 +0000

    ckp

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -8,12 +8,11 @@ build_vars() {
     sirepo_boot=$boot_dir/start
     build_docker_cmd='["'"$sirepo_boot"'"]'
     build_is_public=1
-    build_passenv='PYKERN_BRANCH SIREPO_BRANCH'
-    : ${PYKERN_BRANCH:=}  ${RSLUME_BRANCH:=} ${SIREPO_BRANCH:=}
+    build_passenv='PYKERN_BRANCH RSLUME_BRANCH SIREPO_BRANCH'
+    : ${PYKERN_BRANCH:=} ${RSLUME_BRANCH:=} ${SIREPO_BRANCH:=}
 }
 
 build_as_root() {
-    umask 022
     build_yum config-manager \
         --add-repo \
         https://download.docker.com/linux/fedora/docker-ce.repo
@@ -27,9 +26,9 @@ build_as_run_user() {
     cd "$build_guest_conf"
     umask 022
     sirepo_boot_init
-    _sirepo_pip_install pykern
-    _sirepo_pip_install sirepo
-    _sirepo_pip_install rslume
+    _sirepo_pip_install pykern "$PYKERN_BRANCH"
+    _sirepo_pip_install sirepo "$SIREPO_BRANCH"
+    _sirepo_pip_install rslume "$RSLUME_BRANCH"
     _sirepo_test_static_files
 }
 
@@ -39,11 +38,9 @@ sirepo_boot_init() {
     chmod +x "$sirepo_boot"
 }
 
-_sirepo_clone() {
+_sirepo_pip_install() {
     declare repo=$1
-
     declare branch=$2
- "$RSLUME_BRANCH"
     git clone -q -c advice.detachedHead=false ${branch:+--branch "$branch"} --depth=1 https://github.com/radiasoft/"$repo"
     cd "$repo"
     if [[ $repo == sirepo ]]; then
