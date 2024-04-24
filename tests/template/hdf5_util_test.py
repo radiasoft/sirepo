5b956ef11208867aa018145073a4b50fb833e95d
gurhar1133 <ghns1133@gmail.com>
2024-04-24 19:00:55 +0000

    Fix #7020: ckp

--- tests/template/hdf5_util_test.py
+++ tests/template/hdf5_util_test.py
@@ -4,9 +4,30 @@
 :copyright: Copyright (c) 2023 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
+import asyncio
+import os
+import time
 
 
 def test_read_while_writing():
     from sirepo.template.hdf5_util import HDF5Util
+    from pykern import pkunit
 
-    return
+    for d in pkunit.case_dirs():
+        h = HDF5Util(d.join("does_not_exist_at_first.h5"))
+
+        async def _write_to_filename():
+            # async with lock:
+                # time.sleep(4)
+            os.rename(d.join("x.h5"), d.join("does_not_exist_at_first.h5"))
+
+        async def _read():
+            # async with lock:
+            with h.read_while_writing() as f:
+                print("trying to read non existent")
+
+        async def _run():
+            # lock = asyncio.Lock()
+            await asyncio.gather(_read(), _write_to_filename())
+
+        asyncio.run(_run())
