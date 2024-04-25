90bcdb2176209dd0651c0d8e5199bc10537829aa
gurhar1133 <ghns1133@gmail.com>
2024-04-25 22:44:23 +0000

    Fix #7020: ckp

--- tests/template/hdf5_util_test.py
+++ tests/template/hdf5_util_test.py
@@ -11,31 +11,37 @@ import time
 
 
 def test_read_while_writing():
+    def _read_not_written(case_work_dir):
+        p = _proc(case_work_dir, "read.py", "does_not_exist_at_first.h5")
+        time.sleep(10)
+        os.rename(
+            case_work_dir.join("x.h5"),
+            case_work_dir.join("does_not_exist_at_first.h5"),
+        )
+        _raise(p)
+
+    def _read_while_write(case_work_dir):
+        _proc(case_work_dir, "write.py", "x.h5")
+        _raise(_proc(case_work_dir, "read.py", "x.h5"))
+
+    _case_dirs_from_same_data_dir(
+        [
+            _read_not_written,
+            _read_while_write,
+            # TODO (gurhar1133): test KeyError case
+            # TODO (gurhar1133): also, maybe these should be different tests?
+        ]
+    )
+
+
+def _case_dirs_from_same_data_dir(cases):
     from pykern import pkunit
 
-    d = pkunit.data_dir()
     w = pkunit.work_dir()
-    for case_work_dir in _case_dirs_from_same_data_dir(d, w, 2):
-        if case_work_dir.basename == "1":
-            p = _proc(case_work_dir, "read.py", "does_not_exist_at_first.h5")
-            time.sleep(10)
-            os.rename(
-                case_work_dir.join("x.h5"),
-                case_work_dir.join("does_not_exist_at_first.h5"),
-            )
-            _raise(p)
-        if case_work_dir.basename == "2":
-            _proc(case_work_dir, "write.py", "x.h5")
-            _raise(_proc(case_work_dir, "read.py", "x.h5"))
-
-
-def _case_dirs_from_same_data_dir(data_dir, work_dir, cases):
-    res = []
-    for i in range(cases):
-        w = work_dir.join(str(i + 1))
-        shutil.copytree(data_dir, w)
-        res.append(w)
-    return res
+    for i in range(len(cases)):
+        c = w.join(str(i + 1))
+        shutil.copytree(pkunit.data_dir(), c)
+        cases[i](c)
 
 
 def _proc(dir, exe_name, data_name):
