84a02722d788a3c85122abdcd7d2f87f86951bc9
gurhar1133 <ghns1133@gmail.com>
2024-04-26 20:03:26 +0000

    Fix #6920: use read_while_writing

--- sirepo/template/opal.py
+++ sirepo/template/opal.py
@@ -16,7 +16,6 @@ from sirepo.template import lattice
 from sirepo.template import template_common
 from sirepo.template.lattice import LatticeUtil
 from sirepo.template.madx_converter import MadxConverter
-import h5py
 import math
 import numpy as np
 import re
@@ -1079,7 +1078,7 @@ def _generate_beamline(
 
 
 def _iterate_hdf5_steps(path, callback, state):
-    with h5py.File(str(path), "r") as f:
+    with hdf5_util.HDF5Util(str(path)).read_while_writing() as f:
         _iterate_hdf5_steps_from_handle(f, callback, state)
     return state
 
