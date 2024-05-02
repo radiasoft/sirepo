98c91320aaf03bc35d7a4f07b203fb384e711201
gurhar1133 <ghns1133@gmail.com>
2024-05-02 19:51:40 +0000

    Fix #7020: ckp

--- tests/template/hdf5_util_data/write.py
+++ tests/template/hdf5_util_data/write.py
@@ -5,10 +5,10 @@ import time
 def _write():
     with h5py.File(sys.argv[1], 'w') as f:
         if len(sys.argv) > 2 and sys.argv[2] == "check_key":
-            time.sleep(3)
+            time.sleep(1)
             f.create_dataset("this_key_wont_exist_at_first", data=[])
         else:
-            time.sleep(3)
+            time.sleep(1)
             f.create_dataset('dataset_name', data=[])
 
 _write()
