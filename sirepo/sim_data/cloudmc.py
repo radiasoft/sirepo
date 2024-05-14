c99c300ccce8090e8f53956d7a889374b67e469e
gurhar1133 <ghns1133@gmail.com>
2024-05-14 16:52:07 +0000

    Fix #5989: ckp

--- sirepo/sim_data/cloudmc.py
+++ sirepo/sim_data/cloudmc.py
@@ -53,20 +53,20 @@ class SimData(sirepo.sim_data.SimDataBase):
         )
         if "plane1a" in dm.reflectivePlanes:
             dm.reflectivePlanes.planesList = [
-                 {
+                {
                     "inside": "0",
                     "A": dm.reflectivePlanes.plane1a,
                     "B": dm.reflectivePlanes.plane1b,
                     "C": 0,
-                    "D": 0
+                    "D": 0,
                 },
                 {
                     "inside": "1",
                     "A": dm.reflectivePlanes.plane2a,
                     "B": dm.reflectivePlanes.plane2b,
                     "C": 0,
-                    "D": 0
-                }
+                    "D": 0,
+                },
             ]
         for k in ("plane1a", "plane1b", "plane2a", "plane2b"):
             dm.reflectivePlanes.pkdel(k)
