26dbe8f9a347b4336221eb626b03bf1317e94c0f
Paul Moeller <moeller@radiasoft.net>
2024-05-17 21:24:32 +0000

    for #6991 hide scrollbars in fullscreen mode

--- sirepo/package_data/static/js/sirepo.js
+++ sirepo/package_data/static/js/sirepo.js
@@ -3851,9 +3851,10 @@ SIREPO.app.factory('fileManager', function(requestSender) {
     return self;
 });
 
-SIREPO.app.controller('NavController', function (activeSection, appState, fileManager, requestSender, $scope, $window, $route) {
+SIREPO.app.controller('NavController', function (activeSection, appState, fileManager, requestSender, utilities, $scope, $window, $route) {
 
     var self = this;
+    self.utilities = utilities;
 
     function openSection(name) {
         requestSender.localRedirect(name, sectionParams(name));
