d106b5de45f2c4991bb6d517c4d1301797fca9fc
gurhar1133 <ghns1133@gmail.com>
2024-05-21 21:17:33 +0000

    Fix #6988: ckp

--- sirepo/template/template_common.py
+++ sirepo/template/template_common.py
@@ -611,12 +611,13 @@ def parameter_plot(x, plots, model, plot_fields=None, plot_colors=None):
     if plot_fields:
         res.update(plot_fields)
     pkdp("\n\n\nmodel Fields = {} plots len = {}", model.keys(), len(plots))
+    pkdp("\n\n\n model.frameReport={} \n\n\n", model.frameReport)
     pkdp("\n\n\n res.keys()={}", res.keys())
     columns_dict = PKDict()
     for plot in res.plots:
         pkdp("\n\n\nplt={}", plot)
         columns_dict[plot.col_name] = plot.points
-    pandas.DataFrame(columns_dict).to_csv("x.csv", index=False)
+    pandas.DataFrame(columns_dict).to_csv(f"{model.frameReport}.csv", index=False)
     return res
 
 
