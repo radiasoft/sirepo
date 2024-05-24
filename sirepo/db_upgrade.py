b5467ff4d4ad65134c26b736e50d4e6decc02468
rorour <ravenmarieorourke@gmail.com>
2024-05-24 17:54:30 -0600

    finish dbupgrade

--- sirepo/db_upgrade.py
+++ sirepo/db_upgrade.py
@@ -86,7 +86,8 @@ def _20240507_cloudmc_to_openmc(qcall):
 
 
 def _20240524_add_role_user(qcall):
-    s = """
+    qcall.auth_db.execute_sql(
+        """
 CREATE TABLE user_role_moderation_t (
     uid VARCHAR(8) NOT NULL,
     role VARCHAR(100) NOT NULL,
@@ -95,16 +96,16 @@ CREATE TABLE user_role_moderation_t (
     last_updated DATETIME DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
     PRIMARY KEY (uid, role)
 )
-    """
-    s2 = """
+"""
+    )
+    qcall.auth_db.execute_sql(
+        """
 INSERT INTO user_role_moderation_t (uid, role, status, moderator_uid, last_updated)
 SELECT uid, role, status, moderator_uid, last_updated
 FROM user_role_invite_t
-    """
+"""
+    )
     qcall.auth_db.drop_table("user_role_invite_t")
-    qcall.auth_db.execute_sql(s)
-    # qcall.auth_db.rename_table("user_role_invite_t", "user_role_moderation_t")
-    # qcall.auth_db.execute_sql("ALTER TABLE user_role_moderation_t DROP COLUMN token")
     for u in qcall.auth_db.all_uids():
         with qcall.auth.logged_in_user_set(u):
             qcall.auth_db.model("UserRole").add_roles([sirepo.auth_role.ROLE_USER])
