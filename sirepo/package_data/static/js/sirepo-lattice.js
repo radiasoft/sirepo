18cbf5268cc9c070a2e8d192ecae65f0edf6a99f
Paul Moeller <moeller@radiasoft.net>
2024-07-03 23:06:06 +0000

    update svg drawing if sbend e1 or e2 changes

--- sirepo/package_data/static/js/sirepo-lattice.js
+++ sirepo/package_data/static/js/sirepo-lattice.js
@@ -1375,10 +1375,21 @@ SIREPO.app.directive('lattice', function(appState, latticeService, panelState, p
             }
 
             function itemTrackHash(item, group, length, angle) {
-                return group.items.length + '-' + item.name + '-' + item._id + '-' + length + '-'
-                    + group.rotate + '-' + group.rotateX + '-' + group.rotateY + '-' + (angle || 0)
-                    + '-' + item.beamlineIndex + '-' + (item.elemedge || 0)
-                    + '-' + (item.open_side || '');
+                return [
+                    group.items.length,
+                    item.name,
+                    item._id,
+                    length,
+                    group.rotate,
+                    group.rotateX,
+                    group.rotateY,
+                    angle,
+                    item.beamlineIndex,
+                    item.elemedge,
+                    item.open_side,
+                    item.e1,
+                    item.e2,
+                ].reduce((a, v) => a + '-' + (v || 0), 'v');
             }
 
             function subScaleWatch() {
