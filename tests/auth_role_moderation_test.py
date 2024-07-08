radiasoft/sirepo

commit 4adc9f2747891a821cb579d912403a51d5af88ba

fix #7050: add mode to disable users (#7070)
* fix #7050: add mode to disable users

* ckp undo unwanted changes

* db upgrade, check user role, fix pkdlogs

* ckp

* ckp

* ckp db upgrade

* ckp

* ckp

* ckp

* ckp

* finish dbupgrade

* rename userroleinvite and remove token

* ckp fix upgrade

* update test, add login check

* undo jupyter changes

* adm_and_own_jobs working

* adm_and_own_jobs all testsworking

* auth_role_moderation_test working

* fix data for db_upgrade2

* update test data

* pkcli tests passing

* fix supervisor_purge_free_sims_test.py

* first round comments

* comments round 2

* comments round 3

* comments round 4

* comments round 5

* remove debugging

* comments round 6

* fix db upgrade

* fix roles test

* format strings

sirepo/auth_role_moderation.py
@@ -32,7 +32,7 @@ _ACTIVE = frozenset(
 
 class API(sirepo.quest.API):
     @sirepo.quest.Spec(
-        "require_adm", token="AuthModerationToken", status="AuthModerationStatus"
+        "require_adm", uid="Str", role="Str", status="AuthModerationStatus"
     )
     async def api_admModerate(self):
         def _send_moderation_status_email(info):
@@ -56,21 +56,25 @@ class API(sirepo.quest.API):
         def _set_moderation_status(info):
             if info.status == "approve":
                 self.auth_db.model("UserRole").add_roles(roles=[info.role])
-            self.auth_db.model("UserRoleInvite").set_status(
+            self.auth_db.model("UserRoleModeration").set_status(
                 role=info.role,
                 status=info.status,
                 moderator_uid=info.moderator_uid,
             )
 
         req = self.parse_post(type=False)
-        i = self.auth_db.model("UserRoleInvite").unchecked_search_by(
-            token=req.req_data.token
+        i = self.auth_db.model("UserRoleModeration").unchecked_search_by(
+            uid=req.req_data.uid,
+            role=req.req_data.role,
         )
         if not i:
-            pkdlog(f"No record in UserRoleInvite for token={req.req_data.token}")
+            pkdlog()
             raise sirepo.util.UserAlert(
                 "Could not find the moderation request; "
-                "refresh your browser to get the latest moderation list.",
+                + "refresh your browser to get the latest moderation list.",
+                "UserRoleModeration not found uid={} role={}",
+                req.req_data.uid,
+                req.req_data.role,
             )
         p = PKDict(
             app_name=sirepo.simulation_db.SCHEMA_COMMON.appInfo[
@@ -80,14 +84,13 @@ class API(sirepo.quest.API):
             status=req.req_data.status,
             moderator_uid=self.auth.logged_in_user(),
         )
-        pkdlog("status={} uid={} role={} token={}", p.status, i.uid, i.role, i.token)
-        with self.auth.logged_in_user_set(uid=i.uid, method=self.auth.METHOD_EMAIL):
-            p.pkupdate(
-                display_name=self.auth.user_display_name(i.uid),
-                user_name=self.auth.logged_in_user_name(),
-            )
-            _set_moderation_status(p)
-            _send_moderation_status_email(p)
+        pkdlog("status={} uid={} role={}", p.status, i.uid, i.role)
+        p.pkupdate(
+            display_name=self.auth.user_display_name(i.uid),
+            user_name=self.auth.user_name(i.uid),
+        )
+        _set_moderation_status(p)
+        _send_moderation_status_email(p)
         return self.reply_ok()
 
     @sirepo.quest.Spec("require_adm")
@@ -106,7 +109,9 @@ class API(sirepo.quest.API):
         return self.reply_dict(
             PKDict(
                 rows=_datetime_to_str(
-                    self.auth_db.model("UserRoleInvite").get_moderation_request_rows()
+                    self.auth_db.model(
+                        "UserRoleModeration"
+                    ).get_moderation_request_rows()
                 ),
             ),
         )
@@ -131,15 +136,14 @@ class API(sirepo.quest.API):
             raise sirepo.util.Redirect(sirepo.uri.local_route(req.type))
         try:
             self.auth_db.model(
-                "UserRoleInvite",
+                "UserRoleModeration",
                 uid=u,
                 role=r,
                 status=sirepo.auth_role.ModerationStatus.PENDING,
-                token=sirepo.util.random_base62(32),
             ).save()
         except sqlalchemy.exc.IntegrityError as e:
             pkdlog(
-                "Error={} saving UserRoleInvite for uid={} role={} stack={}",
+                "Error={} saving UserRoleModeration for uid={} role={} stack={}",
                 e,
                 u,
                 r,
@@ -174,7 +178,7 @@ def _datetime_to_str(rows):
 
 
 def raise_control_for_user(qcall, uid, role):
-    s = qcall.auth_db.model("UserRoleInvite").get_status(role=role)
+    s = qcall.auth_db.model("UserRoleModeration").get_status(uid=uid, role=role)
     if s in _ACTIVE:
         raise sirepo.util.SRException("moderationPending", None)
     if s == sirepo.auth_role.ModerationStatus.DENY:

tests/auth_role_moderation_test.py
@@ -26,8 +26,12 @@ def test_moderation(auth_fc):
     auth_fc.sr_email_login("x@x.x", sim_type="srw")
     auth_fc.sr_sim_type_set("myapp")
     with srunit.quest_start() as qcall:
-        qcall.auth_db.model("UserRole").delete_all()
-        qcall.auth_db.model("UserRoleInvite").delete_all()
+        qcall.auth_db.model("UserRole").delete_all_for_column_by_values(
+            "role", [auth_role.for_sim_type(auth_fc.sr_sim_type)]
+        )
+        qcall.auth_db.model("UserRoleModeration").delete_all_for_column_by_values(
+            "role", [auth_role.for_sim_type(auth_fc.sr_sim_type)]
+        )
     auth_fc.assert_post_will_redirect(
         "moderation-request",
         "listSimulations",
@@ -55,7 +59,8 @@ def test_moderation(auth_fc):
     auth_fc.sr_post(
         "admModerate",
         PKDict(
-            token=r.rows[0].token,
+            uid=r.rows[0].uid,
+            role=r.rows[0].role,
             status="approve",
         ),
     )

sirepo/auth/__init__.py
@@ -184,6 +184,16 @@ class _Auth(sirepo.quest.Attr):
             auth_role_moderation.raise_control_for_user(self.qcall, u, r)
         raise sirepo.util.Forbidden(f"uid={u} does not have access to sim_type={t}")
 
+    def _assert_role_user(self):
+        u = self.logged_in_user()
+        if not self.qcall.auth_db.model("UserRole").has_role(
+            role=sirepo.auth_role.ROLE_USER,
+        ):
+            raise sirepo.util.Forbidden(
+                f"uid={u} role={sirepo.auth_role.ROLE_USER} not found"
+            )
+        return u
+
     def complete_registration(self, name=None):
         """Update the database with the user's display_name and sets state to logged-in.
         Guests will have no name.
@@ -481,7 +491,9 @@ class _Auth(sirepo.quest.Attr):
         if not self.qcall.auth_db.model("UserRole").has_role(
             role=sirepo.auth_role.ROLE_ADM,
         ):
-            raise sirepo.util.Forbidden(f"uid={u} role=ROLE_ADM not found")
+            raise sirepo.util.Forbidden(
+                f"uid={u} role={sirepo.auth_role.ROLE_ADM} not found"
+            )
 
     def require_auth_basic(self):
         m = _METHOD_MODULES["basic"]
@@ -521,33 +533,31 @@ class _Auth(sirepo.quest.Attr):
                 if f:
                     pkdc("validate_login method={}", m)
                     f(self.qcall)
-                return u
+                return self._assert_role_user()
             if m in _cfg.deprecated_methods:
                 e = "deprecated"
             else:
                 e = "invalid"
                 self.reset_state()
                 p = PKDict(reload_js=True)
-            e = "auth_method={} is {}, forcing login: uid=".format(m, e, u)
+            e = f"auth_method={m} is {e}, forcing login: uid={u}"
         elif s == _STATE_LOGGED_OUT:
             e = "logged out uid={}".format(u)
             if m in _cfg.deprecated_methods:
                 # Force login to this specific method so we can migrate to valid method
                 r = "loginWith"
                 p = PKDict({":method": m})
-                e = "forced {}={} uid={}".format(m, r, p)
+                e = f"forced {r}={m} uid={u}"
         elif s == _STATE_COMPLETE_REGISTRATION:
             if m == METHOD_GUEST:
                 pkdc("guest completeRegistration={}", u)
                 self.complete_registration()
                 self.qcall.auth_db.commit()
-                return u
+                return self._assert_role_user()
             r = "completeRegistration"
             e = "uid={} needs to complete registration".format(u)
         else:
-            self.qcall.cookie.reset_state(
-                "uid={} state={} invalid, cannot continue".format(s, u)
-            )
+            self.qcall.cookie.reset_state(f"state={s} uid={u} invalid, cannot continue")
             p = PKDict(reload_js=True)
             e = "invalid cookie state={} uid={}".format(s, u)
         pkdc("SRException uid={} route={} params={} method={} error={}", u, r, p, m, e)
@@ -597,14 +607,15 @@ class _Auth(sirepo.quest.Attr):
             return None
         return self._qcall_bound_user()
 
-    def user_name(self, uid, method):
+    def user_name(self, uid, method=None):
         """Return user_name"""
-        u = getattr(_METHOD_MODULES[method], "UserModel", None)
-        if u:
-            return self.qcall.auth_db.model(u).search_by(uid=uid).user_name
-        elif method == METHOD_GUEST:
+        m = method or self._qcall_bound_method()
+        t = getattr(_METHOD_MODULES[m], "UserModel", None)
+        if t:
+            return self.qcall.auth_db.model(t).search_by(uid=uid).user_name
+        elif m == METHOD_GUEST:
             return f"{METHOD_GUEST}-{uid}"
-        raise AssertionError(f"user_name not found for uid={uid} with method={method}")
+        raise AssertionError(f"user_name not found for uid={uid} with method={m}")
 
     def user_registration(self, uid, display_name=None):
         """Get UserRegistration record or create one
@@ -682,19 +693,15 @@ class _Auth(sirepo.quest.Attr):
         pkdc("state={}", v)
         return v
 
-    def _create_roles_for_new_user(self, method):
-        r = sirepo.auth_role.for_new_user(is_guest=method == METHOD_GUEST)
-        if r:
-            self.qcall.auth_db.model("UserRole").add_roles(roles=r)
-
     def _create_user(self, module, want_login):
         u = simulation_db.user_create()
         if want_login:
             self._login_user(module, u)
-            self._create_roles_for_new_user(module.AUTH_METHOD)
-        else:
+        if r := sirepo.auth_role.for_new_user(
+            is_guest=module.AUTH_METHOD == METHOD_GUEST
+        ):
             with self.logged_in_user_set(u, method=module.AUTH_METHOD):
-                self._create_roles_for_new_user(module.AUTH_METHOD)
+                self.qcall.auth_db.model("UserRole").add_roles(roles=r)
         return u
 
     def _handle_user_dir_not_found(self, user_dir, uid):

sirepo/auth_db/__init__.py
@@ -142,7 +142,7 @@ class _AuthDb(sirepo.quest.Attr):
         """Must not be called with user data"""
         column_type = column_type.upper()
         t = model.__table__.name
-        r = self._execute_sql(f"PRAGMA table_info({t})")
+        r = self.execute_sql(f"PRAGMA table_info({t})")
         for c in r.all():
             if not c[1] == column:
                 continue
@@ -151,7 +151,7 @@ class _AuthDb(sirepo.quest.Attr):
                 f" type={column_type} to table={t}",
             )
             return
-        self._execute_sql(f"ALTER TABLE {t} ADD {column} {column_type}")
+        self.execute_sql(f"ALTER TABLE {t} ADD {column} {column_type}")
 
     def all_uids(self):
         return list(self.model("UserRegistration").search_all_for_column("uid"))
@@ -181,12 +181,15 @@ class _AuthDb(sirepo.quest.Attr):
         self._commit_or_rollback(commit=commit)
 
     def drop_table(self, old):
-        self._execute_sql(f"DROP TABLE IF EXISTS {old}")
+        self.execute_sql(f"DROP TABLE IF EXISTS {old}")
 
     def execute(self, statement):
         return self.session().execute(
             statement.execution_options(synchronize_session="fetch")
         )
+
+    def execute_sql(self, text):
+        return self.execute(sqlalchemy.text(text + ";"))
 
     def metadata(self):
         return UserDbBase.metadata
@@ -209,7 +212,7 @@ class _AuthDb(sirepo.quest.Attr):
         return self.session().query(*(_class(m) for m in models))
 
     def rename_table(self, old, new):
-        self._execute_sql(f"ALTER TABLE {old} RENAME TO {new}")
+        self.execute_sql(f"ALTER TABLE {old} RENAME TO {new}")
 
     def session(self):
         if self._orm_session is None:
@@ -217,6 +220,9 @@ class _AuthDb(sirepo.quest.Attr):
             self._orm_session = sqlalchemy.orm.Session(bind=_engine)
             self._orm_session.begin()
         return self._orm_session
+
+    def table_exists(self, table_name):
+        return table_name in sqlalchemy.inspect(_engine).get_table_names()
 
     def _commit_or_rollback(self, commit):
         if self._orm_session is None:
@@ -228,9 +234,6 @@ class _AuthDb(sirepo.quest.Attr):
         else:
             s.rollback()
         s.close()
-
-    def _execute_sql(self, text):
-        return self.execute(sqlalchemy.text(text + ";"))
 
     def init_quest_for_child(self, *args, **kwargs):
         # TODO(robnagler): Consider nested transactions

sirepo/auth_db/user.py
@@ -38,7 +38,7 @@ class UserRole(sirepo.auth_db.UserDbBase):
             try:
                 # Check here, because sqlite doesn't through IntegrityErrors
                 # at the point of the new() operation.
-                if not self.has_role(r):
+                if not self.has_role(r, uid=u):
                     self.new(uid=u, role=r, expiration=expiration).save()
             except sqlalchemy.exc.IntegrityError:
                 # role already exists
@@ -53,14 +53,14 @@ class UserRole(sirepo.auth_db.UserDbBase):
         r.expiration = expiration
         r.save()
 
-    def delete_roles(self, roles):
+    def delete_roles(self, roles, uid=None):
         from sirepo import sim_data
 
         cls = self.__class__
         self.auth_db.execute(
             sqlalchemy.delete(cls)
             .where(
-                cls.uid == self.logged_in_user(),
+                cls.uid == (uid or self.logged_in_user()),
             )
             .where(
                 cls.role.in_(roles),
@@ -71,8 +71,10 @@ class UserRole(sirepo.auth_db.UserDbBase):
     def get_roles(self):
         return self.search_all_for_column("role", uid=self.logged_in_user())
 
-    def has_role(self, role):
-        return bool(self.unchecked_search_by(uid=self.logged_in_user(), role=role))
+    def has_role(self, role, uid=None):
+        return bool(
+            self.unchecked_search_by(uid=uid or self.logged_in_user(), role=role)
+        )
 
     def is_expired(self, role):
         u = self.logged_in_user()
@@ -102,12 +104,11 @@ class UserRole(sirepo.auth_db.UserDbBase):
         ]
 
 
-class UserRoleInvite(sirepo.auth_db.UserDbBase):
-    __tablename__ = "user_role_invite_t"
+class UserRoleModeration(sirepo.auth_db.UserDbBase):
+    __tablename__ = "user_role_moderation_t"
     uid = sqlalchemy.Column(sirepo.auth_db.STRING_ID, primary_key=True)
     role = sqlalchemy.Column(sirepo.auth_db.STRING_NAME, primary_key=True)
     status = sqlalchemy.Column(sirepo.auth_db.STRING_NAME, nullable=False)
-    token = sqlalchemy.Column(sirepo.auth_db.STRING_NAME, nullable=False, unique=True)
     moderator_uid = sqlalchemy.Column(sirepo.auth_db.STRING_ID)
     last_updated = sqlalchemy.Column(
         sqlalchemy.DateTime(),
@@ -135,8 +136,8 @@ class UserRoleInvite(sirepo.auth_db.UserDbBase):
         )
         return [PKDict(zip(r.keys(), r)) for r in q]
 
-    def get_status(self, role):
-        s = self.unchecked_search_by(uid=self.logged_in_user(), role=role)
+    def get_status(self, role, uid=None):
+        s = self.unchecked_search_by(uid=uid or self.logged_in_user(), role=role)
         if not s:
             return None
         return sirepo.auth_role.ModerationStatus.check(s.status)

sirepo/auth_role.py
@@ -10,6 +10,7 @@ import aenum
 import sirepo.feature_config
 
 ROLE_ADM = "adm"
+ROLE_USER = "user"
 ROLE_PAYMENT_PLAN_ENTERPRISE = "enterprise"
 ROLE_PAYMENT_PLAN_PREMIUM = "premium"
 PAID_USER_ROLES = (ROLE_PAYMENT_PLAN_PREMIUM, ROLE_PAYMENT_PLAN_ENTERPRISE)
@@ -17,7 +18,7 @@ _SIM_TYPE_ROLE_PREFIX = "sim_type_"
 
 
 class ModerationStatus(aenum.NamedConstant):
-    """States used by auth_role_moderation and UserRoleInvite"""
+    """States used by auth_role_moderation and UserRoleModeration"""
 
     APPROVE = "approve"
     CLARIFY = "clarify"
@@ -40,7 +41,7 @@ def for_moderated_sim_types():
 def for_new_user(is_guest):
     if is_guest and pkconfig.in_dev_mode():
         return get_all()
-    return []
+    return [ROLE_USER]
 
 
 def for_proprietary_oauth_sim_types():
@@ -60,6 +61,7 @@ def get_all():
         ROLE_ADM,
         ROLE_PAYMENT_PLAN_ENTERPRISE,
         ROLE_PAYMENT_PLAN_PREMIUM,
+        ROLE_USER,
     ]
 
 

sirepo/db_upgrade.py
@@ -1,9 +1,9 @@
-# -*- coding: utf-8 -*-
 """Database upgrade management
 
 :copyright: Copyright (c) 2021-2023 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
+
 from pykern import pkinspect, pkio, pkjson
 from pykern.pkcollections import PKDict
 from pykern.pkdebug import pkdp, pkdlog, pkdexc
@@ -83,6 +83,23 @@ def _20240507_cloudmc_to_openmc(qcall):
     for u in qcall.auth_db.all_uids():
         with qcall.auth.logged_in_user_set(u):
             _migrate_sim_type("cloudmc", "openmc", qcall, u)
+
+
+def _20240524_add_role_user(qcall):
+    if not qcall.auth_db.table_exists("user_role_invite_t"):
+        return
+    qcall.auth_db.execute_sql(
+        """
+        INSERT INTO user_role_moderation_t (uid, role, status, moderator_uid, last_updated)
+        SELECT uid, role, status, moderator_uid, last_updated
+        FROM user_role_invite_t
+        """
+    )
+    qcall.auth_db.drop_table("user_role_invite_t")
+    qcall.auth_db.execute_sql(
+        f"INSERT INTO user_role_t (uid, role, expiration)"
+        + f'SELECT uid, "{sirepo.auth_role.ROLE_USER}", NULL from user_registration_t'
+    )
 
 
 @contextlib.contextmanager

sirepo/package_data/static/js/sirepo-components.js
@@ -4496,7 +4496,7 @@ SIREPO.app.directive('admRolesList', function(appState, errorService, panelState
             $scope.getModerationRequestRows = function () {
                 const handleResponse = (r) => {
                     $scope.rows = r.rows;
-                    $scope.headers = SIREPO.APP_SCHEMA.common.adm.userRoleInviteColumns;
+                    $scope.headers = SIREPO.APP_SCHEMA.common.adm.userRoleModerationColumns;
                 };
                 requestSender.sendRequest(
                     'getModerationRequestRows',
@@ -4516,7 +4516,8 @@ SIREPO.app.directive('admRolesList', function(appState, errorService, panelState
                     'admModerate',
                     handleResponse,
                     {
-                        token: info.token,
+                        uid: info.uid,
+                        role: info.role,
                         status: status,
                     }
                 );

commit c3f0bbd63cefadd7360801239af0405e68a71fef

Fix #7157: Need to set logged_in_user when moderating.
The call to _set_moderation_status needs the logged_in_user to be the
user to moderate. Prior to this change we were using the uid of the
moderator as the uid of the person to moderate.

Regression from #7070.

sirepo/auth_role_moderation.py
@@ -18,7 +18,14 @@ import sirepo.uri
 import sirepo.uri_router
 import sqlalchemy
 
-_STATUS_TO_SUBJECT = None
+_STATUS_TO_SUBJECT = PKDict(
+    approve="Access Request Approved",
+    # TODO(robnagler) should we send an email when moderation pending?
+    # For completeness
+    pending=None,
+    clarify="Additional Info?",
+    deny="Access Request Denied",
+)
 
 _cfg = None
 
@@ -38,7 +45,7 @@ class API(sirepo.quest.API):
         def _send_moderation_status_email(info):
             sirepo.smtp.send(
                 recipient=info.user_name,
-                subject=_STATUS_TO_SUBJECT[info.status].format(info.app_name),
+                subject=f"Sirepo {info.app_name}: {_STATUS_TO_SUBJECT[info.status]}",
                 body=pkjinja.render_resource(
                     f"auth_role_moderation/{info.status}_email",
                     PKDict(
@@ -68,7 +75,11 @@ class API(sirepo.quest.API):
             role=req.req_data.role,
         )
         if not i:
-            pkdlog()
+            pkdlog(
+                "UserRoleModeration not found uid={} role={}",
+                req.req_data.uid,
+                req.req_data.role,
+            )
             raise sirepo.util.UserAlert(
                 "Could not find the moderation request; "
                 + "refresh your browser to get the latest moderation list.",
@@ -85,12 +96,23 @@ class API(sirepo.quest.API):
             moderator_uid=self.auth.logged_in_user(),
         )
         pkdlog("status={} uid={} role={}", p.status, i.uid, i.role)
-        p.pkupdate(
-            display_name=self.auth.user_display_name(i.uid),
-            user_name=self.auth.user_name(i.uid),
-        )
-        _set_moderation_status(p)
-        _send_moderation_status_email(p)
+        # Force METHOD_EMAIL. We are sending them an email so we will
+        # need an email for them. We only have emails for METHOD_EMAIL
+        # users.
+        with self.auth.logged_in_user_set(uid=i.uid, method=self.auth.METHOD_EMAIL):
+            u = self.auth.user_name(i.uid)
+            # Sanity check. user_name() should blow up above if METHOD_EMAIL is not
+            # correct but good to be sure.
+            if not u:
+                raise AssertionError(
+                    f"auth method={self.auth.METHOD_EMAIL} is incorrect for uid={i.uid}"
+                )
+            p.pkupdate(
+                display_name=self.auth.user_display_name(i.uid),
+                user_name=u,
+            )
+            _set_moderation_status(p)
+            _send_moderation_status_email(p)
         return self.reply_ok()
 
     @sirepo.quest.Spec("require_adm")
@@ -189,20 +211,12 @@ def raise_control_for_user(qcall, uid, role):
 
 
 def init_apis(*args, **kwargs):
-    global _cfg, _STATUS_TO_SUBJECT
+    global _cfg
 
     _cfg = pkconfig.init(
         moderator_email=pkconfig.Required(
             str, "The email address to send moderation emails to"
         ),
-    )
-    _STATUS_TO_SUBJECT = PKDict(
-        approve="{} Access Request Approved",
-        # TODO(robnagler) should we send an email when moderation pending?
-        # For completeness
-        pending=None,
-        clarify="Sirepo {}: Additional Info?",
-        deny="{} Access Request Denied",
     )
     x = frozenset(_STATUS_TO_SUBJECT.keys())
     if x != sirepo.auth_role.ModerationStatus.VALID_SET:

tests/auth_role_moderation_test.py
@@ -6,13 +6,15 @@
 
 from pykern.pkcollections import PKDict
 
+_MODERATED_SIM_TYPE = "myapp"
+
 
 def setup_module(module):
     import getpass
     import os
 
     os.environ.update(
-        SIREPO_FEATURE_CONFIG_MODERATED_SIM_TYPES="myapp",
+        SIREPO_FEATURE_CONFIG_MODERATED_SIM_TYPES=_MODERATED_SIM_TYPE,
         SIREPO_AUTH_ROLE_MODERATION_MODERATOR_EMAIL=getpass.getuser()
         + "@localhost.localdomain",
     )
@@ -23,8 +25,9 @@ def test_moderation(auth_fc):
     from sirepo import auth_role, srunit
     from sirepo.pkcli import roles
 
-    auth_fc.sr_email_login("x@x.x", sim_type="srw")
-    auth_fc.sr_sim_type_set("myapp")
+    a = "applicant@x.x"
+    auth_fc.sr_email_login(a, sim_type="srw")
+    auth_fc.sr_sim_type_set(_MODERATED_SIM_TYPE)
     with srunit.quest_start() as qcall:
         qcall.auth_db.model("UserRole").delete_all_for_column_by_values(
             "role", [auth_role.for_sim_type(auth_fc.sr_sim_type)]
@@ -51,6 +54,8 @@ def test_moderation(auth_fc):
         raw_response=True,
     )
     r.assert_http_status(403)
+    auth_fc.sr_logout()
+    auth_fc.sr_email_login("moderator@x.x", sim_type="srw")
     roles.add(auth_fc.sr_auth_state().uid, auth_role.ROLE_ADM)
     r = auth_fc.sr_get("admModerateRedirect")
     r.assert_http_status(200)
@@ -64,6 +69,8 @@ def test_moderation(auth_fc):
             status="approve",
         ),
     )
+    auth_fc.sr_logout()
+    auth_fc.sr_email_login(a, sim_type=_MODERATED_SIM_TYPE)
     r = auth_fc.sr_sim_data()
     pkunit.pkok(r.get("models"), "no models r={}", r)
 
@@ -77,7 +84,7 @@ def test_no_guest(auth_fc):
     r = auth_fc.sr_post(
         "saveModerationReason",
         PKDict(
-            simulationType="myapp",
+            simulationType=_MODERATED_SIM_TYPE,
             reason="reason for needing moderation",
         ),
         raw_response=True,