6c9d0e567c1b738a6f539d3e8225a79d92f74846
robnagler <5495179+robnagler@users.noreply.github.com>
2024-09-10 22:57:35 +0000

    Fix #7247 job_cmd log default_command exceptions

--- sirepo/pkcli/job_cmd.py
+++ sirepo/pkcli/job_cmd.py
@@ -45,6 +45,7 @@ def default_command(in_file):
     Returns:
         str: json output of command, e.g. status msg
     """
+    msg = None
     try:
         f = pkio.py_path(in_file)
         msg = pkjson.load_any(f)
@@ -58,6 +59,13 @@ def default_command(in_file):
             return
         r = PKDict(res).pksetdefault(state=job.COMPLETED)
     except Exception as e:
+        pkdlog(
+            "exception={} jobCmd={} simType={} stack={}",
+            e,
+            msg and msg.get("jobCmd"),
+            msg and msg.get("simulationType"),
+            pkdexc(),
+        )
         r = _maybe_parse_user_alert(e)
     return pkjson.dump_pretty(r, pretty=False)
 
