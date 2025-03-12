1f17188375c8605ee890b78f6a4f300fa06b87b3
Evan Carlin <evan@carlin.com>
2025-03-12 23:25:17 +0000

    ckp

--- tests/payments_test.py
+++ tests/payments_test.py
@@ -27,76 +27,76 @@ def _skip():
 pytestmark = pytest.mark.skipif(_skip(), reason="No Stripe configuration")
 
 
-# def test_auditor(monkeypatch):
-#     from pykern import pkconfig
-#     from pykern import pkio
-#     from pykern import pkunit
-#     from pykern.pkcollections import PKDict
-#     from pykern.pkdebug import pkdp
+def test_auditor(monkeypatch):
+    from pykern import pkconfig
+    from pykern import pkio
+    from pykern import pkunit
+    from pykern.pkcollections import PKDict
+    from pykern.pkdebug import pkdp
 
-#     # Before Sirepo imports
-#     pkconfig.reset_state_for_testing(
-#         PKDict(SIREPO_FEATURE_CONFIG_API_MODULES="payments")
-#     )
-#     from sirepo import auth_role
-#     from sirepo import payments
-#     from sirepo import srdb
-#     from sirepo import srtime
-#     from sirepo import srunit
-#     from sirepo.pkcli import roles
-#     import asyncio
+    # Before Sirepo imports
+    pkconfig.reset_state_for_testing(
+        PKDict(SIREPO_FEATURE_CONFIG_API_MODULES="payments")
+    )
+    from sirepo import auth_role
+    from sirepo import payments
+    from sirepo import srdb
+    from sirepo import srtime
+    from sirepo import srunit
+    from sirepo.pkcli import roles
+    import asyncio
 
-#     pkio.unchecked_remove(srdb.root())
-#     pkunit.data_dir().join("auditor_db").copy(srdb.root())
-#     with srunit.quest_start() as qcall:
-#         qcall.auth_db.model("UserRole").set_role_expiration(
-#             auth_role.ROLE_PLAN_BASIC, _EXPIRATION, uid=_UID_IN_DB
-#         )
-#     with srunit.quest_start() as qcall:
-#         pkunit.pkeq(
-#             1,
-#             len(
-#                 qcall.auth_db.model(
-#                     "UserSubscription"
-#                 ).active_subscriptions_from_stripe()
-#             ),
-#             "expecting just one active subscription",
-#         )
-#         monkeypatch.setattr(
-#             stripe.Subscription,
-#             "retrieve_async",
-#             _subscription_active,
-#         )
-#         asyncio.run(payments._auditor(None))
-#     with srunit.quest_start() as qcall:
-#         monkeypatch.setattr(stripe.Subscription, "retrieve_async", _subscription_unpaid)
-#         asyncio.run(payments._auditor(None))
-#     with srunit.quest_start() as qcall:
-#         pkunit.pkeq(
-#             0,
-#             len(
-#                 qcall.auth_db.model(
-#                     "UserSubscription"
-#                 ).active_subscriptions_from_stripe()
-#             ),
-#             "expecting no active subscriptions",
-#         )
-#         l = roles.list_with_expiration(_UID_IN_DB)
-#         for r in l:
-#             pkdp(
-#                 "e-carlin role={} expiry={} now={}",
-#                 r.role,
-#                 r.expiration,
-#                 srtime.utc_now(),
-#             )
-#             if r.role == auth_role.ROLE_PLAN_BASIC and r.expiration <= srtime.utc_now():
-#                 break
-#         else:
-#             pkunit.pkfail(
-#                 "role={} should be expired in roles={}",
-#                 auth_role.ROLE_PLAN_BASIC,
-#                 l,
-#             )
+    pkio.unchecked_remove(srdb.root())
+    pkunit.data_dir().join("auditor_db").copy(srdb.root())
+    with srunit.quest_start() as qcall:
+        qcall.auth_db.model("UserRole").set_role_expiration(
+            auth_role.ROLE_PLAN_BASIC, _EXPIRATION, uid=_UID_IN_DB
+        )
+    with srunit.quest_start() as qcall:
+        pkunit.pkeq(
+            1,
+            len(
+                qcall.auth_db.model(
+                    "UserSubscription"
+                ).active_subscriptions_from_stripe()
+            ),
+            "expecting just one active subscription",
+        )
+        monkeypatch.setattr(
+            stripe.Subscription,
+            "retrieve_async",
+            _subscription_active,
+        )
+        asyncio.run(payments._auditor(None))
+    with srunit.quest_start() as qcall:
+        monkeypatch.setattr(stripe.Subscription, "retrieve_async", _subscription_unpaid)
+        asyncio.run(payments._auditor(None))
+    with srunit.quest_start() as qcall:
+        pkunit.pkeq(
+            0,
+            len(
+                qcall.auth_db.model(
+                    "UserSubscription"
+                ).active_subscriptions_from_stripe()
+            ),
+            "expecting no active subscriptions",
+        )
+        l = roles.list_with_expiration(_UID_IN_DB)
+        for r in l:
+            pkdp(
+                "e-carlin role={} expiry={} now={}",
+                r.role,
+                r.expiration,
+                srtime.utc_now(),
+            )
+            if r.role == auth_role.ROLE_PLAN_BASIC and r.expiration <= srtime.utc_now():
+                break
+        else:
+            pkunit.pkfail(
+                "role={} should be expired in roles={}",
+                auth_role.ROLE_PLAN_BASIC,
+                l,
+            )
 
 
 def test_checkout_session(monkeypatch):
@@ -116,75 +116,45 @@ def test_checkout_session(monkeypatch):
     from sirepo import util
     from sirepo.pkcli import roles
 
-    #     monkeypatch.setattr(stripe.checkout.Session, "create_async", _checkout_create)
-    #     monkeypatch.setattr(stripe.checkout.Session, "retrieve_async", _checkout_retrieve)
-    #     monkeypatch.setattr(
-    #         stripe.Subscription,
-    #         "retrieve_async",
-    #         _subscription_active,
-    #     )
-    #     pkio.unchecked_remove(srdb.root())
-    #     pkunit.data_dir().join("db").copy(srdb.root())
-    #     with srunit.quest_start() as qcall:
-    #         qcall.cookie.set_sentinel()
-    #         try:
-    #             r = qcall.auth.login(method="guest", uid=_UID_IN_DB, sim_type="myapp")
-    #             pkunit.pkfail("expecting sirepo.util.SReplyExc")
-    #         except util.SReplyExc as e:
-    #             pass
-    #         r = qcall.call_api_sync(
-    #             "paymentCreateCheckoutSession",
-    #             body=PKDict(simulationType=_SIM_TYPE, plan=auth_role.ROLE_PLAN_BASIC),
-    #         )
-    #         pkunit.pkre(rf"^{_CLIENT_SECRET}$", r.content_as_object().clientSecret)
-    #         r = qcall.call_api_sync(
-    #             "paymentCheckoutSessionStatus",
-    #             body=PKDict(sessionId="session_id_test"),
-    #         )
-    #         l = roles.list_with_expiration(_UID_IN_DB)
-    #         for r in l:
-    #             if r.role == auth_role.ROLE_PLAN_BASIC and r.expiration == _EXPIRATION:
-    #                 break
-    #         else:
-    #             pkunit.pkfail(
-    #                 "no role={} with expiration={} in roles={}",
-    #                 auth_role.ROLE_PLAN_BASIC,
-    #                 _EXPIRATION,
-    #                 l,
-    #             )
-    #         pkunit.pkok(
-    #             qcall.auth_db.model("UserSubscription").search_by(uid=_UID_IN_DB),
-    #             "expecting a UserSubscription record",
-    #         )
-    # from pykern import pkconfig
-    # from pykern import pkio
-    # from pykern import pkunit
-    # from pykern.pkcollections import PKDict
-    # from pykern.pkdebug import pkdp
-
-    # # Before Sirepo imports
-    # pkconfig.reset_state_for_testing(
-    #     PKDict(SIREPO_FEATURE_CONFIG_API_MODULES="payments")
-    # )
-    # from sirepo import srdb
-    # from sirepo import srunit
-
-    monkeypatch.setattr(stripe.Webhook, "construct_event", _webhook_construct)
+    monkeypatch.setattr(stripe.checkout.Session, "create_async", _checkout_create)
+    monkeypatch.setattr(stripe.checkout.Session, "retrieve_async", _checkout_retrieve)
     monkeypatch.setattr(
         stripe.Subscription,
         "retrieve_async",
         _subscription_active,
     )
-    monkeypatch.setattr(stripe.Product, "retrieve_async", _product_retrieve)
     pkio.unchecked_remove(srdb.root())
     pkunit.data_dir().join("db").copy(srdb.root())
     with srunit.quest_start() as qcall:
-        r = qcall.call_api_sync("stripeWebhook", body=PKDict())
-        pkunit.pkeq("ok", r.content_as_object().state)
+        qcall.cookie.set_sentinel()
+        try:
+            r = qcall.auth.login(method="guest", uid=_UID_IN_DB, sim_type="myapp")
+            pkunit.pkfail("expecting sirepo.util.SReplyExc")
+        except util.SReplyExc as e:
+            pass
+        r = qcall.call_api_sync(
+            "paymentCreateCheckoutSession",
+            body=PKDict(simulationType=_SIM_TYPE, plan=auth_role.ROLE_PLAN_BASIC),
+        )
+        pkunit.pkre(rf"^{_CLIENT_SECRET}$", r.content_as_object().clientSecret)
+        r = qcall.call_api_sync(
+            "paymentCheckoutSessionStatus",
+            body=PKDict(sessionId="session_id_test"),
+        )
+        l = roles.list_with_expiration(_UID_IN_DB)
+        for r in l:
+            if r.role == auth_role.ROLE_PLAN_BASIC and r.expiration == _EXPIRATION:
+                break
+        else:
+            pkunit.pkfail(
+                "no role={} with expiration={} in roles={}",
+                auth_role.ROLE_PLAN_BASIC,
+                _EXPIRATION,
+                l,
+            )
         pkunit.pkok(
-            qcall.auth_db.model("UserPayment").search_by(uid=_UID_IN_DB),
-            "no UserPayment record for uid={}",
-            _UID_IN_DB,
+            qcall.auth_db.model("UserSubscription").search_by(uid=_UID_IN_DB),
+            "expecting a UserSubscription record",
         )
 
 
