75877e75d880520959da767d27f8c2dc86033a62
Paul Moeller <moeller@radiasoft.net>
2025-07-07 23:45:16 +0000

    added test case for expected sirepo-data.json results

--- tests/exporter_test.py
+++ tests/exporter_test.py
@@ -12,7 +12,7 @@ def test_create_zip(fc):
     from pykern import pkcompat
     from pykern.pkcollections import PKDict
     from pykern.pkdebug import pkdp, pkdpretty
-    from pykern.pkunit import pkeq
+    from pykern.pkunit import pkeq, pkok
     from sirepo import srunit
     import base64
     import re
@@ -33,32 +33,37 @@ def test_create_zip(fc):
 
     with _out_dir() as out_dir:
         imported = _import(fc)
-        for sim_type, sim_name, expect in imported + [
+        for sim_type, sim_name, expect, data_value in imported + [
             (
                 "elegant",
                 "bunchComp - fourDipoleCSR",
                 ["WAKE-inputfile.knsl45.liwake.sdds", "run.py", "sirepo-data.json"],
+                "",
             ),
             (
                 "srw",
                 "Tabulated Undulator Example",
                 ["anything/magnetic_measurements.zip", "run.py", "sirepo-data.json"],
+                '"verticalRange": "15"',
             ),
-            ("warppba", "Laser Pulse", ["run.py", "sirepo-data.json"]),
+            ("warppba", "Laser Pulse", ["run.py", "sirepo-data.json"], ""),
             (
                 "opal",
                 "CSR Bend Drift",
                 ["opal.in", "sirepo-data.json"],
+                "",
             ),
             (
                 "genesis",
                 "PEGASUS FEL",
                 ["genesis.in", "sirepo-data.json"],
+                "",
             ),
             (
                 "madx",
                 "FODO PTC",
                 ["madx.madx", "sirepo-data.json"],
+                "",
             ),
         ]:
             sim_id = fc.sr_sim_data(sim_name, sim_type).models.simulation.simulationId
@@ -77,6 +82,11 @@ def test_create_zip(fc):
                 expect,
                 sorted(zipfile.ZipFile(str(p)).namelist()),
             )
+            if data_value:
+                d = pkcompat.from_bytes(
+                    zipfile.ZipFile(str(p)).read("sirepo-data.json")
+                )
+                pkok(re.search(data_value, d), "")
 
 
 def _import(fc):
@@ -96,5 +106,5 @@ def _import(fc):
             PKDict(simulation_type=f.basename.split("_")[0]),
             file=f,
         )
-        res.append((d.simulationType, d.models.simulation.name, expect))
+        res.append((d.simulationType, d.models.simulation.name, expect, ""))
     return res
