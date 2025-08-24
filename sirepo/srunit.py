db2f9ef6f2615fd14b2f57e755ba88e6e4705d1f
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-08-24 22:56:58 +0000

    undo Fix #7598 srunit.quest_start accepts want_global_user (#7599)

--- sirepo/srunit.py
+++ sirepo/srunit.py
@@ -84,7 +84,7 @@ def http_client(
 
 
 @contextlib.contextmanager
-def quest_start(want_user=False, want_global_user=False, cfg=None):
+def quest_start(want_user=False, cfg=None):
     if cfg is None:
         cfg = {}
     setup_srdb_root(cfg=cfg)
@@ -97,11 +97,9 @@ def quest_start(want_user=False, want_global_user=False, cfg=None):
 
     with quest.start(in_pkcli=True) as qcall:
         qcall.auth_db.create_or_upgrade()
-        if want_global_user or want_user:
-            with qcall.auth.srunit_user(want_global=want_global_user):
-                yield qcall
-        else:
-            yield qcall
+        if want_user:
+            qcall.auth.srunit_user()
+        yield qcall
 
 
 def setup_srdb_root(cfg=None):
