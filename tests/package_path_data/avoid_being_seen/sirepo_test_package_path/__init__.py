4e4923d2754bff25c55659bddbddd3b2690fcebd
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2026-03-04 13:11:36 -0600

    Fix #7835 remove pkg_resources from test (#7837)

--- tests/package_path_data/avoid_being_seen/sirepo_test_package_path/__init__.py
+++ tests/package_path_data/avoid_being_seen/sirepo_test_package_path/__init__.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
-u""":mod:`sirepo_test_package_path` package
+""":mod:`sirepo_test_package_path` package
 
 :copyright: Copyright (c) 2021 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
-from __future__ import absolute_import, division, print_function
-import pkg_resources
+
+import importlib.metadata
 
 try:
+    __version__ = importlib.metadata.version("sirepo_test_package_path")
+except importlib.metadata.PackageNotFoundError:
     # We only have a version once the package is installed.
-    __version__ = pkg_resources.get_distribution('sirepo_test_package_path').version
-except pkg_resources.DistributionNotFound:
     pass
